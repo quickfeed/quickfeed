bf56c0f5dac08af6a9b9385b3e486fdf2de32f2d
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-06-21 16:15:14 +0200

    session.go: Add documentation

--- session.go
+++ session.go
@@ -10,12 +10,14 @@ const (
 	accessTokenKey = iota
 )
 
+// Session holds a session store which stores user sessions.
 type Session struct {
 	store sessions.Store
 
 	authSessionName string
 }
 
+// NewSessionStore initializes a new session store for saving sessions.
 func NewSessionStore(store sessions.Store, authSessionName string) *Session {
 	return &Session{
 		store:           store,
@@ -23,6 +25,7 @@ func NewSessionStore(store sessions.Store, authSessionName string) *Session {
 	}
 }
 
+// Login logs a user in by storing the users access token in the session.
 func (s *Session) Login(w http.ResponseWriter, r *http.Request, accessToken string) error {
 	ss, err := s.store.Get(r, s.authSessionName)
 	if err != nil {
@@ -32,6 +35,7 @@ func (s *Session) Login(w http.ResponseWriter, r *http.Request, accessToken stri
 	return ss.Save(r, w)
 }
 
+// Logout logs a user out by deleting the users access token from the session.
 func (s *Session) Logout(w http.ResponseWriter, r *http.Request) error {
 	ss, err := s.store.Get(r, s.authSessionName)
 	if err != nil {
@@ -41,6 +45,7 @@ func (s *Session) Logout(w http.ResponseWriter, r *http.Request) error {
 	return ss.Save(r, w)
 }
 
+// LoggedIn returns true if a user is logged in.
 func (s *Session) LoggedIn(w http.ResponseWriter, r *http.Request) (ok bool, err error) {
 	ss, err := s.store.Get(r, s.authSessionName)
 	if err != nil {
