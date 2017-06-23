c84c7f0400e802714b10ec8cf763f061947705bd
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2017-06-23 16:34:17 +0200

    models.ts: Added some spaces

--- public/src/models.ts
+++ public/src/models.ts
@@ -6,20 +6,20 @@ interface IUser {
     personId: number;
 }
 
-function isCourse(value: any): value is ICourse{
+function isCourse(value: any): value is ICourse {
     return value
         && typeof value.id === "number"
         && typeof value.name === "string"
         && typeof value.tag === "string";
 }
 
-interface ICourse{
+interface ICourse {
     id: number;
     name: string;
     tag: string;
 }
 
-interface IAssignment{
+interface IAssignment {
     id: number;
     courceId: number;
     name: string;
@@ -28,9 +28,9 @@ interface IAssignment{
     end: Date;
 }
 
-interface ICourseStudent{
+interface ICourseStudent {
     personId: number;
     courseId: number;
 }
 
-export {IUser, isCourse, ICourse, IAssignment, ICourseStudent};
+export { IUser, isCourse, ICourse, IAssignment, ICourseStudent };
