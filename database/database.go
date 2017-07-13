25838122b1b2279ab8365abcf412fc2cf9103d4f
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-07-13 09:24:21 +0200

    database/database.go: Add TODO

--- database/database.go
+++ database/database.go
@@ -8,6 +8,8 @@ type Database interface {
 	AssociateUserWithRemoteIdentity(userID uint64, provider string, remoteID uint64, accessToken string) error
 
 	GetUser(uint64) (*models.User, error)
+	// GetUserByRemoteIdentity gets an user by a remote identity and updates the access token.
+	// TODO: The update access token functionality should be split into its own method.
 	GetUserByRemoteIdentity(provider string, id uint64, accessToken string) (*models.User, error)
 	GetUsers() (*[]models.User, error)
 
