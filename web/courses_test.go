97814471df42771fae03bace72d04af8a63f43d5
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-07-13 11:05:34 +0200

    web/*_test.go: Move utility code into util_test.go

--- web/courses_test.go
+++ web/courses_test.go
@@ -2,16 +2,11 @@ package web_test
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"reflect"
 	"testing"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/autograde/aguis/database"
-	"github.com/autograde/aguis/logger"
 	"github.com/autograde/aguis/models"
 	"github.com/autograde/aguis/web"
 	"github.com/labstack/echo"
@@ -216,50 +211,4 @@ func TestListCoursesForUserWithTwoEnrolledCourses(t *testing.T) {
 		}
 	}
 	assertCode(t, w.Code, http.StatusOK)
-}
-
-func setup(t *testing.T) (*database.GormDB, func()) {
-	const (
-		driver = "sqlite3"
-		prefix = "testdb"
-	)
-
-	f, err := ioutil.TempFile(os.TempDir(), prefix)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		os.Remove(f.Name())
-		t.Fatal(err)
-	}
-
-	db, err := database.NewGormDB(driver, f.Name(), envSet("LOGDB"))
-	if err != nil {
-		os.Remove(f.Name())
-		t.Fatal(err)
-	}
-
-	return db, func() {
-		if err := db.Close(); err != nil {
-			t.Error(err)
-		}
-		if err := os.Remove(f.Name()); err != nil {
-			t.Error(err)
-		}
-	}
-}
-
-func assertCode(t *testing.T, haveCode, wantCode int) {
-	if haveCode != wantCode {
-		t.Errorf("have status code %d want %d", haveCode, wantCode)
-	}
-}
-
-func envSet(env string) database.GormLogger {
-	l := logrus.New()
-	l.Formatter = logger.NewDevFormatter(l.Formatter)
-	if os.Getenv(env) != "" {
-		return database.Logger{Logger: l}
-	}
-	return nil
 }
---
+++ web/util_test.go
@@ -0,0 +1,57 @@
+package web_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/autograde/aguis/database"
+	"github.com/autograde/aguis/logger"
+)
+
+func setup(t *testing.T) (*database.GormDB, func()) {
+	const (
+		driver = "sqlite3"
+		prefix = "testdb"
+	)
+
+	f, err := ioutil.TempFile(os.TempDir(), prefix)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		os.Remove(f.Name())
+		t.Fatal(err)
+	}
+
+	db, err := database.NewGormDB(driver, f.Name(), envSet("LOGDB"))
+	if err != nil {
+		os.Remove(f.Name())
+		t.Fatal(err)
+	}
+
+	return db, func() {
+		if err := db.Close(); err != nil {
+			t.Error(err)
+		}
+		if err := os.Remove(f.Name()); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func assertCode(t *testing.T, haveCode, wantCode int) {
+	if haveCode != wantCode {
+		t.Errorf("have status code %d want %d", haveCode, wantCode)
+	}
+}
+
+func envSet(env string) database.GormLogger {
+	l := logrus.New()
+	l.Formatter = logger.NewDevFormatter(l.Formatter)
+	if os.Getenv(env) != "" {
+		return database.Logger{Logger: l}
+	}
+	return nil
+}
