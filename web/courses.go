12d1e29d592e8471ab1a4f2703cf9169822ab8fc
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-07-17 15:25:48 +0200

    web: Move parseStatuses into util.go

--- web/courses.go
+++ web/courses.go
@@ -332,30 +332,3 @@ func GetEnrollmentsByUser(db database.Database) echo.HandlerFunc {
 		return c.JSONPretty(http.StatusOK, users, "\t")
 	}
 }
-
-// parseStatuses takes a string of comma separated statuses and returns a slice
-// of the corresponding status constants.
-func parseStatuses(s string) ([]uint, bool) {
-	if s == "" {
-		return []uint{}, true
-	}
-
-	ss := strings.Split(s, ",")
-	if len(ss) > 3 {
-		return []uint{}, false
-	}
-	var statuses []uint
-	for _, s := range ss {
-		switch s {
-		case "pending":
-			statuses = append(statuses, models.Pending)
-		case "rejected":
-			statuses = append(statuses, models.Rejected)
-		case "accepted":
-			statuses = append(statuses, models.Accepted)
-		default:
-			return []uint{}, false
-		}
-	}
-	return statuses, true
-}
--- web/util.go
+++ web/util.go
@@ -3,7 +3,9 @@ package web
 import (
 	"net/http"
 	"strconv"
+	"strings"
 
+	"github.com/autograde/aguis/models"
 	"github.com/labstack/echo"
 )
 
@@ -15,4 +17,31 @@ func parseUint(s string) (uint64, error) {
 		return 0, echo.NewHTTPError(http.StatusBadRequest, "invalid identifier")
 	}
 	return n, nil
+}
+
+// parseStatuses takes a string of comma separated statuses and returns a slice
+// of the corresponding status constants.
+func parseStatuses(s string) ([]uint, bool) {
+	if s == "" {
+		return []uint{}, true
+	}
+
+	ss := strings.Split(s, ",")
+	if len(ss) > 3 {
+		return []uint{}, false
+	}
+	var statuses []uint
+	for _, s := range ss {
+		switch s {
+		case "pending":
+			statuses = append(statuses, models.Pending)
+		case "rejected":
+			statuses = append(statuses, models.Rejected)
+		case "accepted":
+			statuses = append(statuses, models.Accepted)
+		default:
+			return []uint{}, false
+		}
+	}
+	return statuses, true
 }
