6ac4911cb1c3c3f563ebafe6fe337dae8ed023ae
Hein Meling <hein.meling@uis.no>
2019-02-20 01:48:59 +0100

    started to clean up web/course.go:UpdateEnrollment()
    mainly fixed lookup for remote identities, student repo name
    construction, avoided unnecessary strings.Replace(), and
    avoided unecessary nil test for team object after err check.

--- web/consts.go
+++ web/consts.go
@@ -2,8 +2,14 @@ package web
 
 // Default repository names.
 const (
-	InfoRepo       = "course-info"
-	AssignmentRepo = "assignments"
-	TestsRepo      = "tests"
-	SolutionsRepo  = "solutions"
+	InfoRepo          = "course-info"
+	AssignmentRepo    = "assignments"
+	TestsRepo         = "tests"
+	SolutionsRepo     = "solutions"
+	StudentRepoSuffix = "-labs"
 )
+
+// StudentRepoName returns the name of the given student's repository.
+func StudentRepoName(userName string) string {
+	return userName + StudentRepoSuffix
+}
