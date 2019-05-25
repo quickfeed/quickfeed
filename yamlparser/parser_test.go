117dd8c4da23c25a6ff9f290351eb1d8728c9d90
Hein Meling <hein.meling@uis.no>
2019-05-25 21:45:32 +0200

    updated parser_test to not depend on testrepos folder.
    This commit removes the need for the testrepos folder and
    instead creates the files in a temp folder before running the
    yaml parser.

--- yamlparser/parser_test.go
+++ yamlparser/parser_test.go
@@ -1,11 +1,17 @@
 package yamlparser_test
 
 import (
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 	"time"
 
 	pb "github.com/autograde/aguis/ag"
+	"github.com/autograde/aguis/ci"
+	"github.com/autograde/aguis/web"
 	"github.com/autograde/aguis/yamlparser"
 	tspb "github.com/golang/protobuf/ptypes"
 )
@@ -18,9 +24,50 @@ func TestParseWithInvalidDir(t *testing.T) {
 	}
 }
 
+const (
+	y1 = `assignmentid: 1
+name: "For loops"
+language: "Go"
+deadline: "27-08-2017 12:00"
+autoapprove: false
+`
+	y2 = `assignmentid: 2
+name: "Nested loops"
+language: "Java"
+deadline: "27-08-2018 12:00"
+autoapprove: false
+`
+)
+
 func TestParse(t *testing.T) {
-	const dir = "testrepos"
-	d, err := time.Parse("02-01-2006 15:04", "27-08-2018 12:00")
+	testsDir, err := ioutil.TempDir("", web.TestsRepo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(testsDir)
+
+	job := &ci.Job{
+		Commands: []string{
+			"cd " + testsDir,
+			"mkdir lab1",
+			"mkdir lab2",
+		},
+	}
+	runner := ci.Local{}
+	_, err = runner.Run(context.Background(), job)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile(filepath.Join(testsDir, "lab1", "assignment.yml"), []byte(y1), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ioutil.WriteFile(filepath.Join(testsDir, "lab2", "assignment.yml"), []byte(y2), 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	d, err := time.Parse("02-01-2006 15:04", "27-08-2017 12:00")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -28,18 +75,33 @@ func TestParse(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	var (
-		wantAssignment1 = &pb.Assignment{
-			Id:          2,
-			Name:        "Lab1",
-			Language:    "java",
-			Deadline:    deadline,
-			AutoApprove: false,
-			Order:       2,
-		}
-	)
+	wantAssignment1 := &pb.Assignment{
+		Id:          1,
+		Name:        "For loops",
+		Language:    "go",
+		Deadline:    deadline,
+		AutoApprove: false,
+		Order:       1,
+	}
 
-	assignments, err := yamlparser.Parse(dir, 0)
+	d, err = time.Parse("02-01-2006 15:04", "27-08-2018 12:00")
+	if err != nil {
+		t.Fatal(err)
+	}
+	deadline, err = tspb.TimestampProto(d)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wantAssignment2 := &pb.Assignment{
+		Id:          2,
+		Name:        "Nested loops",
+		Language:    "java",
+		Deadline:    deadline,
+		AutoApprove: false,
+		Order:       2,
+	}
+
+	assignments, err := yamlparser.Parse(testsDir, 0)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -50,5 +112,8 @@ func TestParse(t *testing.T) {
 
 	if !reflect.DeepEqual(assignments[0], wantAssignment1) {
 		t.Errorf("\nhave %+v \nwant %+v", assignments[0], wantAssignment1)
+	}
+	if !reflect.DeepEqual(assignments[1], wantAssignment2) {
+		t.Errorf("\nhave %+v \nwant %+v", assignments[1], wantAssignment2)
 	}
 }
