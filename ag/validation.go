82ba8d29a1a3eae7efa1cb4f709f83d044859900
0xf8f8ff <0xf8f8ff@protonmail.com>
2019-05-29 17:32:07 +0200

    changed validation methods to use getters instead of accessing fields directly

--- ag/validation.go
+++ ag/validation.go
@@ -2,45 +2,46 @@ package ag
 
 // IsValidGroup checks required fields of a group request
 func (grp Group) IsValidGroup() bool {
-	return grp.Name != "" &&
-		grp.CourseID > 0 &&
-		len(grp.Users) > 0
+	return grp.GetName() != "" &&
+		grp.GetCourseID() > 0 &&
+		len(grp.GetUsers()) > 0
 }
 
 // IsValidCourse checks required fields of a course request
 func (c Course) IsValidCourse() bool {
-	return c.Name != "" &&
-		c.Code != "" &&
-		(c.Provider == "github" || c.Provider == "gitlab" || c.Provider == "fake") &&
-		c.DirectoryID != 0 &&
-		c.Year != 0 &&
-		c.Tag != ""
+	return c.GetName() != "" &&
+		c.GetCode() != "" &&
+		(c.GetProvider() == "github" || c.GetProvider() == "gitlab" || c.GetProvider() == "fake") &&
+		c.GetDirectoryID() != 0 &&
+		c.GetYear() != 0 &&
+		c.GetTag() != ""
 }
 
 // IsValidUser chacks required fields of a user request
 func (u User) IsValidUser() bool {
-	return u.ID > 0
+	return u.GetID() > 0
 }
 
 // IsValidEnrollment checks required fields of an enrollment request
 func (req ActionRequest) IsValidEnrollment() bool {
-	return req.Status <= Enrollment_Teacher &&
-		req.UserID != 0 &&
-		req.CourseID != 0
+	return req.GetStatus() <= Enrollment_Teacher &&
+		req.GetUserID() != 0 &&
+		req.GetCourseID() != 0
 }
 
 // IsValidRequest checks whether RecordRequest fields are valid
 func (req RecordRequest) IsValidRequest() bool {
-	return req.ID > 0
+	return req.GetID() > 0
 }
 
 // IsValidRepoRequest checks required fields of a repository request
 func (req RepositoryRequest) IsValidRepoRequest() bool {
-	return req.CourseID > 0 &&
-		req.Type <= Repository_CourseInfo
+	return req.GetCourseID() > 0 &&
+		req.GetType() <= Repository_CourseInfo
 }
 
+// IsValidRequest checks required fields of an update group request
 func (req ActionRequest) IsValidRequest() bool {
-	return (req.UserID > 0 || req.GroupID > 0) &&
-		req.CourseID > 0
+	return (req.GetUserID() > 0 || req.GetGroupID() > 0) &&
+		req.GetCourseID() > 0
 }
