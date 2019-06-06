b57eacb74712f888bef965f27ff30d95854d3fdc
Hein Meling <hein.meling@uis.no>
2019-06-07 00:19:11 +0200

    removed logrus dependency from selenium test case
    this also reactivates the selenium test case (actually it is skipped)
    because the vendor folder is broken; the selenium go package is
    incomplete.

--- selenium/githublogin_test.go
+++ selenium/githublogin_test.go
@@ -10,7 +10,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/sirupsen/logrus"
 	"github.com/tebeka/selenium"
 )
 
@@ -29,26 +28,25 @@ const (
 
 // Saves a image taken by the webdriver, and saves it to the current
 // folder.
-func saveScreenshot(wd selenium.WebDriver, name string) {
+func saveScreenshot(t *testing.T, wd selenium.WebDriver, name string) {
+	t.Helper()
 	if !TAKE_SCREENSHOT {
 		return
 	}
 	screenshot, err := wd.Screenshot()
 	if err != nil {
-		logrus.Error("Failed to take screenshot")
+		t.Fatal(err)
 	}
 
 	img, _, _ := image.Decode(bytes.NewReader(screenshot))
 	out, err := os.Create("./" + name + ".png")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		t.Fatal(err)
 	}
 
 	err = png.Encode(out, img)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		t.Fatal(err)
 	}
 }
 
@@ -67,47 +65,45 @@ func TestGithubLogin(t *testing.T) {
 		geckoDriverPath = "./drivers/geckodriver"
 		port            = 8080
 	)
-
-	opts := []selenium.ServiceOption{
-		selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
-		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
-		selenium.Output(os.Stderr),            // Output debug information to STDERR.
-	}
+	//TODO(meling) the selenium dependency in vendor seems to be broken; fix and reactivate these commented lines
+	// opts := []selenium.ServiceOption{
+	// 	selenium.StartFrameBuffer(),           // Start an X frame buffer for the browser to run in.
+	// 	selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox.
+	// 	selenium.Output(os.Stderr),            // Output debug information to STDERR.
+	// }
 	selenium.SetDebug(true)
-	service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	defer service.Stop()
+	// service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// defer service.Stop()
 
 	// Connect to the WebDriver instance running locally.
 	caps := selenium.Capabilities{"browserName": "firefox", "acceptSslCerts": true}
 	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	defer wd.Quit()
 
 	// SETUP DONE
 	// Navigate to the index page.
 	if err := wd.Get("https://itest.run"); err != nil {
-		logrus.WithFields(logrus.Fields{
-			"Function": "Get(\"https://www.itest.run\")",
-		}).Fatal(err)
+		t.Fatalf("failed on Get(\"https://www.itest.run\"): %v", err)
 	}
 	// Need time to load the index screen.
 	sleep()
 
 	// One way to get image of how the page you are currently at
 	// looks like.
-	saveScreenshot(wd, "indexpage")
+	saveScreenshot(t, wd, "indexpage")
 
 	// It is also possible to insert script like below, this script does the exact same as
 	// the css selector does.
 	//wd.ExecuteScript("document.getElementsByClassName(\"social-login\")[0].children[0].firstChild.click()", nil)
 	login, err := wd.FindElement(selenium.ByCSSSelector, "a[href=\"/app/login/login/github\"]")
 	if err != nil {
-		logrus.Fatal(err)
+		t.Fatal(err)
 	}
 	login.Click()
 	sleep()
@@ -115,36 +111,36 @@ func TestGithubLogin(t *testing.T) {
 	// Fetch the fields needed.
 	loginField, err := wd.FindElement(selenium.ByID, "login_field")
 	if err != nil {
-		logrus.Fatal(err)
+		t.Fatal(err)
 	}
 
 	loginField.SendKeys(USERNAME)
 
 	passwordField, err := wd.FindElement(selenium.ByID, "password")
 	if err != nil {
-		logrus.Fatal(err)
+		t.Fatal(err)
 	}
 	passwordField.SendKeys(PASSWORD)
 
 	loginButton, err := wd.FindElement(selenium.ByName, "commit")
 	if err != nil {
-		logrus.Fatal(err)
+		t.Fatal(err)
 	}
 
 	// One way to get image of how the page you are currently at
 	// looks like.
-	saveScreenshot(wd, "loginpage")
+	saveScreenshot(t, wd, "loginpage")
 
 	loginButton.Click()
 	sleep()
 
 	// One way to get image of how the page you are currently at
 	// looks like.
-	saveScreenshot(wd, "AuthPage")
+	saveScreenshot(t, wd, "AuthPage")
 
 	authorize, err := wd.FindElement(selenium.ByID, "js-oauth-authorize-btn")
 	if err != nil {
-		logrus.Info("No auth page found")
+		t.Errorf("no auth page found: %v", err)
 	} else {
 		authorize.Click()
 	}
@@ -152,16 +148,15 @@ func TestGithubLogin(t *testing.T) {
 	sleep()
 	hellomsg, err := wd.FindElement(selenium.ByCSSSelector, "div[class='centerblock container']")
 	if err != nil {
-		logrus.Fatal(err)
+		t.Fatal(err)
 	}
 
 	outputText, err := hellomsg.Text()
 	if err != nil {
-		logrus.Error("Could not get output text")
+		t.Fatal(err)
 	}
 
 	if !reflect.DeepEqual(outputText, WANT_MESSAGE) {
 		t.Errorf("have database course %+v want %+v", outputText, WANT_MESSAGE)
 	}
-
 }
