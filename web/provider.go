b9cd7f459ded033bdcacd8de6a89c2e7a8f576a6
Hein Meling <hein.meling@uis.no>
2019-06-13 16:15:54 +0200

    removed unused function web.getSCM() used with echo

--- web/provider.go
+++ web/provider.go
@@ -1,31 +1,15 @@
 package web
 
 import (
-	"net/http"
 	"strings"
 
 	pb "github.com/autograde/aguis/ag"
 
-	"github.com/autograde/aguis/scm"
-	"github.com/labstack/echo"
 	"github.com/markbates/goth"
 )
 
 // TeacherSuffix is used to set user as a teacher to be able to create new course
 const TeacherSuffix = "-teacher"
-
-// getSCM is a helper to get the scm provider for the current echo context.
-// Each user's context store information about their individual scm provider
-// object. (see main.go and web/auth/auth.go for the code that registers the
-// scm.SCM instance.)
-func getSCM(c echo.Context, scmProvider string) (scm.SCM, error) {
-	provider := c.Get(scmProvider)
-	if provider == nil {
-		return nil, echo.NewHTTPError(http.StatusBadRequest, "provider "+scmProvider+" not registered")
-	}
-	// If type assertions fails, the recover middleware will catch the panic and log a stack trace.
-	return provider.(scm.SCM), nil
-}
 
 // GetProviders returns a list of all providers enabled by goth
 func GetProviders() *pb.Providers {
