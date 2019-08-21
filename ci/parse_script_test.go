9752eb780429b88bd3f703a9c60c6cba1f6b4468
Hein Meling <hein.meling@uis.no>
2019-08-21 12:51:38 +0200

    fixed problem with go.tmpl causing tests to not run
    Also updated the test case so that we can run and check output
    by setting the TEST_TMPL environment variable.

--- ci/parse_script_test.go
+++ ci/parse_script_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/sha1"
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 )
@@ -15,8 +16,8 @@ func TestParseScript(t *testing.T) {
 		t.Fatal(err)
 	}
 	randomString := fmt.Sprintf("%x", sha1.Sum(randomness))
-	getURL := "https://github.com/uis-dat520-s2019/assignments.git"
-	testURL := "https://github.com/uis-dat520-s2019/tests.git"
+	getURL := "https://github.com/dat520-s2020/assignments.git"
+	testURL := "https://github.com/dat520-s2020/tests.git"
 	info := AssignmentInfo{
 		AssignmentName:     "lab2",
 		Language:           "go",
@@ -27,9 +28,14 @@ func TestParseScript(t *testing.T) {
 		RawTestURL:         strings.TrimPrefix(strings.TrimSuffix(testURL, ".git"), "https://"),
 		RandomSecret:       randomString,
 	}
-	_, err = ParseScriptTemplate("scripts", info)
+	j, err := ParseScriptTemplate("scripts", info)
 	if err != nil {
 		t.Fatal(err)
+	}
+	if os.Getenv("TEST_TMPL") != "" {
+		for _, cmd := range j.Commands {
+			fmt.Println(cmd)
+		}
 	}
 
 	info.Language = "python361"
