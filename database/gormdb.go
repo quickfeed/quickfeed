ad68bccbe0394284a800d488bffa09e2de0b01d1
0xf8f8ff <0xf8f8ff@protonmail.com>
2019-08-21 14:22:30 +0200

    added enrollment preload when fetching user from the database

--- database/gormdb.go
+++ database/gormdb.go
@@ -68,7 +68,7 @@ func NewGormDB(driver, path string, logger GormLogger) (*GormDB, error) {
 // GetUser fetches a user by ID with remote identities.
 func (db *GormDB) GetUser(uid uint64) (*pb.User, error) {
 	var user pb.User
-	if err := db.conn.Preload("RemoteIdentities").First(&user, uid).Error; err != nil {
+	if err := db.conn.Preload("RemoteIdentities").Preload("Enrollments").First(&user, uid).Error; err != nil {
 		return nil, err
 	}
 	return &user, nil
