0bf7273d1581da96beb48e9578eb2706b9c22937
Hein Meling <hein.meling@uis.no>
2019-10-03 17:46:27 +0200

    fixed a few bugs in gosecret

--- cmd/gosecret/main.go
+++ cmd/gosecret/main.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"go/format"
 	"html/template"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var secretGo = `package {{ .Package }}
@@ -33,8 +35,9 @@ var secretFile = "secret_ag_test.go"
 
 func main() {
 	var (
-		agpath = flag.String("path", ".", "directory to traverse for creating "+secretFile)
-		secret = flag.String("secret", "", "secret string to insert in the "+secretFile)
+		agpath  = flag.String("path", ".", "directory to traverse for creating "+secretFile)
+		secret  = flag.String("secret", "", "secret string to insert in the "+secretFile)
+		verbose = flag.Bool("verbose", false, "be verbose")
 	)
 	flag.Parse()
 
@@ -47,7 +50,12 @@ func main() {
 			fmt.Printf("failure accessing a path %q: %v\n", path, err)
 			return err
 		}
+		if strings.HasPrefix(path, ".git") {
+			// don't traverse .git
+			return nil
+		}
 		if info.IsDir() && info.Name() != "." {
+			// traverse subfolders of . (but not . itself)
 			t, err := template.New("secret").Parse(secretGo)
 			if err != nil {
 				return err
@@ -58,11 +66,20 @@ func main() {
 				return err
 			}
 
-			err = ioutil.WriteFile(filepath.Join(path, secretFile), buffer.Bytes(), 0644)
+			// format secret_ag_test.go to be canonical Go format
+			formattedBuf, err := format.Source(buffer.Bytes())
+			if err != nil {
+				fmt.Printf("error formating source code for %s: %+v", secretFile, err)
+				return err
+			}
+
+			err = ioutil.WriteFile(filepath.Join(path, secretFile), formattedBuf, 0644)
 			if err != nil {
 				return err
 			}
-			fmt.Printf("created: %s\n", filepath.Join(path, secretFile))
+			if *verbose {
+				fmt.Printf("created: %s\n", filepath.Join(path, secretFile))
+			}
 		}
 		return nil
 	})
