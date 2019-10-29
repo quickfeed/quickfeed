c2bcab03129489d86f68b9f7201d01f0b52e2b5b
0xf8f8ff <0xf8f8ff@protonmail.com>
2019-10-29 15:56:36 +0100

    replaced temporary fix for group submissions with group membership check

--- web/autograder_service.go
+++ web/autograder_service.go
@@ -413,10 +413,15 @@ func (s *AutograderService) GetSubmissions(ctx context.Context, in *pb.Submissio
 		s.logger.Errorf("GetSubmissions failed: authentication error: %w", err)
 		return nil, ErrInvalidUserInfo
 	}
+
+	// even if there is no group ID in request and the grp value is nil, checking whether it contains the current user will just return false
+	grp, _ := s.getGroup(&pb.RecordRequest{ID: in.GetGroupID()})
+	s.logger.Debugf("Checking group user: %v+", grp.Contains(usr))
+
 	// ensure that current user is teacher or the current user is owner of the submission request
 	if !s.hasCourseAccess(usr.GetID(), in.GetCourseID(), func(e *pb.Enrollment) bool {
-		return e.Status == pb.Enrollment_TEACHER || usr.GetIsAdmin() || // TODO: remove isadmin after testing stage
-			(e.Status == pb.Enrollment_STUDENT && (usr.IsOwner(in.GetUserID()) || in.GetGroupID() > 0)) //TODO(meling): The Group check should be more precise; check that usr is in group.
+		return e.Status == pb.Enrollment_TEACHER || usr.GetIsAdmin() ||
+			(e.Status == pb.Enrollment_STUDENT && (usr.IsOwner(in.GetUserID()) || grp.Contains(usr)))
 	}) {
 		s.logger.Error("GetSubmissions failed: user is not teacher or submission author")
 		return nil, status.Errorf(codes.PermissionDenied, "only owner and teachers can get submissions")
