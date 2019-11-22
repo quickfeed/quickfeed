6612d34d01058db545dea0b722a8e07226cfc054
0xf8f8ff <0xf8f8ff@protonmail.com>
2019-11-22 11:33:31 +0100

    student or group repo will be fetched more carefully when rebuilding submission

--- web/rebuild.go
+++ web/rebuild.go
@@ -8,19 +8,41 @@ import (
 	pb "github.com/autograde/aguis/ag"
 )
 
-func (s *AutograderService) rebuildSubmission(ctx context.Context, submissionID uint64) error {
+func (s *AutograderService) rebuildSubmission(ctx context.Context, request *pb.LabRequest) error {
 
-	// get the latest submission from the database
-	submission, err := s.db.GetSubmission(&ag.Submission{ID: submissionID})
+	lab, err := s.db.GetAssignment(&pb.Assignment{ID: request.GetAssignmentID()})
 	if err != nil {
 		return err
 	}
 
-	// get user repo for the student
-	repos, err := s.db.GetRepositories(&ag.Repository{UserID: submission.GetUserID(), RepoType: pb.Repository_USER})
+	course, err := s.db.GetCourse(lab.GetCourseID(), false)
 	if err != nil {
 		return err
 	}
+
+	submission, err := s.db.GetSubmission(&pb.Submission{ID: request.GetAssignmentID(), AssignmentID: request.GetAssignmentID()})
+	if err != nil {
+		return err
+	}
+
+	repos := make([]*pb.Repository, 0)
+	if lab.IsGroupLab {
+		repos, err = s.db.GetRepositories(&ag.Repository{
+			OrganizationID: course.GetOrganizationID(),
+			GroupID:        submission.GetGroupID(),
+			RepoType:       pb.Repository_GROUP})
+	} else {
+		repos, err = s.db.GetRepositories(&ag.Repository{
+			OrganizationID: course.GetOrganizationID(),
+			UserID:         submission.GetUserID(),
+			RepoType:       pb.Repository_USER})
+	}
+	if err != nil {
+		return err
+	}
+
+	// TODO(vera): debugging, to be removed
+	s.logger.Debugf("Starting rebuild for user %d or group %d, lab %+v", submission.GetUserID(), submission.GetGroupID(), lab)
 
 	// it is possible to have duplicate records for the same user repo because there were no database constraints
 	// it is fixed for new records, but can be relevant for older database records
@@ -32,7 +54,7 @@ func (s *AutograderService) rebuildSubmission(ctx context.Context, submissionID 
 
 	s.logger.Info("Rebuilding user submission: repo url is: ", repo.GetHTMLURL())
 
-	runTests(s.logger, s.db, s.runner, repo, repo.GetHTMLURL(), submission.GetCommitHash(), "ci/scripts", submission.GetAssignmentID())
+	runTests(s.logger, s.db, s.runner, repo, repo.GetHTMLURL(), submission.GetCommitHash(), "ci/scripts", lab.GetID())
 
 	return nil
 }
