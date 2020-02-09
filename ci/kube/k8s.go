9faac93319bb8b9dc821bbd10697fd5ae15c7284
Hanif <mohamad.h@hotmail.no>
2020-02-09 21:34:27 +0100

    k int

--- ci/kube/k8s.go
+++ ci/kube/k8s.go
@@ -2,19 +2,21 @@ package kube
 
 import (
 	"fmt"
+	"context"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-
 	"k8s.io/client-go/util/retry"
-
-	"github.com/autograde/aguis/ci"
 
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	//corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/api/types"
+
+	"github.com/autograde/aguis/ci"
 )
 
 // K8s is an implementation of the CI interface using K8s.
@@ -23,23 +25,28 @@ type K8s struct {
 }
 
 //CreateJob runs the rescieved push from repository on the podes in our 3 nodes.
-func (k *K8s) CreateJob(d ci.Docker) {
+func (k *K8s) CreateJob(d ci.Docker, dockJob *ci.Job, ctx context.Context) {
 	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
+	check(err)
 
-	//deploymentsClient := clientset.AppsV1().Deployments("agcicd")
+	clientset, err := kubernetes.NewForConfig(config)
+	check(err)
+
+	//create a docker client to pull the image ?!
+	dockCli, err := client.NewEnvClient()
+	if err != nil {
+		return "", err
+	}
+	//pull the docker image
+	if err := pullImage(ctx, dockCli, dockJob.Image); err != nil {
+		return "", err
+	}
 
 	//create job for every push ?!
 	jobsClient := clientset.BatchV1().Jobs()
-	job := &batchv1.Job{
+	kubeJob := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "demo-job",
+			Name:      "webhook-job",
 			Namespace: "agcicd",
 		},
 		Spec: batchv1.JobSpec{
@@ -47,119 +54,42 @@ func (k *K8s) CreateJob(d ci.Docker) {
 				Spec: apiv1.PodSpec{
 					Containers: []apiv1.Container{
 						{
-							Name:  "demo",
-							Image: "myimage",
+							Name:  "webhook-job",
+							Image: dockJob.Image,
+							Command: []string{"/bin/sh", "-c", strings.Join(dockJob.Commands, "\n")}
 						},
 					},
+					RestartPolicy:    apiv1.RestartPolicyOnFailure, //necessaray to set either onfailure or ?..
 				},
 			},
 		},
 	}
 
 	fmt.Println("Creating job... ")
-	result1, err1 := jobsClient.Create(job)
-	if err != nil {
-		fmt.Println(err1)
-		panic(err1)
-	}
-	fmt.Printf("Created job %q.\n", result1)
+	result1, err := jobsClient.Create(kubeJob)
+	check(err)
+	fmt.Printf("Created job %q.\n", result1.Name)
 
 }
 
-//Result returns the result of recently push that are executed on the nodes
+//Result returns the result of recently push that are executed on the nodes ?
 func (k *K8s) Result() string {
 	result := ""
 	return result
 }
 
-//CleanUp cleans up the pods that are done with running of the recently push
-func (k *K8s) CleanUp() {}
+func pullImage(ctx context.Context, dockCli *client.Client, image string) error {
+	progress, err := dockCli.ImagePull(ctx, image, types.ImagePullOptions{})
+	check(err)
 
-func int32Ptr(i int32) *int32 { return &i }
+	defer progress.Close()
 
-//UpdateDeployment updates the deployment if some changes accours
-func (k *K8s) UpdateDeployment(lastImage string) {
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err)
-	}
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-
-	deploymentsClient := clientset.AppsV1().Deployments("agcicd")
-
-	// Update Deployment
-	fmt.Println("Updating deployment...")
-	//    Two options to Update() this Deployment:
-	//
-	//    1. Modify the "deployment" variable and call: Update(deployment).
-	//       This works like the "kubectl replace" command and it overwrites/loses changes
-	//       made by other clients between you Create() and Update() the object.
-	//    2. Modify the "result" returned by Get() and retry Update(result) until
-	//       you no longer get a conflict error. This way, you can preserve changes made
-	//       by other clients between Create() and Update(). This is implemented below
-	//			 using the retry utility package included with client-go. (RECOMMENDED)
-	//
-	// More Info:
-	// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		// Retrieve the latest version of Deployment before attempting update
-		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-
-		result, getErr := deploymentsClient.Get("agcicd", metav1.GetOptions{})
-		if getErr != nil {
-			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
-		}
-		result.Spec.Replicas = int32Ptr(1)                        // reduce replica count
-		result.Spec.Template.Spec.Containers[0].Image = lastImage // change image version
-		_, updateErr := deploymentsClient.Update(result)
-		return updateErr
-	})
-	if retryErr != nil {
-		panic(fmt.Errorf("Update failed: %v", retryErr))
-	}
-
+	_, err = io.Copy(ioutil.Discard, progress)
+	return err
 }
 
-//Deploy deploys...
-func (k *K8s) Deploy(lastImage string) *appsv1.Deployment {
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "agcicd",
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: int32Ptr(2),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "agcicd",
-				},
-			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "agcicd",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "agcicd",
-							Image: lastImage,
-							Ports: []apiv1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 8080,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+func check(err error) {
+	if err != nil {
+		panic(err)
 	}
-	return deployment
 }
