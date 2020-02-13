0180119e574386dfc32803257c22dc0fdb827d63
YSchneiter <yannic.schneiter@hsr.ch>
2020-02-13 10:06:40 +0000

    Kube_Test Added

--- ci/kube/k8s_test.go
+++ ci/kube/k8s_test.go
@@ -1,1 +1,72 @@
 package kube_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/autograde/aguis/ci"
+)
+
+var docker bool
+var host, version string
+
+func init() {
+	host = envString("DOCKER_HOST", "http://localhost:4243")
+	version = envString("DOCKER_VERSION", "1.30")
+
+	if os.Getenv("DOCKER_TESTS") != "" {
+		docker = true
+	}
+
+	cli, err := client.NewClient(host, version, nil, nil)
+	if err != nil {
+		docker = false
+	}
+	if _, err := cli.Ping(context.Background()); err != nil {
+		docker = false
+	}
+}
+
+func newDockerCI() *ci.Docker {
+	return &ci.Docker{
+		Endpoint: host,
+		Version:  version,
+	}
+}
+
+func TestK8s(t *testing.T) {
+	if !docker {
+		t.SkipNow()
+	}
+
+	const (
+		script  = `echo -n "hello world"`
+		wantOut = "hello world"
+	)
+
+	docker := newDockerCI()
+	k := &k8s{};
+	out, err := k.RunKubeJob(context.Background(),
+	&ci.Job{
+		Image:    "golang:1.12.8",
+		Commands: []string{script},
+	}
+	, "")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if out != wantOut {
+		t.Errorf("have %#v want %#v", out, wantOut)
+	}
+}
+
+func envString(env, fallback string) string {
+	e := os.Getenv(env)
+	if e == "" {
+		return fallback
+	}
+	return e
+}
