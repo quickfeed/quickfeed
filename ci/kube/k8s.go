454fc80c393a65e77881815afae3393d4cc46b8c
YSchneiter <yannic.schneiter@hsr.ch>
2020-02-17 14:19:45 +0000

    kube interface impl.

--- ci/kube/k8s.go
+++ ci/kube/k8s.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,6 +16,7 @@ import (
 
 	batchv1 "k8s.io/api/batch/v1"
 	apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/autograde/aguis/ci"
@@ -25,12 +27,16 @@ type K8s struct {
 	Endpoint string
 }
 
+var done bool
+
 func int32Ptr(i int32) *int32 { return &i }
+func int64Ptr(i int64) *int64 { return &i }
 
 //CreateJob runs the rescieved push from repository on the podes in our 3 nodes.
 //dockJob is the container that will be creted using the base client docker image and commands that will run.
 //id is a unique string for each job object
 func (k *K8s) RunKubeJob(ctx context.Context, dockJob *ci.Job, id string) (string, error) {
+	done = false
 	var kubeconfig *string
 	if home := homeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -85,18 +91,45 @@ func (k *K8s) RunKubeJob(ctx context.Context, dockJob *ci.Job, id string) (strin
 	}
 
 	logs := ""
-	pods, err := clientset.CoreV1().Pods("agcicd").List(metav1.ListOptions{}) // TODO: does it find correct pod on correct job?
+	//podName := ""
+	pods, err := clientset.CoreV1().Pods("agcicd").List(metav1.ListOptions{}) // TODO:make it spesific for the jobs
 	fmt.Println(len(pods.Items))
 	for _, pod := range pods.Items {
-		fmt.Println(pod.Status)
-		//logs += k.PodLogs(pod, clientset)
-		fmt.Println("logs : " + logs)
+		k.PodeEvents(pod, *clientset, "agcicd")
+		if done == true {
+			logs = k.PodLogs(pod, clientset)
+			//podName = pod.Name
+		}
 	}
+
+	/*	err = clientset.CoreV1().Pods("agcicd").Delete(pod.Name, &metav1.DeleteOptions{GracePeriodSeconds: int64Ptr(40)})
+			if err != nil {
+				return "", err
+			}
+
+		err = jobsClient.Delete(kubeJob.GetName(), &metav1.DeleteOptions{GracePeriodSeconds: int64Ptr(30)})
+		if err != nil {
+			return "", err
+		}*/
+
+	fmt.Println("logs : " + logs)
 
 	return logs, nil
 }
 
-//PodLogs returns the result of recently push that are executed on the nodes ?
+//DeleteObject deleting job and pod after success
+func (k *K8s) DeleteObject(pod apiv1.Pod, clientset kubernetes.Clientset, namespace string, id string) {
+	err := clientset.CoreV1().Pods("agcicd").Delete(pod.Name, &metav1.DeleteOptions{GracePeriodSeconds: int64Ptr(40)})
+	if err != nil {
+		panic(err)
+	}
+	err = clientset.BatchV1().Jobs(namespace).Delete("cijob"+id, &metav1.DeleteOptions{GracePeriodSeconds: int64Ptr(30)})
+	if err != nil {
+		panic(err)
+	}
+}
+
+//PodLogs returns the result of recently push that are executed on the nodes
 func (k *K8s) PodLogs(pod apiv1.Pod, clientset *kubernetes.Clientset) string {
 	//delete ?
 	podLogOpts := apiv1.PodLogOptions{}
@@ -115,6 +148,34 @@ func (k *K8s) PodLogs(pod apiv1.Pod, clientset *kubernetes.Clientset) string {
 	}
 	str := buf.String()
 	return str
+}
+
+//PodeEvents is a method that watch the pods states
+func (k *K8s) PodeEvents(pod apiv1.Pod, clientset kubernetes.Clientset, namespace string) {
+	//name := pod.GetName()
+	watch, err := clientset.CoreV1().Pods(namespace).Watch(metav1.ListOptions{})
+	if err != nil {
+		fmt.Println("nothing to watch")
+		log.Fatal(err.Error())
+	}
+	//go func() {
+	for event := range watch.ResultChan() {
+		fmt.Printf("Type: %v\n", event.Type)
+		p, ok := event.Object.(*v1.Pod)
+		if !ok {
+			log.Fatal("unexpected type")
+		}
+		if p.Status.Phase == apiv1.PodSucceeded {
+			fmt.Println("notify succ..")
+			done = true
+			break
+		}
+		if p.Status.Phase == apiv1.PodFailed {
+			fmt.Println("POD FAILED") //TODO: What to do? delete and run the job again?
+			break
+		}
+	}
+	//}()
 }
 
 func homeDir() string {
