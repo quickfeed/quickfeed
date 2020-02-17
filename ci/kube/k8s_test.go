997ef33cc10316d18580b34cdae741f90fda48bb
YSchneiter <yannic.schneiter@hsr.ch>
2020-02-17 19:37:44 +0100

    test paralell Kube

--- ci/kube/k8s_test.go
+++ ci/kube/k8s_test.go
@@ -2,6 +2,7 @@ package kube_test
 
 import (
 	"context"
+	"strconv"
 	"testing"
 
 	"github.com/autograde/aguis/ci"
@@ -16,6 +17,17 @@ func init() {
 
 func newKubeCI() *kube.K8s {
 	return &kube.K8s{}
+}
+
+func newTest(script, wantOut string) *test {
+	t := &test{}
+	t.script = script
+	t.wantOut = wantOut
+	return t
+}
+
+type test struct {
+	script, wantOut, out string
 }
 
 func TestK8s(t *testing.T) {
@@ -39,4 +51,38 @@ func TestK8s(t *testing.T) {
 	if out != wantOut {
 		t.Errorf("have %#v want %#v", out, wantOut)
 	}
+}
+
+func TestParalellK8s(t *testing.T) {
+	numberOfPods := 10
+	tests := make([]test, numberOfPods)
+
+	for i := 0; i < numberOfPods; i++ {
+		t := newTest(`echo -n "`+strconv.Itoa(i)+`"`, strconv.Itoa(i))
+		tests[i] = *t
+	}
+
+	for i := 0; i < numberOfPods; i++ {
+		tst := tests[i]
+		k := newKubeCI()
+		out, err := k.RunKubeJob(context.Background(),
+			&ci.Job{
+				Image:    "golang",
+				Commands: []string{tst.script},
+			},
+			"")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		tst.out = out
+	}
+
+	for i := 0; i < numberOfPods; i++ {
+		tst := tests[i]
+		if tst.out != tst.wantOut {
+			t.Errorf("have %#v want %#v", tst.out, tst.wantOut)
+		}
+	}
+
 }
