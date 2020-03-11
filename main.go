ba7ccd3a3675672a83f938b4d102519ff5daaa7d
0xf8f8ff <0xf8f8ff@protonmail.com>
2020-03-11 12:15:33 +0100

    methods with names count info sent from interceptor

--- main.go
+++ main.go
@@ -60,18 +60,12 @@ var (
 
 	// Create some standard server metrics.
 	grpcMetrics = grpc_prometheus.NewServerMetrics()
-
-	// Create a customized counter metric.
-	customizedCounterMetric = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "demo_server_say_hello_method_handle_count",
-		Help: "Total number of RPCs handled on the server.",
-	}, []string{"name"})
 )
 
 func init() {
 	// Register standard server metrics and customized metrics to registry.
-	reg.MustRegister(grpcMetrics, customizedCounterMetric)
-	customizedCounterMetric.WithLabelValues("Test")
+	reg.MustRegister(grpcMetrics, pb.CustomizedCounterMetric)
+	pb.CustomizedCounterMetric.WithLabelValues("Test")
 }
 
 func main() {
