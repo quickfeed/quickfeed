c1589ed98a202d24425230e6504d22ec9b4be3bf
Hanif <mohamad.h@hotmail.no>
2020-03-27 23:41:24 +0100

    merge

--- ci/kube/k8s_test.go
+++ ci/kube/k8s_test.go
@@ -69,7 +69,6 @@ func TestK8sFP(t *testing.T) {
 	cloneURL := "https://github.com/dat320-2019/assignments.git"
 	getURLTest := "https://github.com/dat320-2019/tests.git"
 
-<<<<<<< HEAD
 	sec := "59fd5fe1c4f741604c1beeab875b9c789d2a7c73"
 	jobName := "jobName"
 	err := kube.Jobsecrets(jobName, "agcicd", sec)
@@ -77,10 +76,6 @@ func TestK8sFP(t *testing.T) {
 		panic(err)
 	}
 	//jobName := tea.Format("20060102-150405")
-=======
-	//jobName := tea.Format("20060102-150405")
-	jobName := "jobname"
->>>>>>> 389b2143a9f5a976e6605b0e0aa4aa9c6af6982d
 	ass := &kube.AssignmentInfo{
 		AssignmentName:     "lab5",
 		Language:           "go",
@@ -116,60 +111,3 @@ func TestK8sFP(t *testing.T) {
 	}
 	fmt.Println(time.Since(tea))
 }
-<<<<<<< HEAD
-=======
-
-func TestK8sFPSecret(t *testing.T) {
-	tea := time.Now()
-	fmt.Println(tea.Format("20060102-150405"))
-	cloneURL := "https://github.com/dat320-2019/assignments.git"
-	getURLTest := "https://github.com/dat320-2019/tests.git"
-
-	//jobName := tea.Format("20060102-150405")
-	jobName := "jobname"
-
-	ass := &kube.AssignmentInfo{
-		AssignmentName:     "lab5",
-		Language:           "go",
-		CreatorAccessToken: "dfacdda59b10863f14a712e436a6d9ebfe9c299e",
-		GetURL:             cloneURL,
-		TestURL:            getURLTest,
-		RawGetURL:          strings.TrimPrefix(strings.TrimSuffix(cloneURL, ".git"), "https://"),
-		RawTestURL:         strings.TrimPrefix(strings.TrimSuffix(getURLTest, ".git"), "https://"),
-		RandomSecret:       "59fd5fe1c4f741604c1beeab875b9c789d2a7c73", //jobName,
-	}
-	jobdock, err := kube.ParseKubeScriptTemplate("", ass) ///root/work/aguisforYannic/aguis/ci/scripts
-	if err != nil {
-		panic(err)
-	}
-	script := jobdock.Commands
-	container := &kube.Container{
-		Image:    "golang",
-		Commands: script,
-	}
-
-	k := newKubeCI()
-	out, err := k.KRun(context.Background(), container, jobName, "agcicd", "59fd5fe1c4f741604c1beeab875b9c789d2a7c73")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	res, err := kube.ExtractKubeResult(zap.NewNop().Sugar(), out, "59fd5fe1c4f741604c1beeab875b9c789d2a7c73", 10)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if strings.Contains(res.BuildInfo.BuildLog, "59fd5fe1c4f741604c1beeab875b9c789d2a7c73") {
-		fmt.Println(out)
-		t.Fatal("build log contains secret")
-		t.Logf("res %+v", res.BuildInfo)
-	}
-	fmt.Println(out)
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
-}
->>>>>>> 389b2143a9f5a976e6605b0e0aa4aa9c6af6982d
