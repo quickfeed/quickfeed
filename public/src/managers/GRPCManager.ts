395e556f6b249797f1cde5a9d065d3b760c7e106
0xf8f8ff <0xf8f8ff@protonmail.com>
2020-04-17 09:38:53 +0200

    fixed a wrong reference that somehow ended up in the client code

--- public/src/managers/GRPCManager.ts
+++ public/src/managers/GRPCManager.ts
@@ -127,7 +127,7 @@ export class GrpcManager {
 
         const request = new EnrollmentRequest();
         request.setCourseid(courseID);
-        request.setFilteroutgroupmembers(noGroupMembers ?? false);
+        request.setIgnoregroupmembers(noGroupMembers ?? false);
         request.setStatusesList(statuses ?? []);
         return this.grpcSend<Enrollments>(this.agService.getEnrollmentsByCourse, request);
     }
