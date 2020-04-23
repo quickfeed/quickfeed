fb9760ae1b0953793c25677031700122b9d4149c
Hein Meling <hein.meling@uis.no>
2020-04-23 22:11:00 +0200

    moved slipday sketch to new file slipdays.go

--- ci/run_tests.go
+++ ci/run_tests.go
@@ -61,22 +61,6 @@ func RunTests(logger *zap.SugaredLogger, db database.Database, runner Runner, rD
 		logger.Errorf("Failed to extract results from log: %w", err)
 		return
 	}
-
-	// TODO(meling) move this code to separate file and function: slipdays.go
-	// TODO(meling) write tests for this stuff first
-	//TODO(meling) must also handle groups; if group assignment is late; withdraw one slip day per group member.
-	timeUntilDeadline := rData.Assignment.DurationUntilDeadline(start)
-	if timeUntilDeadline < 0 {
-		// TODO(meling) implement steps below
-		// get course slip days from database
-		// get user's current slip day count from database
-		// check whether the user has already exceeded or will exceed the course maximum
-		// update slip days for user in database
-		rData.Repo.GetUserID()
-	}
-
-	//TODO(meling) also need to propogate slip day information to frontend to show to user
-
 	recordResults(logger, db, rData, result)
 }
 
