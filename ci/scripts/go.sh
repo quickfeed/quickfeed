af99d2bfe2ab4b7440a6573a461d345a262eb0ae
Hein Meling <hein.meling@uis.no>
2020-07-20 18:07:15 +0200

    renamed scripts s/tmpl/sh/ and revised go.sh significantly
    The new go.sh script does away with gosecret, adds timing output,
    and fixes issue #273 (@EivindSt please confirm). Further, it also
    cleans up the script a bit, using variables to ensure consistent
    paths are used.
    
    The other tmpl scripts have not been updated.

--- ci/scripts/go.sh
+++ ci/scripts/go.sh
@@ -1,28 +1,25 @@
-#image/golang:latest
+#image/go-quickfeed:latest
 
+start=$SECONDS
 echo "*** Preparing for Test Execution ***\n"
-# echo "Current dir: `pwd`"
-# echo "Path: $PATH"
-
-# create bin dir (silently, if already exists)
-mkdir -p /go/bin
-# fetch gosecret binary (This is a HACK)
-wget -q http://www.ux.uis.no/~meling/tmp/gosecret -P /go/bin
-chmod 700 /go/bin/gosecret
 
 git config --global url."https://{{ .CreatorAccessToken }}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 
-# Fetch student repo
-git clone {{ .GetURL }} assignments
-# Fetch tests repo
-git clone {{ .TestURL }}
+ASSIGNMENTS=/quickfeed/assignments
+TESTDIR=/quickfeed/tests
+ASSIGNDIR=$ASSIGNMENTS/{{ .AssignmentName }}/
 
-# Run gosecret for all tests
-cd /go/tests
-/go/bin/gosecret -secret {{ .RandomSecret }}
+# Fetch student and test repos
+git clone {{ .GetURL }} $ASSIGNMENTS
+git clone {{ .TestURL }} $TESTDIR
+
+if [ ! -d "$ASSIGNDIR" ]; then
+  echo "No code to test for {{ .GetURL }}/{{ .AssignmentName }}"
+  exit
+fi
 
 # Move to folder for assignment to test.
-cd /go/assignments/{{ .AssignmentName }}/
+cd $ASSIGNDIR
 
 # Fail student code that attempts to access secret
 if grep -r -e common.Secret -e GlobalSecret * ; then
@@ -30,31 +27,32 @@ if grep -r -e common.Secret -e GlobalSecret * ; then
   exit
 fi
 
-# Make sure there are no Autograder tests in the student repo
-rm $(find ./ | grep _ag_test.go)
+# Remove student written tests to avoid interference
+rm -rf *_test.go
+rm -f setup.sh
 
-# echo "Removing `pwd`/go.mod, if any"
-rm -f go.mod
+# Run gosecret for all tests
+cd $TESTDIR
+bin/gosecret -secret {{ .RandomSecret }}
 
 # Copy tests into student assignments folder for running tests
-cp -r /go/tests/* /go/assignments/
+cp -r $TESTDIR/* $ASSIGNMENTS/
 
 # Clear access token and the shell history to avoid leaking information to student test code.
 git config --global url."https://0:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 history -c
 
 # (ensure) Move to folder for assignment to test.
-cd /go/assignments/{{ .AssignmentName }}/
+cd $ASSIGNDIR
 
 # Perform lab specific setup
 if [ -f "setup.sh" ]; then
     bash setup.sh
 fi
 
-# Avoid fetching dependencies from proxy.
-export GOPROXY=direct
+echo "\n*** Finished Test Setup in $(( SECONDS - start )) seconds ***\n"
 
+start=$SECONDS
 echo "\n*** Running Tests ***\n"
 go test -v -timeout 30s ./... 2>&1
-echo "\n*** Finished Running Tests ***\n"
-
+echo "\n*** Finished Running Tests in $(( SECONDS - start )) seconds ***\n"
