a880fbeea628252786907d81fb27ead232854b1a
Hein Meling <hein.meling@uis.no>
2020-08-07 15:26:54 +0200

    added test for commandline

---
+++ kit/exercise/command_line_test.go
@@ -0,0 +1,31 @@
+package exercise
+
+import (
+	"testing"
+
+	"github.com/autograde/quickfeed/kit/score"
+)
+
+func TestCmdLine(t *testing.T) {
+	// TODO(meling) the following works, but doesn't exercise the test failure
+	// cmds := Commands{
+	// 	{Command: "ls -l", Result: "command_line.go", Search: ResultContains},
+	// 	{Command: "ls -a", Result: "command_line.go", Search: ResultContains},
+	// }
+	// The following is expected to fail:
+	cmds := Commands{
+		{Command: "ls -l", Result: "command_line.go", Search: ResultDoesNotContain},
+		{Command: "ls -a", Result: "command_line.go", Search: ResultDoesNotContain},
+		{Command: "obviouslyDoesntWork", Result: "works", Search: ResultEquals},
+	}
+	sc := score.NewScoreMax(10, 1)
+	outs := CommandLine(t, sc, cmds)
+	for i := 0; i < len(cmds); i++ {
+		t.Logf("stdout: %s", outs[i].StdOut())
+		t.Logf("stderr: %s", outs[i].StdErr())
+		if outs[i].err == nil {
+			t.Logf("STEP %d: ERROR IS NIL", i)
+		}
+		t.Logf("err: %v", outs[i].Error())
+	}
+}
