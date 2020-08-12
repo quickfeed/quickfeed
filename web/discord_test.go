56ebe379a9cbf5461977ba79a2d4e6a5d0332868
0xf8f8ff <0xf8f8ff@protonmail.com>
2020-08-12 10:59:28 +0200

    skip the test if user ID not set

--- web/discord_test.go
+++ web/discord_test.go
@@ -20,6 +20,11 @@ import (
 // The user has to have an active record in the database and either be an admin
 // or a teacher of the requested course.
 func TestDiscordClient(t *testing.T) {
+	currentUserID := os.Getenv("DISCORD_USER")
+	if currentUserID == "" {
+		t.Skip("This test requires a 'DISCORD_USER' environmental variable with a valid ID of a registered user")
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	conn, err := grpc.DialContext(ctx, ":9090", grpc.WithInsecure(), grpc.WithBlock())
@@ -30,10 +35,6 @@ func TestDiscordClient(t *testing.T) {
 
 	client := pb.NewAutograderServiceClient(conn)
 
-	currentUserID := os.Getenv("DISCORD_USER")
-	if currentUserID == "" {
-		t.Skip("This test requires a 'DISCORD_USER' environmental variable with a valid ID of a registered user")
-	}
 	convertedID := strings.TrimSpace(currentUserID)
 	requestMetadata := metadata.New(map[string]string{"user": convertedID})
 	requestContext := metadata.NewOutgoingContext(context.Background(), requestMetadata)
