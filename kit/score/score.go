b5f5664ac2c37d8f4cc429fe856766ab7995cf90
Hein Meling <hein.meling@uis.no>
2021-01-25 23:40:01 +0100

    Deprecated NewScore and Write functions

--- kit/score/score.go
+++ kit/score/score.go
@@ -3,43 +3,10 @@ package score
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"runtime/debug"
 	"strings"
 	"testing"
 )
-
-// NewScore returns a new Score object with the given max and weight.
-// The Score is initially 0, and Inc() and IncBy() can be called
-// on the returned Score object, for each test that passes.
-// The TestName is initialized to the name of the provided t.Name().
-// This function also prints a JSON representation of the Score object
-// to ensure that the test is recorded by Quickfeed.
-func NewScore(t *testing.T, max, weight int) *Score {
-	sc := &Score{
-		Secret:   sessionSecret,
-		TestName: t.Name(),
-		MaxScore: int32(max),
-		Weight:   int32(weight),
-	}
-	// prints JSON score object with zero score, e.g.:
-	// {"Secret":"my secret code","TestName":"TestPanicHandler","Score":0,"MaxScore":8,"Weight":5}
-	// This registers the test, in case a panic occurs that prevents printing the score object.
-	fmt.Println(sc.json())
-	return sc
-}
-
-// NewScoreMax returns a new Score object with the given max and weight.
-// The Score is initially set to max, and Dec() and DecBy() can be called
-// on the returned Score object, for each test that fails.
-// The TestName is initialized as the name of the provided t.Name().
-// This function also prints a JSON representation of the Score object
-// to ensure that the test is recorded by Quickfeed.
-func NewScoreMax(t *testing.T, max, weight int) *Score {
-	sc := NewScore(t, max, weight)
-	sc.Score = int32(max)
-	return sc
-}
 
 // Fail sets Score to zero.
 func (s *Score) Fail() {
@@ -81,40 +48,18 @@ func (s *Score) DecBy(n int) {
 }
 
 // Equal returns true if sc equals other. Ignores the Secret field.
-func (sc *Score) Equal(other *Score) bool {
+func (s *Score) Equal(other *Score) bool {
 	return other != nil &&
-		sc.TestName == other.TestName &&
-		sc.Score == other.Score &&
-		sc.MaxScore == other.MaxScore &&
-		sc.Weight == other.Weight
+		s.TestName == other.TestName &&
+		s.Score == other.Score &&
+		s.MaxScore == other.MaxScore &&
+		s.Weight == other.Weight
 }
 
-// String returns a string representation of the score.
-// Format: "TestName: 2/10 test cases passed".
-func (s *Score) StringOld() string {
+// PassedTests returns a string with the following format:
+// "TestName: 2/10 test cases passed".
+func (s *Score) PassedTests() string {
 	return fmt.Sprintf("%s: %d/%d test cases passed", s.TestName, s.Score, s.MaxScore)
-}
-
-// WriteString writes the string representation of s to w.
-// Deprecated: Do not use this function; it will be removed in the future.
-// Use Print() instead to replace both WriteString() and WriteJSON().
-func (s *Score) WriteString(w io.Writer) {
-	if r := recover(); r != nil {
-		s.Score = 0
-		fmt.Fprintf(w, "******************\n%s panicked:\n%s\n******************\n", s.TestName, r)
-	}
-	fmt.Fprintf(w, "%v\n", s)
-}
-
-// WriteJSON writes the JSON representation of s to w.
-// Deprecated: Do not use this function; it will be removed in the future.
-// Use Print() instead to replace both WriteString() and WriteJSON().
-func (s *Score) WriteJSON(w io.Writer) {
-	if r := recover(); r != nil {
-		s.Score = 0
-		fmt.Fprintf(w, "******************\n%s panicked:\n%s\n******************\n", s.TestName, r)
-	}
-	fmt.Fprintf(w, "\n%s\n", s.json())
 }
 
 // Print prints both the JSON secret string and emits the number of test cases passed.
