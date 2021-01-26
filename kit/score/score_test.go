29a0af33ed9fda09471134d9a215820eebbd4c27
Hein Meling <hein.meling@uis.no>
2021-01-26 20:31:28 +0100

    Added TestNormalize()

--- kit/score/score_test.go
+++ kit/score/score_test.go
@@ -11,11 +11,24 @@ func TestRelativeScore(t *testing.T) {
 		TestName: t.Name(),
 		MaxScore: 10,
 		Score:    3,
-		Weight:   10,
 	}
 	rs := sc.RelativeScore()
 	expectedRS := t.Name() + ": score = 3/10 = 0.3"
 	if rs != expectedRS {
-		t.Errorf("RelativeScore()='%s', expected '%s'", rs, expectedRS)
+		t.Errorf(`RelativeScore() = %q, expected %q`, rs, expectedRS)
+	}
+}
+
+func TestNormalize(t *testing.T) {
+	sc := &score.Score{
+		TestName: t.Name(),
+		MaxScore: 100,
+		Score:    33,
+	}
+	newMaxScore := 50
+	sc.Normalize(newMaxScore)
+	expectedScore := int32(17)
+	if sc.Score != expectedScore {
+		t.Errorf("Normalize(%d) = %d, expected %d", newMaxScore, sc.Score, expectedScore)
 	}
 }
