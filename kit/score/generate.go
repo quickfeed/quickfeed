72af583a861f4c552316079e832779e421ded05f
Hein Meling <hein.meling@uis.no>
2021-01-27 22:39:45 +0100

    Removed unused gRPC code

--- kit/score/generate.go
+++ kit/score/generate.go
@@ -1,2 +1,2 @@
-//go:generate protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. score.proto
+//go:generate protoc --go_out=paths=source_relative:. score.proto
 package score
