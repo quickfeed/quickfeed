6e0289ec0903ba15b5f72c1d67245193ed495a55
Hein Meling <hein.meling@uis.no>
2021-02-20 17:48:16 +0100

    Added IsValid() function for sanity checking scores
    IsValid() checks that the parsed score object is valid.

--- kit/score/parse.go
+++ kit/score/parse.go
@@ -6,12 +6,20 @@ import (
 	"strings"
 )
 
-// hiddenSecret is used to replace the global secret when parsing.
-var hiddenSecret = "hidden"
-
 // ErrScoreNotFound is returned if the parse string did not contain a
 // JSON score string.
 var ErrScoreNotFound = errors.New("score not found in string")
+
+const (
+	ErrScoreInterval = "Score must be in the interval [0, MaxScore]"
+	ErrMaxScore      = "MaxScore must be greater than 0"
+	ErrWeight        = "Weight must be greater than 0"
+	ErrEmptyTestName = "TestName must be specified"
+	ErrSecret        = "Secret field must match expected secret"
+)
+
+// hiddenSecret is used to replace the global secret when parsing.
+const hiddenSecret = "hidden"
 
 // Parse returns a score object for the provided JSON string s
 // which contains secret.
@@ -20,8 +28,9 @@ func Parse(s, secret string) (*Score, error) {
 		var sc Score
 		err := json.Unmarshal([]byte(s), &sc)
 		if err == nil {
-			if sc.Secret == secret {
-				sc.Secret = hiddenSecret // overwrite secret
+			err = sc.IsValid(secret)
+			if err != nil {
+				return nil, err
 			}
 			return &sc, nil
 		}
@@ -32,6 +41,31 @@ func Parse(s, secret string) (*Score, error) {
 		return nil, err
 	}
 	return nil, ErrScoreNotFound
+}
+
+// IsValid returns an error if the score object is invalid.
+// Otherwise, nil is returned.
+// If the given secret matches the score's secret value,
+// the Secret field is overwritten with the string "hidden".
+func (sc *Score) IsValid(secret string) error {
+	tName := sc.GetTestName()
+	if tName == "" {
+		return errMsg("", ErrEmptyTestName)
+	}
+	if sc.MaxScore <= 0 {
+		return errMsg(tName, ErrMaxScore)
+	}
+	if sc.Weight <= 0 {
+		return errMsg(tName, ErrWeight)
+	}
+	if sc.Score < 0 || sc.Score > sc.MaxScore {
+		return errMsg(tName, ErrScoreInterval)
+	}
+	if sc.Secret != secret {
+		return errMsg(tName, ErrSecret)
+	}
+	sc.Secret = hiddenSecret // overwrite secret
+	return nil
 }
 
 // HasPrefix returns true if the provided string s has a parsable prefix string.
--- kit/score/parse_test.go
+++ kit/score/parse_test.go
@@ -1,6 +1,7 @@
 package score_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/autograde/quickfeed/kit/score"
@@ -64,5 +65,86 @@ func TestParseJSONStrings(t *testing.T) {
 				t.Errorf("Parse function failed to hide global secret: %v", sc.Secret)
 			}
 		}
+	}
+}
+
+var scoreValidTests = []struct {
+	name string
+	in   []*score.Score
+	want string
+}{
+	{
+		name: "EmptyTestName",
+		in: []*score.Score{
+			{TestName: "", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 0},
+		},
+		want: score.ErrEmptyTestName,
+	},
+	{
+		name: "BadWeights",
+		in: []*score.Score{
+			{TestName: "BadWeights", Secret: theSecret, Weight: 0, MaxScore: 100, Score: 0},
+			{TestName: "BadWeights", Secret: theSecret, Weight: -10, MaxScore: 100, Score: 0},
+			{TestName: "BadWeights", Secret: theSecret, Weight: -1, MaxScore: 100, Score: 0},
+		},
+		want: score.ErrWeight,
+	},
+	{
+		name: "BadMaxScore",
+		in: []*score.Score{
+			{TestName: "BadMaxScore", Secret: theSecret, Weight: 10, MaxScore: 0, Score: 0},
+			{TestName: "BadMaxScore", Secret: theSecret, Weight: 10, MaxScore: -100, Score: 0},
+			{TestName: "BadMaxScore", Secret: theSecret, Weight: 10, MaxScore: -1, Score: 0},
+		},
+		want: score.ErrMaxScore,
+	},
+	{
+		name: "BadScore",
+		in: []*score.Score{
+			{TestName: "BadScore", Secret: theSecret, Weight: 10, MaxScore: 100, Score: -1},
+			{TestName: "BadScore", Secret: theSecret, Weight: 10, MaxScore: 100, Score: -20},
+			{TestName: "BadScore", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 101},
+			{TestName: "BadScore", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 1000},
+		},
+		want: score.ErrScoreInterval,
+	},
+	{
+		name: "BadSecret",
+		in: []*score.Score{
+			{TestName: "BadSecret", Secret: "xyz", Weight: 10, MaxScore: 100, Score: 0},
+		},
+		want: score.ErrSecret,
+	},
+	{
+		name: "GoodScore",
+		in: []*score.Score{
+			{TestName: "GoodScoreW", Secret: theSecret, Weight: 1, MaxScore: 100, Score: 0},
+			{TestName: "GoodScoreW", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 0},
+			{TestName: "GoodScoreW", Secret: theSecret, Weight: 100, MaxScore: 100, Score: 0},
+			{TestName: "GoodScoreM", Secret: theSecret, Weight: 10, MaxScore: 1, Score: 0},
+			{TestName: "GoodScoreM", Secret: theSecret, Weight: 10, MaxScore: 10, Score: 0},
+			{TestName: "GoodScoreM", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 0},
+			{TestName: "GoodScoreS", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 10},
+			{TestName: "GoodScoreS", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 50},
+			{TestName: "GoodScoreS", Secret: theSecret, Weight: 10, MaxScore: 100, Score: 100},
+		},
+		want: "nil",
+	},
+}
+
+func TestScoreIsValid(t *testing.T) {
+	for _, test := range scoreValidTests {
+		t.Run(test.name, func(t *testing.T) {
+			for _, sc := range test.in {
+				err := sc.IsValid(theSecret)
+				if err != nil {
+					if !strings.Contains(err.Error(), test.want) {
+						t.Errorf("IsValid(%q) = %v, expected = %v", sc, err, test.want)
+					}
+				} else if test.want != "nil" {
+					t.Errorf("IsValid(%q) = %v, expected = %v", sc, err, test.want)
+				}
+			}
+		})
 	}
 }
