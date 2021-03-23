d26eb99d9c50157762dbfa604a96b374047d8dec
Oskar Gjølga <os.gjolga@stud.uis.no>
2021-03-23 12:57:44 +0100

    skipbuildinfo field

--- public/proto/ag_pb.js
+++ public/proto/ag_pb.js
@@ -2,11 +2,14 @@
 /**
  * @fileoverview
  * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
  * @suppress {messageConventions} JS Compiler reports an error if a variable or
  *     field starts with 'MSG_' and isn't a translatable message.
  * @public
  */
 // GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
 
 var jspb = require('google-protobuf');
 var goog = jspb;
@@ -9321,8 +9324,10 @@ proto.EnrollmentRequest.deserializeBinaryFromReader = function(msg, reader) {
       msg.setWithactivity(value);
       break;
     case 4:
-      var value = /** @type {!Array<!proto.Enrollment.UserStatus>} */ (reader.readPackedEnum());
-      msg.setStatusesList(value);
+      var values = /** @type {!Array<!proto.Enrollment.UserStatus>} */ (reader.isDelimited() ? reader.readPackedEnum() : [reader.readEnum()]);
+      for (var i = 0; i < values.length; i++) {
+        msg.addStatuses(values[i]);
+      }
       break;
     default:
       reader.skipField();
@@ -9557,8 +9562,10 @@ proto.EnrollmentStatusRequest.deserializeBinaryFromReader = function(msg, reader
       msg.setUserid(value);
       break;
     case 2:
-      var value = /** @type {!Array<!proto.Enrollment.UserStatus>} */ (reader.readPackedEnum());
-      msg.setStatusesList(value);
+      var values = /** @type {!Array<!proto.Enrollment.UserStatus>} */ (reader.isDelimited() ? reader.readPackedEnum() : [reader.readEnum()]);
+      for (var i = 0; i < values.length; i++) {
+        msg.addStatuses(values[i]);
+      }
       break;
     default:
       reader.skipField();
@@ -10749,8 +10756,10 @@ proto.URLRequest.deserializeBinaryFromReader = function(msg, reader) {
       msg.setCourseid(value);
       break;
     case 2:
-      var value = /** @type {!Array<!proto.Repository.Type>} */ (reader.readPackedEnum());
-      msg.setRepotypesList(value);
+      var values = /** @type {!Array<!proto.Repository.Type>} */ (reader.isDelimited() ? reader.readPackedEnum() : [reader.readEnum()]);
+      for (var i = 0; i < values.length; i++) {
+        msg.addRepotypes(values[i]);
+      }
       break;
     default:
       reader.skipField();
@@ -11499,7 +11508,8 @@ proto.SubmissionsForCourseRequest.prototype.toObject = function(opt_includeInsta
 proto.SubmissionsForCourseRequest.toObject = function(includeInstance, msg) {
   var f, obj = {
     courseid: jspb.Message.getFieldWithDefault(msg, 1, 0),
-    type: jspb.Message.getFieldWithDefault(msg, 2, 0)
+    type: jspb.Message.getFieldWithDefault(msg, 2, 0),
+    skipbuildinfo: jspb.Message.getBooleanFieldWithDefault(msg, 3, false)
   };
 
   if (includeInstance) {
@@ -11544,6 +11554,10 @@ proto.SubmissionsForCourseRequest.deserializeBinaryFromReader = function(msg, re
       var value = /** @type {!proto.SubmissionsForCourseRequest.Type} */ (reader.readEnum());
       msg.setType(value);
       break;
+    case 3:
+      var value = /** @type {boolean} */ (reader.readBool());
+      msg.setSkipbuildinfo(value);
+      break;
     default:
       reader.skipField();
       break;
@@ -11584,6 +11598,13 @@ proto.SubmissionsForCourseRequest.serializeBinaryToWriter = function(message, wr
   if (f !== 0.0) {
     writer.writeEnum(
       2,
+      f
+    );
+  }
+  f = message.getSkipbuildinfo();
+  if (f) {
+    writer.writeBool(
+      3,
       f
     );
   }
@@ -11632,6 +11653,24 @@ proto.SubmissionsForCourseRequest.prototype.getType = function() {
  */
 proto.SubmissionsForCourseRequest.prototype.setType = function(value) {
   return jspb.Message.setProto3EnumField(this, 2, value);
+};
+
+
+/**
+ * optional bool skipBuildInfo = 3;
+ * @return {boolean}
+ */
+proto.SubmissionsForCourseRequest.prototype.getSkipbuildinfo = function() {
+  return /** @type {boolean} */ (jspb.Message.getBooleanFieldWithDefault(this, 3, false));
+};
+
+
+/**
+ * @param {boolean} value
+ * @return {!proto.SubmissionsForCourseRequest} returns this
+ */
+proto.SubmissionsForCourseRequest.prototype.setSkipbuildinfo = function(value) {
+  return jspb.Message.setProto3BooleanField(this, 3, value);
 };
 
 
