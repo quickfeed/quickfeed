32fb90b2fd3560d2ffb2e62f9c8e042f98028d23
plakolaki <mikael.bjerga@gmail.com>
2021-04-13 22:19:54 +0200

    Fixes issue #2. Adds ProgressBar.tsx to be used in navbar and labs.

--- public/src/components/NavBarLabs.tsx
+++ public/src/components/NavBarLabs.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { useOvermind } from "../overmind"
+import { Submission } from "../proto/ag_pb"
+import { ProgressBar } from "./ProgressBar"
 
 /* 
     This component generates links for all labs for the sidebar. 
@@ -25,23 +27,32 @@ const NavBarLabs = () => {
 
     const labs = (): JSX.Element[] => { 
         
-        if(state.submissions !== undefined && state.activeCourse > 0) {
-            let links = state.submissions[state.activeCourse]?.map((submission, index) => {
-                let assignment = state.assignments[state.activeCourse][index]
+        if(state.assignments[state.activeCourse] !== undefined && state.activeCourse > 0) {
+            let links = state.assignments[state.activeCourse]?.map((assignment, index) => {
                 let rand = Math.random()
-                let percentage = 100 - (submission.getScore() - rand * 100)
+                let percentage = 0
+                let score = 0
+                if (state.submissions[state.activeCourse][index] !== undefined) {
+                    let submission = state.submissions[state.activeCourse][index]
+                    percentage = 100 - (submission.getScore() - rand * 100)
+                    score = submission.getScore() - rand * 100
+                }
+
                 return (
-                    <li style={{position: "relative"}} className={active === index && state.activeCourse === assignment.getCourseid() ? "active" : ""} key={submission.getAssignmentid()} onClick={() => {setActive(index)}}>
+                    <li style={{position: "relative"}} className={active === index && state.activeCourse === assignment.getCourseid() ? "active" : ""} key={assignment.getId()} onClick={() => {setActive(index)}}>
                         <div id="icon">
                             <i className={assignment.getIsgrouplab() ? "fa fa-users" : "fa fa-user"} title={assignment.getIsgrouplab() ? "Group Lab" : "Individual Lab"}></i>
                         </div>
                         <div id="title">
-                            <Link to={`/course/${state.activeCourse}/${submission.getAssignmentid()}`}>{assignment.getName()}</Link>
+                            <Link to={`/course/${state.activeCourse}/${assignment.getId()}`}>{assignment.getName()}</Link>
                         </div> 
                         
                         {/** The following code adds a "progress" bar below the lab in the sidebar to indicate how many % done a user is with a lab where the percentage is (100 - submission.getScore()).
-                         * if the score is above the assignment score limit, the bar turns green, else it is yellow. //TODO Should look into de-spaghettifying this */ }
-                        <div style={{position: "absolute", borderBottom: "1px solid green", bottom: 0, left: 0, right: `${percentage}%`, borderColor: `${ submission.getScore() - rand * 100 >= assignment.getScorelimit() ? "green" : "yellow"}`, opacity:0.3 }}></div>
+                         * if the score is above the assignment score limit, the bar turns green, else it is yellow. //TODO Should look into de-spaghettifying this. */ }
+                        <ProgressBar courseID={state.activeCourse} assignmentID={index} type="navbar" />
+
+                        {/* <div style={{ position: "absolute", borderBottom: "1px solid green", bottom: 0, left: 0, right: `${percentage}%`, borderColor: `${ score >= assignment.getScorelimit() ? "green" : "yellow"}`, opacity:0.3 }}></div> */}
+                        {/* TODO This line should perhaps be its own component */}
                     </li>
                 )
             })
