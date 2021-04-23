718527e43d1c8e4ccd74a8aca3d2901a9ace063a
JosteinLindhom <jossi2010@gmail.com>
2021-04-23 13:21:32 +0200

    New Routes & Alert fix

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -12,6 +12,9 @@ import { SingleEntryPlugin } from 'webpack';
 import Group from './components/Group';
 import Alert from './components/Alert';
 import Members from './components/Members';
+import Admin from './components/Admin';
+import Review from './components/Review';
+import Groups from './components/Groups';
 
 
 
@@ -54,26 +57,30 @@ const App = () => {
             <NavBar />
                 <div className={state.theme+" app wrapper"} >
                     <div id="content">
+                    <Alert />
                         {state.isLoading ? ( // if not logged in, enable only the Info component to be rendered
                             <div className="centered">
                                 <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i>
                                 <p><strong>Loading...</strong></p>
                             </div>
-                        ) : ( // Else if, user logged in, but has not added their information redirect to Profile
-                            (state.user.email.length == 0 || state.user.name.length == 0 || state.user.studentid == 0) && loggedIn ? (
-                                <Switch>
-                                    <Route path="/" component={Profile} />
-                                </Switch>
+                            ) : ( // Else if, user logged in, but has not added their information redirect to Profile
+                                (state.user.email.length == 0 || state.user.name.length == 0 || state.user.studentid == 0) && loggedIn ? (
+                                    <Switch>
+                                        <Route path="/" component={Profile} />
+                                    </Switch>
                             ) : ( loggedIn ? ( // Else render page as expected for a logged in user
                                 <Switch>
                                     <Route path="/" exact component={Home} />
                                     <Route path="/info" component={Info} />
                                     <Route path="/profile" component={Profile} />
                                     <Route path="/course/:id/members" component={Members} />
+                                    <Route path="/course/:id/review" component={Review} />
                                     <Route path="/course/:id/group" exact component={Group} />
+                                    <Route path="/course/:id/groups" exact component={Groups} />
                                     <Route path="/course/:id" exact component={Course} />
                                     <Route path="/courses" exact component={Courses} />
                                     <Route path="/course/:id/:lab" component={Lab} />
+                                    <Route path="/admin" component={Admin} />
                                 </Switch>
                                 // Admin stuff is probably also needed here somewhere. 
                             ) : (
