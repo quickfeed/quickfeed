a0413d6f8045be65709d90f5c5009d00ff0bc46c
JosteinLindhom <jossi2010@gmail.com>
2021-05-06 23:58:29 +0200

    WIP Group view for teacher. Missing group acceptance

--- public/src/components/Groups.tsx
+++ public/src/components/Groups.tsx
@@ -2,23 +2,28 @@ import React, { useEffect } from "react"
 import { RouteComponentProps } from "react-router-dom"
 import { useOvermind } from "../overmind"
 
-
-
+/* Lists all groups for a given course. */
 export const Groups = (props: RouteComponentProps<{id?: string | undefined}>) => {
     const {state, actions} = useOvermind()
     const courseID = Number(props.match.params.id)
+
     useEffect(() => {
         actions.getGroupsByCourse(courseID)
     }, [state.groups])
 
-
-    const makeGroups = state.groups[courseID]?.map(group => {
+    // Generates JSX.Element array containing all groups for the course
+    const groups = state.groups[courseID]?.map(group => {
         return (
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">Name: {group.getName()}</li>
                 <li className="list-group-item">Status: {group.getStatus()}</li>
-                { group.getEnrollmentsList().map(enrol => 
-                    <li className="list-group-item"><img src={enrol.getUser()?.getAvatarurl()} style={{width: "40px", borderRadius: "50%", marginRight: "10px"}}></img>{enrol.getUser()?.getName()} <span>{enrol.getSlipdaysremaining()}</span></li>
+                { // Populates the unordered list with list elements for every user in the group
+                    group.getEnrollmentsList().map(enrol => 
+                        <li className="list-group-item">
+                            <img src={enrol.getUser()?.getAvatarurl()} style={{width: "40px", borderRadius: "50%", marginRight: "10px"}}></img>
+                            {enrol.getUser()?.getName()} 
+                            <span>{enrol.getSlipdaysremaining()}</span>
+                        </li>
                 )}
             </ul>
         )
@@ -26,10 +31,10 @@ export const Groups = (props: RouteComponentProps<{id?: string | undefined}>) =>
 
     return (
         <div className="box">
-        <div className="card well" style={{width: "400px"}}>
-        <div className="card-header">Groups</div>
-            {makeGroups}
-        </div>
+            <div className="card well" style={{width: "400px"}}>
+                <div className="card-header">Groups</div>
+                {groups}
+            </div>
         </div>
     )
 }
