506f4105ab15f4d31f7c95fd1533f06f309d30bd
JosteinLindhom <jossi2010@gmail.com>
2021-05-07 00:03:29 +0200

    First attempt at listing all assignments for a course

--- public/src/components/Review.tsx
+++ public/src/components/Review.tsx
@@ -1,16 +1,52 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router"
+import { sortByField } from "../Helpers"
 import { useOvermind } from "../overmind"
+import { Enrollment, EnrollmentLink, User } from "../proto/ag_pb"
+import Lab from "./Lab"
 
 
+const Review = () => {
+    const {state, actions} = useOvermind()
 
-export const Review = () => {
-    const {actions} = useOvermind()
+    const history = useHistory()
     useEffect(() => {
-        console.log("REVIEW TEST")
-        actions.getAllCourseSubmissions(2)
-    }, [])
+            actions.getAllCourseSubmissions(4)
+
+    }, [state.courseSubmissions])
+
+
+    if (state.courseSubmissions[4]) {
+        const s = sortByField(state.courseSubmissions[4], [EnrollmentLink.prototype.getEnrollment, Enrollment.prototype.getUser], User.prototype.getEmail, true).map((l: EnrollmentLink) => {
+            
+            return (
+            <div className="card well" style={{width: "400px", marginBottom: "5px"}}>
+            <div className="card-header">{l.getEnrollment()?.getUser()?.getEmail()} - {l.getEnrollment()?.getStatus()}</div>
+            <ul className="list-group list-group-flush">
+                
+                {l.getSubmissionsList().map(s => {
+                    return (
+                    <React.Fragment>
+                    <li className="list-group-item">{s.getAssignment()?.getName()} - {s.getSubmission()?.getScore()} / 100</li>
+                    
+                    </React.Fragment>
+                    )
+                })
+                }
+            </ul>
+            </div>
+            )
+        })
+    
+
+        return (
+            <div>
+                {s}
+            </div>
+        )
+    }
     return (
-        <div>Test</div>
+        <div>Loading</div>
     )
 }
 
