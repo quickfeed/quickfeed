a1069ffdb7241f03ea9729585d470de5c5ed4927
JosteinLindhom <jossi2010@gmail.com>
2021-05-07 18:15:05 +0200

    Added button to show group if user has a group

--- public/src/components/CourseOverview.tsx
+++ public/src/components/CourseOverview.tsx
@@ -10,14 +10,15 @@ interface MatchProps {
     id: string
 }
 
-
+/* */
 const CourseOverview = (props: RouteComponentProps<MatchProps>) => {
-
     const { state, actions } = useOvermind()
-    let courseID = Number(props.match.params.id)
+    const courseID = Number(props.match.params.id)
+    
     useEffect(() => {
 
     }, [props])
+    
 
     return (
         <div className="box">
@@ -27,20 +28,34 @@ const CourseOverview = (props: RouteComponentProps<MatchProps>) => {
                 <div className="col-md-9" >
                     <CourseLabs crsid={courseID}/>
                 </div>
-                <div className="col-sm-3" >
-                    
+                <div className="col-sm-3" >           
                     <div className="list-group">
                         <div className="list-group-item list-group-item-action active text-center"><h6><strong>Utility</strong></h6></div>
-                        <a href={state.repositories[courseID][Repository.Type.USER]} className="list-group-item list-group-item-action">User Repository</a>
+                            <a href={state.repositories[courseID][Repository.Type.USER]} className="list-group-item list-group-item-action">
+                                User Repository
+                            </a>
                         {state.repositories[courseID][Repository.Type.GROUP] !== "" ?(
-                        <a href={state.repositories[courseID][Repository.Type.GROUP]} className="list-group-item list-group-item-action overflow-ellipses" style={{textAlign:"left"}}>Group Repository ({state.enrollmentsByCourseId[courseID].getGroup()?.getName()})</a>
+                            <a href={state.repositories[courseID][Repository.Type.GROUP]} className="list-group-item list-group-item-action overflow-ellipses" style={{textAlign:"left"}}>
+                                Group Repository ({state.enrollmentsByCourseId[courseID].getGroup()?.getName()})
+                            </a>
                         ):(
-                            <Link to={"/course/" + courseID + "/group"} className="list-group-item list-group-item-action list-group-item-success">Create a Group</Link>
+                            <Link to={"/course/" + courseID + "/group"} className="list-group-item list-group-item-action list-group-item-success">
+                                Create a Group
+                            </Link>
                         )}
-                        <a href={state.repositories[courseID][Repository.Type.ASSIGNMENTS]} className="list-group-item list-group-item-action">Assignments</a>
+                            <a href={state.repositories[courseID][Repository.Type.ASSIGNMENTS]} className="list-group-item list-group-item-action">
+                                Assignments
+                            </a>
 
-                        <a href={state.repositories[courseID][Repository.Type.COURSEINFO]} className="list-group-item list-group-item-action">Course Info</a>
-                        {state.enrollmentsByCourseId[courseID].hasGroup() ? <Link to={"/course/" + courseID + "/group"} className="list-group-item list-group-item-action">View Group</Link> : ""}
+                            <a href={state.repositories[courseID][Repository.Type.COURSEINFO]} className="list-group-item list-group-item-action">
+                                Course Info
+                            </a>
+
+                        {state.enrollmentsByCourseId[courseID].hasGroup() ? 
+                            <Link to={"/course/" + courseID + "/group"} className="list-group-item list-group-item-action">
+                                View Group
+                            </Link>
+                        : ""}
                     </div>
                 </div>
             </div>
