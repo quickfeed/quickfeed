a5d45950c0fc0c8a7aeadf711a5799454458f887
JosteinLindhom <jossi2010@gmail.com>
2021-05-07 18:19:20 +0200

    Remove redundant check and fix formatting of code

--- public/src/components/NavBarLabs.tsx
+++ public/src/components/NavBarLabs.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { useOvermind } from "../overmind"
-import { Submission } from "../../proto/ag_pb"
+import { Enrollment, Submission } from "../../proto/ag_pb"
 import { ProgressBar } from "./ProgressBar"
 
 
@@ -16,21 +16,22 @@ const NavBarLabs = () => {
 
     const labs = (): JSX.Element[] => { 
         
-        if(state.assignments[state.activeCourse] !== undefined && state.activeCourse > 0) {
+        if(state.assignments[state.activeCourse] && state.activeCourse > 0) {
             let links = state.assignments[state.activeCourse]?.map((assignment, index) => {
                 return (
                     <li style={{position: "relative"}} className={state.activeLab === assignment.getId() && state.activeCourse === assignment.getCourseid() ? "active" : ""} key={assignment.getId()} onClick={() => {setActive(assignment.getId())}}>
                         <div id="icon">
                             <i className={assignment.getIsgrouplab() ? "fa fa-users" : "fa fa-user"} title={assignment.getIsgrouplab() ? "Group Lab" : "Individual Lab"}>
                             </i>
-                            
                         </div>
                         <div id="title">
-                            <Link to={`/course/${state.activeCourse}/${assignment.getId()}`}>{assignment.getName()}</Link>
+                            <Link to={`/course/${state.activeCourse}/${assignment.getId()}`}>
+                                {assignment.getName()}
+                            </Link>
                         </div> 
-                        {/*approved==1 && 
-                            <i className="fa fa-check">    </i>
-                        */}
+                        {state.submissions[state.activeCourse][assignment.getOrder() - 1]?.getStatus() === Submission.Status.APPROVED && 
+                            <i className="fa fa-check" style={{marginLeft: "10px"}}></i>
+                        }
                         <ProgressBar courseID={state.activeCourse} assignmentIndex={index} type="navbar" />
 
                     </li>
