8f2502f3782d38699939103d56acb5686be36f86
JosteinLindhom <jossi2010@gmail.com>
2021-05-13 02:43:00 +0200

    Fixed loading and cleanup of App.tsx

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -3,49 +3,33 @@ import { useOvermind } from './overmind'
 import Home from './components/Home'
 import About from "./components/About";
 import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { useHistory } from 'react-router'
+import { Switch, Route } from 'react-router-dom';
 import Profile from "./components/Profile";
 import CoursePage from "./components/CoursePage"
-
 import Courses from "./components/Courses";
-
 import Alert from './components/Alert';
-
 import Admin from './components/Admin';
+import Loading from './components/Loading';
 
 
 
 
 
 const App = () => {
-
+    const { state, actions } = useOvermind()
     const [loggedIn, setLoggedIn] = useState(false)
-    const [isloading, setloading] =useState(true)
-    const history = useHistory()
-
-    useEffect(() => {
-        if (!loggedIn) {
-             actions.setupUser().then(success => {
-                if (success) {
-                    console.log(success)
-                    setLoggedIn(true)
-                    
-                }
-                setTimeout(() => {
-                    actions.loading()
-                }, 500)
-            })
+    useEffect( () => {
+        async function setup() {
+            const result = await actions.fetchUserData()
+            setLoggedIn(result)
         }
-
-        // state.isLoading = false
-        console.log('App.tsx useeffect runs')
-
+        if (!loggedIn) {
+            setup()
+        }
     }, [loggedIn,setLoggedIn])
 
     // This is just to Update the Time object in state, every 20 minutes (after mount, it mounts with a new dateobject)
     useEffect(()=> {
-        
         let updateDateNow = setInterval(()=>{
             actions.setTimeNow()
         },1200000)
@@ -54,24 +38,21 @@ const App = () => {
 
 
     // General
-    const { state, actions } = useOvermind()
-    return ( 
-        <div>
+    
+    return  (
+        <div> 
             <NavBar />
                 <div className={state.theme+" app wrapper"} >
                     <div id="content">
                     <Alert />
-                        {state.isLoading ? ( // if not logged in, enable only the Info component to be rendered
-                            <div className="centered">
-                                <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i>
-                                <p><strong>Loading...</strong></p>
-                            </div>
+                        {state.isLoading ? ( // If state.isLoading
+                            <Loading />
                             ) : ( // Else if, user logged in, but has not added their information redirect to Profile
                                 (state.user.email.length == 0 || state.user.name.length == 0 || state.user.studentid == 0) && loggedIn ? (
                                     <Switch>
                                         <Route path="/" component={Profile} />
                                     </Switch>
-                            ) : ( !state.isLoading && loggedIn ? ( // Else render page as expected for a logged in user
+                            ) : (loggedIn ? ( // Else render page as expected for a logged in user
                                 <Switch>
                                     <Route path="/" exact component={Home} />
                                     <Route path="/about" component={About} />
@@ -89,7 +70,7 @@ const App = () => {
                 </div>
             </div>
         </div>
-        )
+    )
 
 }
 
