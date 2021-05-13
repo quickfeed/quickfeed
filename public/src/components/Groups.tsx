f908e158604c353bf450ea7b29290374d86c5d4c
JosteinLindhom <jossi2010@gmail.com>
2021-05-13 03:00:14 +0200

    Case

--- public/src/components/Groups.tsx
+++ public/src/components/Groups.tsx
@@ -12,14 +12,14 @@ export const Groups = (props: RouteComponentProps<{id?: string | undefined}>) =>
     }, [state.groups])
 
     // Generates JSX.Element array containing all groups for the course
-    const groups = state.groups[courseID]?.map(group => {
+    const Groups = state.groups[courseID]?.map(group => {
         return (
             <ul className="list-group list-group-flush">
                 <li className="list-group-item">Name: {group.getName()}</li>
                 <li className="list-group-item">Status: {group.getStatus()}</li>
                 { // Populates the unordered list with list elements for every user in the group
                     group.getEnrollmentsList().map(enrol => 
-                        <li className="list-group-item">
+                        <li key={enrol.getId()} className="list-group-item">
                             <img src={enrol.getUser()?.getAvatarurl()} style={{width: "40px", borderRadius: "50%", marginRight: "10px"}}></img>
                             {enrol.getUser()?.getName()} 
                             <span>{enrol.getSlipdaysremaining()}</span>
@@ -33,7 +33,7 @@ export const Groups = (props: RouteComponentProps<{id?: string | undefined}>) =>
         <div className="box">
             <div className="card well" style={{width: "400px"}}>
                 <div className="card-header">Groups</div>
-                {groups}
+                {Groups}
             </div>
         </div>
     )
