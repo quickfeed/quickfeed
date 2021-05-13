86640b357f6d78b12a2006e2d8296c2479b6de21
JosteinLindhom <jossi2010@gmail.com>
2021-05-13 03:02:15 +0200

    Added a counter for passed tests, added progressbar padding & made boxshadow style a variable

--- public/src/components/LabResultTable.tsx
+++ public/src/components/LabResultTable.tsx
@@ -1,54 +1,66 @@
 import React from "react"
+import { Assignment, Submission } from "../../proto/ag_pb"
 import { getBuildInfo, getScoreObjects, IScoreObjects, SubmissionStatus } from "../Helpers"
 import { useOvermind } from "../overmind"
 import { ProgressBar } from "./ProgressBar"
 
 interface lab {
-    id: number
-    courseID: number
+    submission: Submission
+    assignment: Assignment
 }
 
-const LabResultTable = ({id, courseID}: lab) => {
-    const {state: {assignments, submissions}} = useOvermind()
+const LabResultTable = ({submission, assignment}: lab) => {
+    const {state: {
+        enrollmentsByCourseId, }
+    } = useOvermind()
 
-
-    const ListScoreObjects = (scoreObjects: IScoreObjects[]) => {
-        return scoreObjects.map(scoreObject => {
-            const boxShadow = (scoreObject.Score === scoreObject.MaxScore) ? "0 0px 0 #000 inset, 5px 0 0 green inset" : "0 0px 0 #000 inset, 5px 0 0 red inset"
-            return (
-                <tr>
-                    <th style={{boxShadow: boxShadow}}>
-                        {scoreObject.TestName}
-                    </th>
-                    <th>
-                        {scoreObject.Score}/{scoreObject.MaxScore}
-                    </th>
-                    <th>
-                        {scoreObject.Weight}
-                    </th>
-                </tr>
+    const ScoreObject = ({ scoreObject }: {scoreObject: IScoreObjects}) => {
+        const boxShadow = (scoreObject.Score === scoreObject.MaxScore) ? "0 0px 0 #000 inset, 5px 0 0 green inset" : "0 0px 0 #000 inset, 8px 0 0 red inset"
+        return (
+            <tr>
+                <th style={{boxShadow: boxShadow, paddingLeft: "15px"}}>
+                    {scoreObject.TestName}
+                </th>
+                <th>
+                    {scoreObject.Score}/{scoreObject.MaxScore}
+                </th>
+                <th>
+                    {scoreObject.Weight}
+                </th>
+            </tr>
             )
-        })
     }
 
     const LabResult = (): JSX.Element => {
-
-        let submission = submissions[courseID]?.find(s => s.getAssignmentid() === id)
-        let assignment = assignments[courseID]?.find(a => a.getId() == id)
-        
         if (submission && assignment) {
             const buildInfo = getBuildInfo(submission.getBuildinfo())
             const scoreObjects = getScoreObjects(submission.getScoreobjects())
             
+            let totalTests = 0
+            let passedTests = 0
+            scoreObjects.forEach(scoreObject => {
+                if (scoreObject.Score === scoreObject.MaxScore) {
+                    passedTests++
+                } 
+                totalTests++
+            })
+
+            const boxShadow = (submission.getStatus() === Submission.Status.APPROVED) ? "0 0px 0 #000 inset, 5px 0 0 green inset" : "0 0px 0 #000 inset, 8px 0 0 red inset"
             return (
                 <div className="container" style={{paddingBottom: "20px"}}>
-                    <ProgressBar courseID={courseID} assignmentIndex={assignment.getOrder() - 1} submission={submission} type={"lab"} />
-                    <table className="table table-curved">
-                        <thead>
-                            <th colSpan={3}>Lab information</th>
+                    <div style={{paddingBottom: "10px"}}>
+                        <ProgressBar key={"progress-bar"} courseID={assignment.getCourseid()} assignmentIndex={assignment.getOrder() - 1} submission={submission} type={"lab"} />
+                    </div>
+                    <table className="table table-curved table-striped">
+                        <thead className={"thead-dark"}>
+                            <tr>
+                                <th colSpan={2}>Lab information</th>
+                                <th colSpan={1}>{assignment.getName()}</th>
+                            </tr>
                         </thead>
-                        <tr className="clickable-row">
-                            <th colSpan={2}>Status</th>
+                        <tbody>
+                        <tr>
+                            <th colSpan={2} style={{boxShadow: boxShadow}}>Status</th>
                             <td>{SubmissionStatus[submission.getStatus()]}</td>
                         </tr>
                         <tr>
@@ -63,20 +75,42 @@ const LabResultTable = ({id, courseID}: lab) => {
                             <th colSpan={2}>Deadline</th>
                             <td>{assignment.getDeadline()}</td>
                         </tr>
-                        {ListScoreObjects(scoreObjects)}
-                    <tfoot>
-                    
-                    </tfoot>
-                </table>
-            </div>
+                        <tr>
+                        <th colSpan={2}>Tests Passed</th>
+                            <td>{passedTests}/{totalTests}</td>
+                        </tr>
+                        <tr>
+                            <th colSpan={2}>Slip days</th>
+                            <td>{enrollmentsByCourseId[assignment.getCourseid()].getSlipdaysremaining()}</td>
+                        </tr>
+                        <tr className={"thead-dark"}>
+                            <th colSpan={1}>Test Name</th>
+                            <th colSpan={1}>Score</th>
+                            <th colSpan={1}>Weight</th>
+                        
+                        </tr>
+                        {scoreObjects.map((scoreObject, index) => 
+                            <ScoreObject key={index} scoreObject={scoreObject} />
+                        )}
+                        
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th>Total Score</th>
+                                <th>{submission.getScore()}%</th>
+                                <th>100%</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             )
         }
-        return (<div></div>)
+        return (<div className="container"> No Submission </div>)
     }
 
     return (
     <div>
-        {LabResult()}
+        <LabResult />
     </div>
     )
 }
