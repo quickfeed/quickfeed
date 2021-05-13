283790c435c5c5b11476fa7fdcbf8a88e2859439
JosteinLindhom <jossi2010@gmail.com>
2021-05-13 03:24:45 +0200

    Added experimental approval and rejection of submissions

--- public/src/components/Review.tsx
+++ public/src/components/Review.tsx
@@ -1,45 +1,85 @@
 import React, { useEffect, useState } from "react"
-import { useHistory } from "react-router"
 import { sortByField } from "../Helpers"
 import { useOvermind } from "../overmind"
-import { Enrollment, EnrollmentLink, User } from "../../proto/ag_pb"
+import { Assignment, Enrollment, EnrollmentLink, Submission, SubmissionLink, User} from "../../proto/ag_pb"
+import { useParams } from "react-router"
+import Lab from "./Lab"
 
 
 const Review = () => {
     const {state, actions} = useOvermind()
+    const course = useParams<{id?: string}>()
+    const courseID = Number(course.id)
 
+    const [submission, setSubmission] = useState<number | undefined>(undefined)
+    const [assignment, setAssignment] = useState<number | undefined>(undefined)
     useEffect(() => {
-            actions.getAllCourseSubmissions(4)
+        if (courseID && !state.courseSubmissions[courseID]) {
+            actions.getAllCourseSubmissions(courseID)
+        }
 
-    }, [state.courseSubmissions])
+    }, [state.courseSubmissions, submission, setSubmission])
 
+    const updateStatus = (status: Submission.Status, submission?: Submission) => {
+        if (submission) {
+            let s = new Submission()
+            s.setId(submission.getId())
+            s.setStatus(status)
+            s.setReleased(submission.getReleased())
+            s.setScore(submission.getScore())
+            actions.updateSubmission({courseID: courseID, submission: s})
+        }
+    }
 
-    if (state.courseSubmissions[4]) {
-        const submissions = sortByField(state.courseSubmissions[4], [EnrollmentLink.prototype.getEnrollment], Enrollment.prototype.setStatus, false).map((link: EnrollmentLink) => {
-            
-            return (
-            <div className="card well" style={{width: "400px", marginBottom: "5px"}}>
-            <div className="card-header">{link.getEnrollment()?.getUser()?.getEmail()} - {link.getEnrollment()?.getSlipdaysremaining()}</div>
-            <ul className="list-group list-group-flush">
-                
-                {link.getSubmissionsList().map(submissionLink => {
-                    return (
-                    <React.Fragment>
-                    <li className="list-group-item">{submissionLink.getAssignment()?.getName()} - {submissionLink.getSubmission()?.getScore()} / 100</li>
-                    
-                    </React.Fragment>
-                    )
-                })
-                }
-            </ul>
-            </div>
-            )
+    const ReviewSubmissionsListItem = ({ submissionLink }: { submissionLink: SubmissionLink }) => {
+        return (
+                <li className="list-group-item" >
+                    <span  onClick={() => { setSubmission(submissionLink.getSubmission()?.getId()), setAssignment(submissionLink.getAssignment()?.getId())}}>{submissionLink.getAssignment()?.getName()} - {submissionLink.getSubmission()?.getScore()} / 100</span>
+                    <button style={{float: "right"}} onClick={() => updateStatus(Submission.Status.REJECTED, submissionLink.getSubmission())}>
+                        Reject
+                    </button>
+                    <button style={{float: "right"}} onClick={() => updateStatus(Submission.Status.APPROVED, submissionLink.getSubmission())}>
+                        Approve
+                    </button>
+                </li>
+        )
+    }
+    
+
+    if (state.courseSubmissions[courseID]) {
+        const ReviewSubmissionsTable = sortByField(state.courseSubmissions[courseID], [EnrollmentLink.prototype.getEnrollment, Enrollment.prototype.getUser], User.prototype.getName, false).map((link: EnrollmentLink, index) => {
+                return (<div key={index} className="card well" style={{width: "400px", marginBottom: "5px"}}>
+                    <div key={"header"} className="card-header">
+                        {link.getEnrollment()?.getUser()?.getName()} - {link.getEnrollment()?.getSlipdaysremaining()}
+                    </div>
+                    <ul key={"list"} className="list-group list-group-flush">
+                        {link.getSubmissionsList().map((submissionLink, index) => 
+                            <ReviewSubmissionsListItem key={index} submissionLink={submissionLink} />
+                        )}
+                    </ul>
+                </div>
+                )
         })
     
 
         return (
-            <div>
-                {submissions}
+            <div className="review box">
+                <div className="reviewTable">
+                    {ReviewSubmissionsTable}
+                </div>
+
+                
+                { // If submission & assignment is set by clicking an entry in ReviewSubmissionsListItem, the Lab will be displayed next to it
+                submission && assignment ? (
+                
+                    <div className="reviewLab">
+                        <Lab submissionID={submission} assignmentID={assignment} />
+                    </div> )
+
+                : "" }
+                    
+                
+                
             </div>
         )
     }
