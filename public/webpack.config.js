6e79a779b9450cae3aa79653a144a7670c00853e
JosteinLindhom <jossi2010@gmail.com>
2021-06-23 18:13:34 +0200

    Testing

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack")
 module.exports = {
     entry: "./src/index.tsx",
     output: {
-        filename: "bundle.js",
+        filename: "dev.js",
         path: __dirname + "/dist"
     },
     mode: "development",
@@ -14,13 +14,13 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".json"],
     },
 
 
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('test')
+        new webpack.ProvidePlugin({
+            process: 'process/browser'
         }),
         new webpack.DefinePlugin({
             'process.env.ASSET_PATH': JSON.stringify("static"),
@@ -37,7 +37,6 @@ module.exports = {
             { test: /\.css$/i,use: ["style-loader", "css-loader"],},
         ]
     },
-
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
