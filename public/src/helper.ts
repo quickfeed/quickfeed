f3951258ce719719d287a1b189588248bd9cf0c8
Hein Meling <hein.meling@uis.no>
2021-06-23 16:15:51 +0200

    Fixed stupid typescript compile error in formatDate()

--- public/src/helper.ts
+++ public/src/helper.ts
@@ -16,14 +16,13 @@ export function bindFunc<T>(thisVar: any, func: (props: T) => JSX.Element): (pro
 export type RProp<T> = { children?: JSX.Element | string } & T;
 
 export function formatDate(str: string | Date): string {
-    const dateOptions = {
-        weekday: "short",
-        month: "short",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        hour12: false,
-    };
     const date = str instanceof Date ? str : new Date(str);
-    return date.toLocaleString("no-NO", dateOptions);
+    return date.toLocaleString("no-NO", {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+    });
 }
