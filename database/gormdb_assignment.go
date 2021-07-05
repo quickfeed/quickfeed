42ae078d001c132d2e2232d466fc2b0a839ba483
Hein Meling <hein.meling@uis.no>
2021-07-05 17:29:40 +0200

    Revert "Simplified Assign stmt in CreateAssignment"
    This reverts commit 275b36575988bdfafcd05122eef50679fdee81df.

--- database/gormdb_assignment.go
+++ database/gormdb_assignment.go
@@ -29,8 +29,18 @@ func (db *GormDB) CreateAssignment(assignment *pb.Assignment) error {
 			CourseID: assignment.CourseID,
 			Order:    assignment.Order,
 		}).
-		Assign(assignment).
-		FirstOrCreate(assignment).Error
+		Assign(map[string]interface{}{
+			"name":              assignment.Name,
+			"order":             assignment.Order,
+			"script_file":       assignment.ScriptFile,
+			"deadline":          assignment.Deadline,
+			"auto_approve":      assignment.AutoApprove,
+			"score_limit":       assignment.ScoreLimit,
+			"is_group_lab":      assignment.IsGroupLab,
+			"reviewers":         assignment.Reviewers,
+			"container_timeout": assignment.ContainerTimeout,
+			"skip_tests":        assignment.SkipTests,
+		}).FirstOrCreate(assignment).Error
 }
 
 // GetAssignment returns assignment with the given ID.
