0e4d0b0ba96834e13125bdf5b37925c2569f62ec
JosteinLindhom <jossi2010@gmail.com>
2021-07-16 23:07:11 +0200

    Changed props

--- public/src/components/CourseLabs.tsx
+++ public/src/components/CourseLabs.tsx
@@ -5,25 +5,21 @@ import { Submission } from "../../proto/ag/ag_pb"
 import { ProgressBar } from "./ProgressBar"
 import React from "react"
 
-interface MatchProps {
-    crsid: number
-}
-
-export const CourseLabs = (props:MatchProps) =>  {
+export const CourseLabs = ({courseID}: {courseID: number}) =>  {
     const { state } = useOvermind()
     const history  = useHistory()
     
     function redirectToLab(assignmentid:number){
-        history.push(`/course/${props.crsid}/${assignmentid}`)
+        history.push(`/course/${courseID}/${assignmentid}`)
     }
     const Labs: Function = (): JSX.Element[] => {
         let labs :JSX.Element[] = []
         let submission: Submission = new Submission()
-        if(state.assignments[props.crsid] && state.submissions[props.crsid]) {
-            state.assignments[props.crsid].forEach(assignment => {
+        if(state.assignments[courseID] && state.submissions[courseID]) {
+            state.assignments[courseID].forEach(assignment => {
                 // Submissions are indexed by the assignment order.    
-                if (state.submissions[props.crsid][assignment.getOrder() - 1]){
-                    submission = state.submissions[props.crsid][assignment.getOrder() - 1]
+                if (state.submissions[courseID][assignment.getOrder() - 1]){
+                    submission = state.submissions[courseID][assignment.getOrder() - 1]
                 }
                 
                 labs.push(
@@ -34,8 +30,8 @@ export const CourseLabs = (props:MatchProps) =>  {
                             <div className="col-4 text-center"><strong>Deadline:</strong></div>
                         </div>
                         <div className="row" >
-                            <div className="col-5"><ProgressBar courseID={props.crsid} assignmentIndex={assignment.getOrder()-1} submission={submission} type="lab"/></div>
-                            <div className="col-3 text-center">{(submission.getStatus()==0 && submission.getScore()>=assignment.getScorelimit()) ? "AWAITING APPROVAL":SubmissionStatus[submission.getStatus()]}</div>
+                            <div className="col-5"><ProgressBar courseID={courseID} assignmentIndex={assignment.getOrder()-1} submission={submission} type="lab"/></div>
+                            <div className="col-3 text-center">{(submission.getStatus()==0 && submission.getScore()>=assignment.getScorelimit()) ? "Awaiting Approval":SubmissionStatus[submission.getStatus()]}</div>
                             <div className="col-4 text-center">{getFormattedTime(assignment.getDeadline())}</div>
                         </div>
                     </li>
