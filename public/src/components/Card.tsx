50fa0edc399db5a01b0a320f2acc808605639b36
JosteinLindhom <jossi2010@gmail.com>
2021-07-17 02:07:45 +0200

    Added a Card component (to be renamed when the brain comes online) thatis intended to be used to list and link to teacher and admin functionalities from the teacher and admin pages

---
+++ public/src/components/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { useHistory } from "react-router"
+
+
+const Card = (props: {title: string, text: string, buttonText: string, to: string}) => {
+    const history = useHistory()
+
+    return (
+        <div className="col-sm-6">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.text}</p>
+                    <div className="btn btn-primary" onClick={() => history.push(props.to)}>{props.buttonText}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
