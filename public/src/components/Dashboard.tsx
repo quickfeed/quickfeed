dde2722b19746d13de2109397568e65f6fc220d0
JosteinLindhom <jossi2010@gmail.com>
2021-07-19 04:12:43 +0200

    A user that has no enrollments will be redirected from dashboard to courses when logging in.

--- public/src/components/Dashboard.tsx
+++ public/src/components/Dashboard.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Redirect } from "react-router";
+import { Enrollment } from "../../proto/ag/ag_pb";
+import { isEnrolled } from "../Helpers";
 import { useOvermind } from "../overmind";
 import Courses from "./Courses";
 import LandingPageLabTable from "./LandingPageLabTable";
@@ -6,12 +9,17 @@ import LandingPageLabTable from "./LandingPageLabTable";
 
 /* Dashboard for a signed in user. */
 const Dashboard = () => {
-    const { state: {self: user} } = useOvermind()
+    const { state } = useOvermind()
+
+    // New users logging in are redirected to courses to ease enrollment
+    if (!isEnrolled(state.enrollments)) {
+        return <Redirect to={"/courses"}></Redirect>
+    }
 
     return(
         <div className='box'>
             <div>
-                <h1>Welcome, {user.getName()}!</h1>
+                <h1>Welcome, {state.self.getName()}!</h1>
             </div>
             <LandingPageLabTable courseID={0}/>
             <Courses home={true} />
