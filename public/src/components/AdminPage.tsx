51cad9a8a9996ef64ccc4ca60debd8b1fd0b973d
JosteinLindhom <jossi2010@gmail.com>
2021-07-19 04:10:57 +0200

    Hide the button to return to '/admin' when at '/admin'. Updated route for editing course.

--- public/src/components/AdminPage.tsx
+++ public/src/components/AdminPage.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Redirect, Route, Switch, useHistory } from "react-router"
 import { useOvermind } from "../overmind"
+import EditCourse from "./admin/EditCourse"
 import Users from "./admin/Users"
 import Card from "./Card"
 import CourseCreationForm from "./forms/CourseCreationForm"
@@ -8,19 +9,23 @@ import CourseCreationForm from "./forms/CourseCreationForm"
 
 
 export const AdminPage = () => {
+    
     const {state} = useOvermind()
 
     // Ideas: Statistics, Create Course, Promote Users
+    
     const manageUsers = {title: "Manage Users", text: "View and manage all users.", buttonText: "Manage Users", to: "/admin/manage"}
     const createCourse = {title: "Create Course", text: "Create a new course.", buttonText: "Create Course", to: "/admin/create"}
-    const editCourse = {title: "Edit Course", text: "Edit an existing course.", buttonText: "Edit Course", to: "/admin/create"}
+    const editCourse = {title: "Edit Course", text: "Edit an existing course.", buttonText: "Edit Course", to: "/admin/edit"}
+    
     /** Button used to redirect a user, ex. return to top layer of course page */
     const RedirectButton = ({to}: {to: string}) => {
         const history  = useHistory()
+        const hide = history.location.pathname == "/admin" ? true : false
         return (
-        <div className={"btn btn-dark redirectButton"} onClick={() => history.push(to)}>
-            <i className="fa fa-arrow-left"></i>
-        </div>
+            <div className={"btn btn-dark redirectButton"} onClick={() => history.push(to)} hidden={hide}>
+                <i className="fa fa-arrow-left"></i>
+            </div>
         )
     }
     
@@ -36,12 +41,13 @@ export const AdminPage = () => {
                 <Switch>
                     <Route path={"/admin/manage"} component={Users}></Route>
                     <Route path={"/admin/create"} component={CourseCreationForm}></Route>
+                    <Route path={"/admin/edit"} component={EditCourse}></Route>
                 </Switch>
             </div>
         )
     }
     return (
-        <Redirect to="" />
+        <Redirect to="/" />
     )
 }
 
