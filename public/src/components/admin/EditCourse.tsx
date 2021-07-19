6e7c223fc6f0fcd6f040f6929b0a6e8084ce6cb1
JosteinLindhom <jossi2010@gmail.com>
2021-07-19 04:27:06 +0200

    WIP course edit component.

---
+++ public/src/components/admin/EditCourse.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { useOvermind } from "../../overmind"
+
+/** TODO:  */
+const EditCourse = () => {
+    const { state } = useOvermind()
+
+    const courses = state.courses.map(course => {
+        return (
+            <tr style={{"cursor": "pointer"}}>
+                <th colSpan={2}>{course.getName()}</th>
+                <td>{course.getCode()}</td>
+                <td>{course.getTag()}</td>
+                <td>{course.getYear()}</td>
+                <td>{course.getSlipdays()}</td>
+            </tr>
+        )
+    })
+
+    return (
+        <div className={"box"}>
+            <table className="table table-curved table-striped">
+                <thead className={"thead-dark"}>
+                <th colSpan={2}>Course</th>
+                <th>Code</th>
+                <th>Tag</th>
+                <th>Year</th>
+                <th>Slipdays</th>
+                </thead>
+                <tbody>
+                {courses}
+                </tbody>
+
+            </table>
+        </div>
+    )
+}
+
+export default EditCourse
