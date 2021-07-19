7f43bea8c01c462301427b751677c72386ecf870
JosteinLindhom <jossi2010@gmail.com>
2021-07-19 04:16:44 +0200

    Formatting.

--- public/src/components/Card.tsx
+++ public/src/components/Card.tsx
@@ -9,9 +9,15 @@ const Card = (props: {title: string, text: string, buttonText: string, to: strin
         <div className="col-sm-6">
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">{props.text}</p>
-                    <div className="btn btn-primary" onClick={() => history.push(props.to)}>{props.buttonText}</div>
+                    <h5 className="card-title">
+                        {props.title}
+                    </h5>
+                    <p className="card-text">
+                        {props.text}
+                    </p>
+                    <div className="btn btn-primary" onClick={() => history.push(props.to)}>
+                        {props.buttonText}
+                    </div>
                 </div>
             </div>
         </div>
