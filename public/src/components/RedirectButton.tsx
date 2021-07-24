2c3f967577e62861ddf1389a5a95fc7a0b90f516
JosteinLindhom <jossi2010@gmail.com>
2021-07-24 04:38:01 +0200

    Added keyboard shortcut to navigate back to root.

--- public/src/components/RedirectButton.tsx
+++ public/src/components/RedirectButton.tsx
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useHistory } from "react-router"
 
 
 const RedirectButton = ({to}: {to: string}) => {
     const history = useHistory()
     const hide = history.location.pathname == to ? true : false
+    
+    useEffect(() => {
+        const handleKeyPress = (e: KeyboardEvent) => {
+            if (e.key == "Backspace" && !hide) {
+                history.push(to)
+            }
+        }
+
+        document.addEventListener('keydown', handleKeyPress)
+
+        const cleanup = () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        }
+        return cleanup
+    }, [])
+
     return (
         <div className={"btn btn-dark redirectButton"} onClick={() => history.push(to)} hidden={hide}>
             <i className="fa fa-arrow-left"></i>
