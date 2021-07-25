17721c79457494805275164609e1b196ff0895d5
JosteinLindhom <jossi2010@gmail.com>
2021-07-25 04:54:23 +0200

    Added check to disable keyboard shortcut if enableRedirect is set to false. Not doing this would cause redirects when trying to erase text in text inputs.

--- public/src/components/RedirectButton.tsx
+++ public/src/components/RedirectButton.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react"
 import { useHistory } from "react-router"
+import { useOvermind } from "../overmind"
 
 
 const RedirectButton = ({to}: {to: string}) => {
+    const { state } = useOvermind()
     const history = useHistory()
     const hide = history.location.pathname == to ? true : false
     
     useEffect(() => {
         const handleKeyPress = (e: KeyboardEvent) => {
-            if (e.key == "Backspace" && !hide) {
+            if (e.key == "Backspace" && !hide && state.enableRedirect) {
                 history.push(to)
             }
         }
