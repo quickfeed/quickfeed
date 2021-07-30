d1b8268560744aa1a2ddd8a36bf63e964f8aed3b
JosteinLindhom <jossi2010@gmail.com>
2021-07-30 04:23:19 +0200

    Added Lab view when selecting a user submission. Also replaced the component state that handled search with overmind state search.

--- public/src/components/Results.tsx
+++ public/src/components/Results.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from "react"
-import { Redirect, useParams } from "react-router-dom"
+import React, { useEffect } from "react"
 import { Enrollment } from "../../proto/ag/ag_pb"
 import { getCourseID } from "../Helpers"
 import { useActions, useAppState } from "../overmind"
+import Lab from "./Lab"
+import Search from "./Search"
 import ResultItem from "./teacher/ResultItem"
 
 
 const Results = () => {
     const state = useAppState()
     const actions = useActions()
-    
+    const { getAllCourseSubmissions } = useActions()
     const courseID = getCourseID()
-
-    const [query, setQuery] = useState<string>("")
 
     useEffect(() => {
         if (courseID && !state.courseSubmissions[courseID]) {
-            actions.getAllCourseSubmissions(courseID)
+            getAllCourseSubmissions(courseID)
         }
-    }, [state.courseSubmissions, query, setQuery])
+        return actions.setActiveSubmission(undefined)
+    }, [state.courseSubmissions])
 
 
     const TableAssignmentsHead = state.assignments[courseID].map(assignment => {
@@ -32,22 +32,33 @@ const Results = () => {
 
     const UserResults = state.courseSubmissions[courseID].map(user => {
         if (user.enrollment && user.submissions) {
-            return <ResultItem enrollment={user.enrollment} submissionsLink={user.submissions} query={query} />
+            return <ResultItem enrollment={user.enrollment} submissionsLink={user.submissions} />
         }
     })
 
     return (
-        <React.Fragment>
-        <input onChange={e => setQuery(e.target.value.toLowerCase())}></input>
-        <table>
-            <thead>
-                <td>Name</td>
-                <td>Group</td>
-                {TableAssignmentsHead}
-            </thead>
-            {UserResults}
-        </table>
-        </React.Fragment>
+        <div className="box">
+        <Search />
+        <div className="row">
+            <div className="col">
+            <table className="table table-curved table-striped">
+                <thead className="thead-dark">
+                    <td>Name</td>
+                    <td>Group</td>
+                    {TableAssignmentsHead}
+                </thead>
+                <tbody>
+                {UserResults}
+                </tbody>
+            </table>
+            </div>
+            <div className="col">
+                {state.activeSubmission ?
+                <Lab teacherSubmission={state.activeSubmission} /> : null
+                }  
+            </div>
+        </div>
+        </div>
 
     )
 }
