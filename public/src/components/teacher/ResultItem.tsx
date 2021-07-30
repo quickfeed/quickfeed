dac5861b59b8cf01c82e3432aab1ddca0becb0bd
JosteinLindhom <jossi2010@gmail.com>
2021-07-30 04:35:03 +0200

    Add state.query to resolve hiding element.

--- public/src/components/teacher/ResultItem.tsx
+++ public/src/components/teacher/ResultItem.tsx
@@ -1,26 +1,32 @@
+import { json } from "overmind"
 import React from "react"
 import { Enrollment, SubmissionLink } from "../../../proto/ag/ag_pb"
+import { useActions, useAppState } from "../../overmind"
 
 
 
-const ResultItem = (data: {enrollment: Enrollment | undefined, submissionsLink: SubmissionLink[], query: string}) => {
-    const submissions = data.submissionsLink.map(link => {
+const ResultItem = ({enrollment, submissionsLink}: {enrollment: Enrollment | undefined, submissionsLink: SubmissionLink[]}) => {
+    const query = useAppState().query
+    const actions = useActions()
+    const submissions = submissionsLink.map(link => {
         if (link.hasSubmission() && link.hasAssignment()) {
             return (
-                <td>
+                <td onClick={() => actions.setActiveSubmission(json(link.getSubmission()))}>
                     {link.getSubmission()?.getScore()}%
                 </td>
             )
         }
     })
-    let hidden = data.enrollment?.getUser()?.getName().toLowerCase().includes(data.query) || data.enrollment?.getGroup()?.getName().toLowerCase().includes(data.query)
+
+    // TODO: Make a helper function that can resolve hidden.
+    const hidden = enrollment?.getUser()?.getName().toLowerCase().includes(query) || enrollment?.getGroup()?.getName().toLowerCase().includes(query)
     return (
         <tr hidden={!hidden}>
             <td>
-                {data.enrollment?.getUser()?.getName()}
+                {enrollment?.getUser()?.getName()}
             </td>
             <td>
-                {data.enrollment?.getGroup()?.getName()}
+                {enrollment?.getGroup()?.getName()}
             </td>
             {submissions}
         </tr>
