8ee4a6cf4ce95547bc7086c0ba4e26bd4c4899b5
0xf8f8ff <0xf8f8ff@protonmail.com>
2021-08-04 16:04:49 +0200

    Add more test cases

--- web/rebuild_test.go
+++ web/rebuild_test.go
@@ -38,7 +38,6 @@ func TestRebuildSubmissions(t *testing.T) {
 	}); err != nil {
 		t.Fatal(err)
 	}
-
 	student2 := createFakeUser(t, db, 4)
 	if err := db.CreateEnrollment(&pb.Enrollment{UserID: student2.ID, CourseID: course.ID}); err != nil {
 		t.Fatal(err)
@@ -50,7 +49,6 @@ func TestRebuildSubmissions(t *testing.T) {
 	}); err != nil {
 		t.Fatal(err)
 	}
-
 	repo1 := pb.Repository{
 		OrganizationID: 1,
 		RepositoryID:   1,
@@ -69,7 +67,6 @@ func TestRebuildSubmissions(t *testing.T) {
 	if err := db.CreateRepository(&repo2); err != nil {
 		t.Fatal(err)
 	}
-
 	fakeProvider, scms := fakeProviderMap(t)
 	ags := web.NewAutograderService(zap.NewNop(), db, scms, web.BaseHookOptions{}, &ci.Local{})
 	ctx := withUserContext(context.Background(), teacher)
@@ -78,7 +75,6 @@ func TestRebuildSubmissions(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	assignment := &pb.Assignment{
 		CourseID:         course.ID,
 		Name:             "lab1",
@@ -100,7 +96,6 @@ func TestRebuildSubmissions(t *testing.T) {
 	}); err != nil {
 		t.Fatal(err)
 	}
-
 	if err = db.CreateSubmission(&pb.Submission{
 		AssignmentID: 1,
 		UserID:       student2.ID,
@@ -108,38 +103,51 @@ func TestRebuildSubmissions(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// rebuild a single submission
+	var rebuildRequest pb.RebuildRequest
+	rebuildRequest.AssignmentID = assignment.ID
+	rebuildRequest.SubmissionID = 123
+	if _, err := ags.RebuildSubmission(ctx, &rebuildRequest); err == nil {
+		t.Errorf("Expected error: record not found")
+	}
+	rebuildRequest.SubmissionID = 1
+	if _, err := ags.RebuildSubmission(ctx, &rebuildRequest); err != nil {
+		t.Fatalf("Failed to rebuild submission: %s", err)
+	}
 	submissions, err := db.GetSubmissions(&pb.Submission{AssignmentID: assignment.ID})
 	if err != nil {
 		t.Fatalf("Failed to get created submissions: %s", err)
 	}
 
-	var request pb.AssignmentRequest
 	// make sure wrong course ID returns error
+	var request pb.AssignmentRequest
 	request.CourseID = 15
-	_, err = ags.RebuildSubmissions(ctx, &request)
-	if err == nil {
+	if _, err = ags.RebuildSubmissions(ctx, &request); err == nil {
 		t.Fatal("Expected error: record not found")
 	}
 
 	// make sure wrong assignment ID returns error
 	request.CourseID = course.ID
 	request.AssignmentID = 1337
-	_, err = ags.RebuildSubmissions(ctx, &request)
-	if err == nil {
+	if _, err = ags.RebuildSubmissions(ctx, &request); err == nil {
 		t.Fatal("Expected error: record not found")
 	}
 
 	request.AssignmentID = assignment.ID
-	_, err = ags.RebuildSubmissions(ctx, &request)
-	if err != nil {
+	if _, err = ags.RebuildSubmissions(ctx, &request); err != nil {
 		t.Fatalf("Failed to rebuild submissions: %s", err)
 	}
-
 	rebuiltSubmissions, err := db.GetSubmissions(&pb.Submission{AssignmentID: assignment.ID})
 	if err != nil {
 		t.Fatalf("Failed to get created submissions: %s", err)
 	}
 	if len(submissions) != len(rebuiltSubmissions) {
 		t.Errorf("Incorrect number of submissions after rebuild: expected %d, got %d", len(submissions), len(rebuiltSubmissions))
+	}
+
+	// check access control
+	ctx = withUserContext(ctx, student1)
+	if _, err = ags.RebuildSubmissions(ctx, &request); err == nil {
+		t.Fatal("Expected error: authentication failed")
 	}
 }
