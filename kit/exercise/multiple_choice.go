0b49900a25bb06f155625a965ea783e26e8916a4
Hein Meling <hein.meling@uis.no>
2021-08-12 00:46:27 +0200

    Redesigned MultipleChoice API
    New API contains two main functions:
    - ParseMarkdownAnswers: parses markdown file with answers
    - CheckMultipleChoice: compare answers with correct answers
    
    And one function:
    - Print: helper for printing answers

--- kit/exercise/multiple_choice.go
+++ kit/exercise/multiple_choice.go
@@ -1,15 +1,12 @@
 package exercise
 
 import (
+	"fmt"
 	"io/ioutil"
 	"regexp"
 	"sort"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/autograde/quickfeed/kit/score"
-	"github.com/google/go-cmp/cmp"
 )
 
 // compile regular expressions only once
@@ -18,54 +15,58 @@ var (
 	selectionRegExp = regexp.MustCompile(`^\s+\-\s\[(x|X)\]\s+([a-f])\)\s.*$`)
 )
 
-func parseMCAnswers(mdFile string) (map[string]string, error) {
-	md, err := ioutil.ReadFile(mdFile)
+// ParseMarkdownAnswers returns a map of the answers found in the given answer file.
+func ParseMarkdownAnswers(answerFile string) (map[int]string, error) {
+	md, err := ioutil.ReadFile(answerFile)
 	if err != nil {
 		return nil, err
 	}
 
-	var curQ string
+	currentQ := -1
 	// map: question# -> answer label
-	qaMap := make(map[string]string)
+	answerMap := make(map[int]string)
 	for _, line := range strings.Split(string(md), "\n") {
 		if qNumRegExp.MatchString(line) {
-			curQ = qNumRegExp.ReplaceAllString(line, "$1")
+			qNum := qNumRegExp.ReplaceAllString(line, "$1")
+			// ignore error since regular expression ensure it is already a number
+			currentQ, _ = strconv.Atoi(qNum)
 		}
-		_, found := qaMap[curQ]
-		if !found && curQ != "" && selectionRegExp.MatchString(line) {
-			qaMap[curQ] = selectionRegExp.ReplaceAllString(line, "$2")
+		_, found := answerMap[currentQ]
+		if !found && currentQ != -1 && selectionRegExp.MatchString(line) {
+			answerMap[currentQ] = selectionRegExp.ReplaceAllString(line, "$2")
 		}
 	}
-	return qaMap, nil
+	return answerMap, nil
 }
 
-// MultipleChoice computes the score of a multiple choice exercise
-// with student providing answers in the mdFile, where the correct map is
-// expected to contain the correct answers. The function emits a JSON Score
-// object and a corresponding message for x/y test cases passed.
-func MultipleChoice(t *testing.T, mdFile string, correct map[int]string) {
-	t.Helper()
-	sc := score.NewScoreMax(t, len(correct), 1)
-	defer sc.Print(t)
-
-	qaMap, err := parseMCAnswers(mdFile)
-	if err != nil {
-		sc.Score = 0
-		t.Fatal(err)
-	}
-	// sort map keys: question numbers
-	qNumbers := make([]int, 0, len(correct))
-	for k := range correct {
-		qNumbers = append(qNumbers, k)
-	}
-	sort.Ints(qNumbers)
-
-	for _, qNum := range qNumbers {
-		ans, found := qaMap[strconv.Itoa(qNum)]
-		if !found || !cmp.Equal(correct[qNum], ans) {
-			t.Errorf("%v: Question %d: Answer not found or incorrect.\n", sc.TestName, qNum)
-			sc.Dec()
+// CheckMultipleChoice returns the result of comparing the answers to the correct maps.
+// The answers and correct maps from keys representing the question number to the labels (answer value).
+// The question numbers (keys) in the correct map must contain all question numbers in the range 1 - len(correct).
+// The returned slices contain question numbers deemed correctly and incorrectly answered, respectively.
+func CheckMultipleChoice(answers, correct map[int]string) (correctA []int, incorrectA []int) {
+	for qNum, label := range correct {
+		if answers[qNum] == label {
+			correctA = append(correctA, qNum)
 			continue
+		} else {
+			incorrectA = append(incorrectA, qNum)
 		}
 	}
+	sort.Ints(correctA)
+	sort.Ints(incorrectA)
+	return
+}
+
+// Print returns a string representation of the given list of questions.
+// The preLabel and afterLabel precedes and succeed the question number,
+// and all but the last question number is preceded by the sep separator.
+func Print(questions []int, preLabel, afterLabel, sep string) string {
+	var b strings.Builder
+	for i, q := range questions {
+		fmt.Fprintf(&b, "%s%d%s", preLabel, q, afterLabel)
+		if i < len(questions)-1 {
+			fmt.Fprint(&b, sep)
+		}
+	}
+	return b.String()
 }
--- kit/exercise/multiple_choice_test.go
+++ kit/exercise/multiple_choice_test.go
@@ -1,43 +1,230 @@
 package exercise_test
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/autograde/quickfeed/kit/exercise"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func TestMultipleChoice(t *testing.T) {
-	tests := []struct {
-		name  string
-		file  string
-		qaMap map[int]string
-	}{
-		{
-			name:  "BlankAnswers",
-			file:  "c-prog-questions-blank-answers.md",
-			qaMap: map[int]string{},
-		},
-		{
-			name:  "PartialAnswers",
-			file:  "c-prog-questions-partial-answers.md",
-			qaMap: map[int]string{1: "c", 4: "c", 6: "a"},
-		},
-		{
-			name:  "AllAnswersSomeVCheckMark",
-			file:  "c-prog-questions-all-answers-v-mark.md",
-			qaMap: map[int]string{1: "a", 3: "c", 5: "b", 6: "b", 7: "d"},
-		},
-		{
-			name:  "AllAnswers",
-			file:  "c-prog-questions-all-answers.md",
-			qaMap: map[int]string{1: "a", 2: "b", 3: "c", 4: "a", 5: "b", 6: "b", 7: "d"},
-		},
+func init() {
+	scores.Add(TestMultipleChoice0, len(tests[0].correct), 1)
+	scores.Add(TestMultipleChoice1, len(tests[1].correct), 1)
+	scores.Add(TestMultipleChoice2, len(tests[2].correct), 1)
+	scores.Add(TestMultipleChoice3, len(tests[3].correct), 1)
+	for _, test := range tests {
+		scores.AddSub(TestMultipleChoiceWithFail, test.name, len(test.correct), 1)
+	}
+}
+
+var tests = []struct {
+	name          string
+	file          string
+	answers       map[int]string
+	correct       map[int]string
+	wantCorrect   []int
+	wantIncorrect []int
+}{
+	{
+		name:          "BlankAnswers",
+		file:          "c-prog-questions-blank-answers.md",
+		answers:       map[int]string{},
+		correct:       map[int]string{1: "a", 2: "b", 3: "c", 4: "a", 5: "b", 6: "b", 7: "d"},
+		wantCorrect:   []int{},
+		wantIncorrect: []int{1, 2, 3, 4, 5, 6, 7},
+	},
+	{
+		name:          "PartialAnswers",
+		file:          "c-prog-questions-partial-answers.md",
+		answers:       map[int]string{1: "c", 4: "c", 6: "a"},
+		correct:       map[int]string{1: "a", 2: "b", 3: "c", 4: "a", 5: "b", 6: "b", 7: "d"},
+		wantCorrect:   []int{},
+		wantIncorrect: []int{1, 2, 3, 4, 5, 6, 7},
+	},
+	{
+		name:          "AllAnswersSomeVCheckMark",
+		file:          "c-prog-questions-all-answers-v-mark.md",
+		answers:       map[int]string{1: "a", 3: "c", 5: "b", 6: "b", 7: "d"},
+		correct:       map[int]string{1: "a", 2: "b", 3: "c", 4: "a", 5: "b", 6: "b", 7: "d"},
+		wantCorrect:   []int{1, 3, 5, 6, 7},
+		wantIncorrect: []int{2, 4},
+	},
+	{
+		name:          "AllAnswers",
+		file:          "c-prog-questions-all-answers.md",
+		answers:       map[int]string{1: "a", 2: "b", 3: "c", 4: "a", 5: "b", 6: "b", 7: "d"},
+		correct:       map[int]string{1: "a", 2: "b", 3: "c", 4: "a", 5: "b", 6: "b", 7: "d"},
+		wantCorrect:   []int{1, 2, 3, 4, 5, 6, 7},
+		wantIncorrect: []int{},
+	},
+}
+
+func TestParseMarkdownAnswers(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			answerFile := filepath.Join("..", "testdata", test.file)
+			gotAnswers, err := exercise.ParseMarkdownAnswers(answerFile)
+			if err != nil {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(test.answers, gotAnswers); diff != "" {
+				t.Errorf("TestParseMarkdownAnswers() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestCheckMultipleChoice(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			gotCorrect, gotIncorrect := exercise.CheckMultipleChoice(test.answers, test.correct)
+			// Example code for use of Print function
+			if len(gotCorrect) > 0 {
+				t.Log("Correct answers:\n", exercise.Print(gotCorrect, "\tQuestion ", " is correct", "\n"))
+			}
+			if len(gotIncorrect) > 0 {
+				t.Log("Incorrect or missing answers:", exercise.Print(gotIncorrect, "Q", "=W", ", "))
+			}
+			if diff := cmp.Diff(test.wantCorrect, gotCorrect, cmpopts.EquateEmpty()); diff != "" {
+				t.Errorf("TestCheckMultipleChoice():correct mismatch (-want +got):\n%s", diff)
+			}
+			if diff := cmp.Diff(test.wantIncorrect, gotIncorrect, cmpopts.EquateEmpty()); diff != "" {
+				t.Errorf("TestCheckMultipleChoice():incorrect mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+// To run these tests use:
+//   QUICKFEED_FAIL_TEST=1 go test -v -run TestMultipleChoice
+//
+
+const (
+	failTestEnvName = "QUICKFEED_FAIL_TEST"
+)
+
+func TestMultipleChoice0(t *testing.T) {
+	// This test aims to emulate that student submitted code may result in a panic,
+	// and thus a test failure along with a stack trace would be expected.
+	// Hence, we do not run this as part of the CI tests. To run, see instructions below.
+	failTest := os.Getenv(failTestEnvName)
+	if failTest == "" {
+		t.Skipf("Skipping; expected to fail. Run with: %s=1 go test -v -run %s", failTestEnvName, t.Name())
+	}
+
+	test := tests[0]
+	sc := scores.Max()
+	defer sc.Print(t)
+	answerFile := filepath.Join("..", "testdata", test.file)
+	answers, err := exercise.ParseMarkdownAnswers(answerFile)
+	if err != nil {
+		t.Error(err)
+	}
+	_, gotIncorrect := exercise.CheckMultipleChoice(answers, test.correct)
+	for _, incorrect := range gotIncorrect {
+		t.Errorf("%v: Question %d: Answer not found or incorrect.\n", sc.TestName, incorrect)
+		sc.Dec()
+	}
+}
+
+func TestMultipleChoice1(t *testing.T) {
+	// This test aims to emulate that student submitted code may result in a panic,
+	// and thus a test failure along with a stack trace would be expected.
+	// Hence, we do not run this as part of the CI tests. To run, see instructions below.
+	failTest := os.Getenv(failTestEnvName)
+	if failTest == "" {
+		t.Skipf("Skipping; expected to fail. Run with: %s=1 go test -v -run %s", failTestEnvName, t.Name())
+	}
+
+	test := tests[1]
+	sc := scores.Max()
+	defer sc.Print(t)
+	answerFile := filepath.Join("..", "testdata", test.file)
+	answers, err := exercise.ParseMarkdownAnswers(answerFile)
+	if err != nil {
+		t.Error(err)
+	}
+	_, gotIncorrect := exercise.CheckMultipleChoice(answers, test.correct)
+	for _, incorrect := range gotIncorrect {
+		t.Errorf("%v: Question %d: Answer not found or incorrect.\n", sc.TestName, incorrect)
+		sc.Dec()
+	}
+}
+
+func TestMultipleChoice2(t *testing.T) {
+	// This test aims to emulate that student submitted code may result in a panic,
+	// and thus a test failure along with a stack trace would be expected.
+	// Hence, we do not run this as part of the CI tests. To run, see instructions below.
+	failTest := os.Getenv(failTestEnvName)
+	if failTest == "" {
+		t.Skipf("Skipping; expected to fail. Run with: %s=1 go test -v -run %s", failTestEnvName, t.Name())
+	}
+
+	test := tests[2]
+	sc := scores.Max()
+	defer sc.Print(t)
+	answerFile := filepath.Join("..", "testdata", test.file)
+	answers, err := exercise.ParseMarkdownAnswers(answerFile)
+	if err != nil {
+		t.Error(err)
+	}
+	_, gotIncorrect := exercise.CheckMultipleChoice(answers, test.correct)
+	for _, incorrect := range gotIncorrect {
+		t.Errorf("%v: Question %d: Answer not found or incorrect.\n", sc.TestName, incorrect)
+		sc.Dec()
+	}
+}
+
+func TestMultipleChoice3(t *testing.T) {
+	// This test aims to emulate that student submitted code may result in a panic,
+	// and thus a test failure along with a stack trace would be expected.
+	// Hence, we do not run this as part of the CI tests. To run, see instructions below.
+	failTest := os.Getenv(failTestEnvName)
+	if failTest == "" {
+		t.Skipf("Skipping; expected to fail. Run with: %s=1 go test -v -run %s", failTestEnvName, t.Name())
+	}
+
+	test := tests[3]
+	sc := scores.Max()
+	defer sc.Print(t)
+	answerFile := filepath.Join("..", "testdata", test.file)
+	answers, err := exercise.ParseMarkdownAnswers(answerFile)
+	if err != nil {
+		t.Error(err)
+	}
+	_, gotIncorrect := exercise.CheckMultipleChoice(answers, test.correct)
+	for _, incorrect := range gotIncorrect {
+		t.Errorf("%v: Question %d: Answer not found or incorrect.\n", sc.TestName, incorrect)
+		sc.Dec()
+	}
+}
+
+func TestMultipleChoiceWithFail(t *testing.T) {
+	// This test aims to emulate that student submitted code may result in a panic,
+	// and thus a test failure along with a stack trace would be expected.
+	// Hence, we do not run this as part of the CI tests. To run, see instructions below.
+	failTest := os.Getenv(failTestEnvName)
+	if failTest == "" {
+		t.Skipf("Skipping; expected to fail. Run with: %s=1 go test -v -run %s", failTestEnvName, t.Name())
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			exercise.MultipleChoice(t, filepath.Join("..", "testdata", test.file), test.qaMap)
+			sc := scores.MaxByName(t.Name())
+			defer sc.Print(t)
+			answerFile := filepath.Join("..", "testdata", test.file)
+			answers, err := exercise.ParseMarkdownAnswers(answerFile)
+			if err != nil {
+				t.Error(err)
+			}
+			_, gotIncorrect := exercise.CheckMultipleChoice(answers, test.correct)
+			for _, incorrect := range gotIncorrect {
+				t.Errorf("%v: Question %d: Answer not found or incorrect.\n", sc.TestName, incorrect)
+				sc.Dec()
+			}
 		})
 	}
 }
