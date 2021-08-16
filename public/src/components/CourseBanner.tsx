9b8448c6e2fa822caf1129e109e33b6a9ce6b0cd
JosteinLindhom <jossi2010@gmail.com>
2021-08-16 03:52:58 +0200

    Added button to enable teachers to switch between teacher and student view.

--- public/src/components/CourseBanner.tsx
+++ public/src/components/CourseBanner.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Enrollment } from "../../proto/ag/ag_pb";
-import { useActions } from "../overmind";
+import { useActions, useAppState } from "../overmind";
 
+
+// TODO: Maybe add route specific information, ex. if user is viewing a lab, show that in the banner. Could use state in components to display.
 
 const CourseBanner = ({enrollment}: {enrollment: Enrollment}) => {
+    const status = useAppState().status
     const actions = useActions()
     const style = enrollment.getState() === Enrollment.DisplayState.VISIBLE ? 'fa fa-star-o' : "fa fa-star "
     return (
@@ -16,6 +19,11 @@ const CourseBanner = ({enrollment}: {enrollment: Enrollment}) => {
                         </i>
                     </span>
                 </h1>
+                {status[enrollment.getCourseid()] === Enrollment.UserStatus.TEACHER && 
+                    <span style={{"cursor": "pointer"}} onClick={() => actions.changeView(enrollment.getCourseid())}>
+                        {enrollment.getStatus() === Enrollment.UserStatus.TEACHER ? "Switch to Student View" : "Switch to Teacher View" }
+                    </span>
+                }
             </div>
         </div>
     )
