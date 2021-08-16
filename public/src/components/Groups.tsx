2f9baa7aa3e838914f8fb4032ce791b6ed8e3a5c
JosteinLindhom <jossi2010@gmail.com>
2021-08-16 04:08:25 +0200

    Added editing group, deleting group, changing group status, along visual improvements for groups.

--- public/src/components/Groups.tsx
+++ public/src/components/Groups.tsx
@@ -1,44 +1,114 @@
-import React, { useEffect } from "react"
+import { json } from "overmind"
+import React, { useEffect, useState } from "react"
+import { Group } from "../../proto/ag/ag_pb"
 import { getCourseID } from "../Helpers"
-import { useActions, useAppState } from "../overmind"
-import Card from "./Card"
+import { useActions, useAppState, useGrpc } from "../overmind"
+import GroupForm from "./group/GroupForm"
+import Search from "./Search"
 
 /* Lists all groups for a given course. */
 export const Groups = () => {
     const state = useAppState()
     const actions = useActions()
+    const grpc = useGrpc()
     const courseID = getCourseID()
-    const style = {width: "40px", borderRadius: "50%", marginRight: "10px"}
+
+
+    const [editing, setEditing] = useState<Group>()
 
     useEffect(() => {
         actions.getGroupsByCourse(courseID)
-    }, [state.groups])
+    }, [state.groups, state.query])
 
-    // Generates JSX.Element array containing all groups for the course
-    const Groups = state.groups[courseID]?.map(group => {
-        return (
-            <ul key={group.getId()} className="list-group list-group-flush">
-                <li className="list-group-item">{group.getName()}</li>
+    const groupSearch = (group: Group) => {
+        // Show all groups if query is empty
+        if (state.query.length == 0) {
+            return false
+        }
+
+        // Show group if group name includes query
+        if (group.getName().toLowerCase().includes(state.query)) { 
+            return false
+        }
+        
+        // Show group if any group user includes query
+        for (const user of group.getUsersList()) {
+            if (user.getName().toLowerCase().includes(state.query)) {
+                return false
+            }
+        }
+        // Hide group if none of the above include query
+        return true
+    }
+
+    const deleteGroup = (group: Group) => {
+        if (confirm("Deleting a group is an irreversible action. Are you sure?")) {
+            actions.deleteGroup(group)
+        }
+
+    }
+
+    const updateGroupStatus = (group: Group, status: Group.GroupStatus) => {
+        actions.updateGroupStatus({group, status})
+    }
+
+    const GroupButtons = ({group}: {group: Group}) => {
+        if (group.getStatus() === Group.GroupStatus.PENDING) {
+            return (
                 <li className="list-group-item">
-                    {group.getStatus() == 0 ? "Pending" : "Approved"}
-                    <span className="badge badge-primary float-right">Approve</span>
+                        <span onClick={() => setEditing(group)}>EDIT</span>
+                        <span onClick={() => updateGroupStatus(group, Group.GroupStatus.APPROVED)} className="badge badge-primary float-right">Approve</span>
+                        <span onClick={() => deleteGroup(group)} className="badge badge-danger float-right">Delete</span>
                 </li>
-                { // Populates the unordered list with list elements for every user in the group
-                    group.getEnrollmentsList().map(enrol => 
+            )
+        }
+        return <li className="list-group-item"><span onClick={() => setEditing(group)}>EDIT</span></li>
+    }
+
+    const GroupList = ({group}: {group: Group}) => {
+        const style = {width: "40px", borderRadius: "50%", marginRight: "10px"}
+        const classname = group.getStatus() == Group.GroupStatus.APPROVED ? "list-group-item active" : "list-group-item list-group-item-warning"
+            return (
+                <><ul key={group.getId()} hidden={groupSearch(group)} className="list-group list-group-flush">
+                    <li className={classname}>
+                        {group.getName()}
+                        <span className="float-right badge badge-warning">{group.getStatus() == Group.GroupStatus.PENDING ? "Pending" : null}</span>
+                    </li>
+                    {// Populates the unordered list with list elements for every user in the group
+                        group.getEnrollmentsList().map(enrol => 
                         <li key={enrol.getId()} className="list-group-item">
                             <img src={enrol.getUser()?.getAvatarurl()} style={style}></img>
-                            {enrol.getUser()?.getName()} 
+                            {enrol.getUser()?.getName()}
                         </li>
-                )}
-            </ul>
-        )
+                        )}
+                    <GroupButtons group={group} />
+                </ul></>
+            )
+    }
+
+    // Generates JSX.Element array containing all groups for the course
+    const PendingGroups = state.groups[courseID]?.filter(g => g.getStatus() == Group.GroupStatus.PENDING).map(group => {
+        return <GroupList group={group} />
     })
+
+    const ApprovedGroups = state.groups[courseID]?.filter(g => g.getStatus() == Group.GroupStatus.APPROVED).map(group => {
+        return <GroupList group={group} />
+    })
+
+    if (editing) {
+        return <div className="box"><GroupForm editGroup={editing} setGroup={setEditing} /></div>
+    }
 
     return (
         <div className="box">
-            <div className="card well" style={{width: "400px"}}>
-                <div className="card-header">Groups</div>
-                {Groups}
+            <Search />
+            <div className="row">
+                <div className="col-sm-6">
+                    {ApprovedGroups}
+                </div>
+                <div className="col-sm-6">
+                    {PendingGroups}
+                </div>
             </div>
         </div>
     )
---
+++ public/src/components/group/GroupForm.tsx
@@ -0,0 +1,160 @@
+import { json } from "overmind/lib/utils"
+import React, { useEffect, useState } from "react"
+import { Enrollment, Group, User } from "../../../proto/ag/ag_pb"
+import { getCourseID, sortByField } from "../../Helpers"
+import { useActions, useAppState } from "../../overmind"
+import Search from "../Search"
+
+
+
+
+const GroupForm = ({editGroup, setGroup}: {editGroup?: Group, setGroup?: Function}) => {
+    const state = useAppState()
+    const actions = useActions()
+    const [users, setUsers] = useState<number[]>([])
+    const [name, setName] = useState<string>("Group Name")
+    const [query, setQuery] = useState<string>("")
+    const courseID = getCourseID()
+
+    const group = editGroup ? json(editGroup) : null
+
+    useEffect(() => {
+        // Add self to group if not teacher
+        if (!users.includes(state.self.getId()) && state.enrollmentsByCourseId[courseID].getStatus() != Enrollment.UserStatus.TEACHER) {
+            users.push(state.self.getId())
+            setUsers([...users])
+        }
+        if (group) {
+            users.splice(0,100)
+            for (const user of group.getUsersList()) {
+                users.push(user.getId())
+            }
+        }
+        actions.getEnrollmentsByCourse({courseID: courseID, statuses: [Enrollment.UserStatus.STUDENT, Enrollment.UserStatus.TEACHER]})
+    }, [group])
+
+
+    const search = (enrollment: Enrollment): boolean => {
+        if (enrollment.getUserid() in users || enrollment.hasGroup() && enrollment.getGroupid() != group?.getId()) {
+            return true
+        }
+        if (enrollment.getUser()) {
+            return !enrollment.getUser()?.getName().toLowerCase().includes(query)
+        } 
+        return false
+    }
+
+    const updateGroupUsers = (id: number) => {
+        if (id === state.self.getId()) { 
+            return 
+        }
+
+        if (group) {
+            let added = false
+            for (const user of group.getUsersList()) {
+                if (id == user.getId()) {
+                    group.getUsersList().splice(group.getUsersList().indexOf(user), 1)
+                    group.setUsersList(group.getUsersList())
+                    added = true
+                }
+            }
+            if (!added) {
+                const enrollment = state.courseEnrollments[courseID].find(enrollment => enrollment.getUserid() == id)
+                if (enrollment?.hasUser()) {
+                    group.getUsersList().push(json(enrollment).getUser() as User)
+                    group.setUsersList(group.getUsersList())
+                }
+            }
+
+        }
+        if (!users.includes(id)) {
+            users.push(id)
+            setUsers([...users])
+        }
+        else {
+            setUsers(users.filter(e => e != id))
+        }
+        
+    }
+
+    const updateGroupName = (input: string) => {
+        if (group) {
+            group.setName(input)
+        } 
+        else {
+            const name = input.length == 0 ? "Group Name" : input
+            setName(name)
+        }
+    }
+
+    const sortedEnrollments = sortByField(state.courseEnrollments[courseID], [Enrollment.prototype.getUser], User.prototype.getName) as Enrollment[]
+
+    const FreeUser = ({enrollment}: {enrollment: Enrollment}) => {
+        const id = enrollment.getUserid()
+        if (id !== state.self.getId() && !users.includes(id)) {
+            return (
+                <li hidden={search(enrollment)} key={id} className="list-group-item">
+                    {enrollment.getUser()?.getName()} 
+                    <i style={{float: "right", cursor:"pointer"}} onClick={() => updateGroupUsers(id)}>+</i>
+                </li>
+            )
+        }
+        return null
+    }
+
+    const GroupMember = ({enrollment}: {enrollment: Enrollment}) => {
+        const id = enrollment.getUserid()
+        if (users.includes(id)) {
+            return (
+                <li key={id} className="list-group-item">
+                    <img id="group-image" src={enrollment.getUser()?.getAvatarurl()}></img>
+                        {enrollment.getUser()?.getName()} 
+                    <i style={{float: "right", cursor:"pointer"}} onClick={() => updateGroupUsers(id)}>-</i>
+                </li>
+            )
+        }
+        return null
+    }
+
+    const GroupNameBanner = group ? <div className="card-header" style={{textAlign: "center"}}>{group.getName()}</div> : <div className="card-header" style={{textAlign: "center"}}>{name}</div>
+    const GroupNameInput = group && group.getStatus() === Group.GroupStatus.APPROVED ? null : <input placeholder={"Group Name:"} onKeyUp={e => updateGroupName(e.currentTarget.value)}></input>
+   
+    return(
+        <div className="container box"> 
+            <div className="row">
+                <div className="card well col-md-offset-2">
+                    <div className="card-header" style={{textAlign: "center"}}>
+                        Students
+                    </div>
+                    <Search placeholder={"Search"} setQuery={setQuery} />
+
+                    <ul className="list-group list-group-flush">
+                        {sortedEnrollments.map(enrollment => {
+                            return <FreeUser enrollment={enrollment} />
+                        })} 
+                    </ul>
+                </div>
+                
+                <div className='col'>
+                    <div className="card well col-md-offset-2" >
+                        {GroupNameBanner}
+                        {GroupNameInput}
+                        {state.courseEnrollments[courseID].map(enrollment => {
+                                return <GroupMember enrollment={enrollment} />
+                        })}
+                        {group && setGroup ? 
+                            <>
+                            <div className="btn btn-primary" onClick={() => actions.updateGroup(group)}> Update </div>
+                            <div className="btn btn-primary" onClick={() => setGroup(undefined)}> Cancel </div> 
+                            </>
+                            : 
+                            <div className="btn btn-primary" onClick={() => actions.createGroup({courseID: courseID, users: users, name: name})}> Create Group </div> 
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GroupForm
