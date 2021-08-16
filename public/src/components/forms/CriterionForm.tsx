4ccc00e33d39bc735235ba0e8856d3cb23797b3f
JosteinLindhom <jossi2010@gmail.com>
2021-08-16 04:29:51 +0200

    Added form for editing or creating benchmarks & initial stages of creating or revising reviews.

---
+++ public/src/components/forms/CriterionForm.tsx
@@ -0,0 +1,47 @@
+import { json } from "overmind/lib/utils";
+import React from "react";
+import { Assignment, GradingCriterion } from "../../../proto/ag/ag_pb";
+import { useActions, useGrpc } from "../../overmind";
+
+
+
+const CriterionForm = ({criterion, benchmarkID, assignment, setEditing}: {criterion?: GradingCriterion, benchmarkID?: number, assignment?: Assignment, setEditing: Function}) => {
+    
+    const grpc = useGrpc().grpcMan
+    const actions = useActions()
+    const newCriterion = new GradingCriterion()
+
+    if (benchmarkID) {
+        newCriterion.setBenchmarkid(benchmarkID)
+    }
+
+    const handleCriteria = (event: React.FormEvent<HTMLInputElement>) => {
+        const {name, value} = event.currentTarget
+        switch (name) {
+            case "criterion":
+                criterion ? criterion.setDescription(value) : newCriterion.setDescription(value)
+                break
+            case "points":
+                criterion ? criterion.setPoints(Number(value)) : newCriterion.setPoints(Number(value))
+                break
+            case "submit":
+                criterion ? grpc.updateCriterion(criterion) : actions.createCriterion({criterion: newCriterion, assignment: assignment as Assignment})
+                setEditing(-1)
+                break
+            default:
+                break
+        }
+    }
+
+    const submitText = criterion ? "Edit" : "Create"
+
+    return (
+        <>
+        <input type="text" defaultValue={criterion?.getDescription()} name="criterion" onKeyUp={e => {handleCriteria(e)}}></input>
+        <input type="number" defaultValue={criterion?.getPoints()} name="points" placeholder="Points" onKeyUp={e => {handleCriteria(e)}}></input>
+        <input type="submit" value={submitText} name="submit" onClick={e => handleCriteria(e)}></input>
+        </>
+    )
+}
+
+export default CriterionForm
---
+++ public/src/components/forms/ReviewForm.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react"
+import { Assignment, Review, Submission, SubmissionLink } from "../../../proto/ag/ag_pb"
+import { useAppState } from "../../overmind"
+
+
+// TODO: Ensure all criteria are graded before setting ready
+
+const ReviewForm = ({submissionLink, setSelected}: {submissionLink: SubmissionLink, setSelected: Function}) => {
+
+    const state = useAppState()
+
+    const [selectedReview, setSelectedReview] = useState<Review | undefined>(undefined)
+
+    useEffect(() => {
+        return setSelectedReview(undefined)
+    }, [submissionLink])
+
+    const countReadyReviews = (submission: Submission) => {
+        let total = 0
+        for (const review of submission.getReviewsList()) {
+            total = review.getReady() ? total++ : total
+        }
+        return total
+    }
+
+    const canTakeOver = (submission: Submission, assignment: Assignment) => {
+        if (submission.getStatus() === Submission.Status.REVISION || countReadyReviews(submission) < assignment.getReviewers()) {
+            return true;
+        }
+        return false;
+    }
+
+    const isAuthor = (reviews: Review[]) => {
+        for (const review of reviews) {
+            if (state.self.getId() === review.getReviewerid()) {
+                return true
+            }
+        }
+        return false
+    }
+
+    const Benchmarks = () => {
+        if (selectedReview) {
+            return selectedReview.getGradingbenchmarksList().map(bm => {
+                return <li>{bm.getHeading()}</li>
+            })
+        }
+        return <li>"No selected reviews"</li>
+    }
+
+    const Reviews = () => {
+        if (submissionLink.hasSubmission()) {
+            return (submissionLink.getSubmission() as Submission).getReviewsList().map(rw => {
+                return <li onClick={() => setSelectedReview(rw)}>{rw.getReviewerid()}</li>
+            })
+        }
+    }
+
+
+    if (submissionLink.hasAssignment() && submissionLink.getAssignment()?.getReviewers() == 0) {
+        return <div>This assignment is not for manual grading.</div>
+    }
+    return (
+        <ul>
+            {Reviews()}
+            {Benchmarks()}
+        </ul>
+    )
+}
+
+export default ReviewForm
