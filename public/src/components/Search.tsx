a21f95a83bae6154717bfb122ad65e4a9ac4b021
JosteinLindhom <jossi2010@gmail.com>
2021-08-16 03:46:10 +0200

    Added an optional argument for local component state search query.

--- public/src/components/Search.tsx
+++ public/src/components/Search.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react"
+import { Void } from "../../proto/ag/ag_pb"
 import { useActions } from "../overmind"
 
-export const Search = ({placeholder}: {placeholder?: string}) => {
+export const Search = ({placeholder, setQuery}: {placeholder?: string, setQuery?: Function}) => {
     const actions = useActions()
 
     useEffect(() => {
@@ -9,7 +10,13 @@ export const Search = ({placeholder}: {placeholder?: string}) => {
     }, [])
 
     return (
-        <input type={"text"} onFocus={() => actions.enableRedirect(false)} onBlur={() => actions.enableRedirect(true)} placeholder={placeholder ? placeholder : "Search"} onKeyUp={(e) => actions.setQuery(e.currentTarget.value.toLowerCase()) }></input>
+        <input  
+                type={"text"} 
+                onFocus={() => actions.enableRedirect(false)} 
+                onBlur={() => actions.enableRedirect(true)} 
+                placeholder={placeholder ? placeholder : "Search"} 
+                onKeyUp={(e) => setQuery ? setQuery(e.currentTarget.value.toLowerCase()) : actions.setQuery(e.currentTarget.value.toLowerCase()) }>
+        </input>
     )
 }
 
