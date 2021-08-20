e00df9e0b82cd0bd084411b8026f29251cb39a7a
JosteinLindhom <jossi2010@gmail.com>
2021-08-20 03:52:56 +0200

    Move button to lower right and remove status text for enrollment user has no status in.

--- dev/src/components/CourseCard.tsx
+++ dev/src/components/CourseCard.tsx
@@ -14,34 +14,41 @@ interface CardProps {
     status: number 
 }
 
-const cardcolor = [
+const CardColor = [
     "info", // "NONE in enrollment. Shouldn't ever appear."
     "secondary",
     "primary",
     "success"
 ]
 
-const CourseCard = (props: CardProps) => {
+const CourseCard = (props: CardProps): JSX.Element => {
     const actions = useActions()
     const history = useHistory()
 
     return (
         <div className="col-sm-4">
             <div className="card" style= {{maxWidth: "35rem", marginBottom:"10px",minHeight:"205px"}}>
-                <div className={"card-header bg-"+cardcolor[props.status]+" text-white"}>
+                <div className={"card-header bg-"+CardColor[props.status]+" text-white"}>
                     {props.course.getCode()}
-                    <span className="float-right"><i className={props.enrollment.getState() === Enrollment.DisplayState.VISIBLE ? 'fa fa-star-o' : "fa fa-star "} onClick={() => actions.setEnrollmentState(props.enrollment)}></i></span>
-                    <p className="float-sm-right">{props.enrollment ? EnrollmentStatus[props.enrollment?.getStatus()]  : ''}</p>
+                    {props.enrollment.getStatus() > Enrollment.UserStatus.NONE && 
+                    <>
+                        <span className="float-right">
+                            <i  className={props.enrollment.getState() === Enrollment.DisplayState.VISIBLE ? 'fa fa-star-o' : "fa fa-star "} 
+                                onClick={() => actions.setEnrollmentState(props.enrollment)}></i>
+                        </span>
+                        <p className="float-sm-right mr-2">{props.enrollment ? EnrollmentStatus[props.enrollment?.getStatus()]  : ''}</p>
+                    </>
+                    }
                 </div>
                 
-                <div className="card-body">
+                <div className="card-body position-relative">
                     <h5 className="card-title">{props.course.getName()} - {props.course.getYear()}/{props.course.getTag()}</h5>
                     { props.status === Enrollment.UserStatus.NONE ? 
-                        <div className="btn btn-primary float-down-left" onClick={() => actions.enroll(props.course.getId())}>Enroll to Course</div>
+                        <div className="btn btn-primary course-button" onClick={() => actions.enroll(props.course.getId())}>Enroll</div>
                     : props.status === Enrollment.UserStatus.PENDING ?
-                        null
+                        <div className="btn btn-secondary course-button disabled">Pending</div>
                     :
-                        <div className="btn btn-primary float-down-left" onClick={() => history.push("/course/"+props.enrollment.getCourseid())}>Go to Course</div>
+                        <div className="btn btn-primary course-button" onClick={() => history.push("/course/"+props.enrollment.getCourseid())}>Go to Course</div>
                     }
                 </div>
 
