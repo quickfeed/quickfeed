f9b9cc51b96680587d5671bcabde5b03110da8f1
Hein Meling <hein.meling@uis.no>
2021-08-23 23:25:56 +0200

    Removed useless GetRepo check and logging
    Turns out CreateRepository also checks if the repo exists, and if
    it does logs a message and returns the repo. Hence, we do not need
    to check using GetRepository also here.

--- web/scm_helpers.go
+++ web/scm_helpers.go
@@ -95,26 +95,15 @@ func deleteGroupRepoAndTeam(ctx context.Context, sc scm.SCM, repositoryID uint64
 
 // creates {username}-labs repository and provides pull/push access to it for the given student
 func createStudentRepo(ctx context.Context, sc scm.SCM, org *pb.Organization, path string, student string) (*scm.Repository, error) {
-	// we have to check that repository for given user has not already been created on github
+	// create repo, or return existing repo if it already exists
 	// if repo is found, it is safe to reuse it
-	repo, err := sc.GetRepository(ctx, &scm.RepositoryOptions{
-		Path:  path,
-		Owner: org.GetPath(),
+	repo, err := sc.CreateRepository(ctx, &scm.CreateRepositoryOptions{
+		Organization: org,
+		Path:         path,
+		Private:      true,
 	})
 	if err != nil {
-		fmt.Println("createStudentRepo: repo not found (as expected). Error: ", err.Error())
-	}
-
-	// if no github repository found, create it
-	if repo == nil {
-		repo, err = sc.CreateRepository(ctx, &scm.CreateRepositoryOptions{
-			Organization: org,
-			Path:         path,
-			Private:      true,
-		})
-		if err != nil {
-			return nil, fmt.Errorf("createStudentRepo: failed to create repo: %w", err)
-		}
+		return nil, fmt.Errorf("createStudentRepo: failed to create repo: %w", err)
 	}
 
 	// add push access to student repo
