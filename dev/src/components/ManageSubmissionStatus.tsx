714547b0d10d6f2746c009b310329d5192c4a1fb
JosteinLindhom <jossi2010@gmail.com>
2021-12-01 07:01:22 +0100

    Refactored and made only the button corresponding to the current submission status be color highlighted

--- dev/src/components/ManageSubmissionStatus.tsx
+++ dev/src/components/ManageSubmissionStatus.tsx
@@ -10,23 +10,31 @@ const ManageSubmissionStatus = (): JSX.Element => {
     const state = useAppState()
     const courseID = getCourseID()
 
+    // TODO: Add an "Are you sure you want to <Status> submission" prompt?
     const updateStatus = (status: Submission.Status) => {
         if (state.activeSubmission) {
             actions.updateSubmission({courseID: courseID, submission: state.activeSubmission, status: status})
         }
     }
+
+    const buttons: {text: string, status: Submission.Status, style: string}[] = [
+        {text: "Approved", status: Submission.Status.APPROVED, style: "col btn btn-primary mr-2"}, 
+        {text: "Revision", status: Submission.Status.REVISION, style: "col btn btn-warning mr-2"},
+        {text: "Rejected", status: Submission.Status.REJECTED, style: "col btn btn-danger mr-2"}
+    ]
+
+    const StautusButtons = buttons.map((button, index) => {
+        const style = state.activeSubmission?.getStatus() === button.status ? button.style : "col btn border mr-2"
+        return (
+            <div key={index} className={style} onClick={() => updateStatus(button.status)}>
+                {button.text}
+            </div>
+        )
+    })
     return (
         <div className="container">
             <div className="row m-auto">
-            <div className="col btn btn-primary mr-2" onClick={() => updateStatus(Submission.Status.APPROVED)}>
-                Approve
-            </div>
-            <div className="col btn btn-warning mr-2" onClick={() => { updateStatus(Submission.Status.REVISION) } }>
-                Revision
-            </div>
-            <div className="col btn btn-danger mr-2" onClick={() => { updateStatus(Submission.Status.REJECTED) } }>
-                Reject
-            </div>
+                {StautusButtons}
             </div>
         </div>
     )
