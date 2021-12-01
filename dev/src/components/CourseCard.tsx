2a43238ee514c84030cc719f8c6fa7e806c31a23
JosteinLindhom <jossi2010@gmail.com>
2021-12-01 10:35:49 +0100

    Destructuring of props

--- dev/src/components/CourseCard.tsx
+++ dev/src/components/CourseCard.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { useHistory } from 'react-router';
-
 import { EnrollmentStatus } from '../Helpers';
-
 import { useActions } from '../overmind';
 import { Course, Enrollment } from '../../proto/ag/ag_pb';
 
@@ -21,37 +19,36 @@ const CardColor = [
     "success"
 ]
 
-const CourseCard = (props: CardProps): JSX.Element => {
+const CourseCard = ({course, enrollment, status}: CardProps): JSX.Element => {
     const actions = useActions()
     const history = useHistory()
 
     return (
         <div className="col-sm-4">
             <div className="card" style= {{maxWidth: "35rem", marginBottom:"10px",minHeight:"205px"}}>
-                <div className={"card-header bg-"+CardColor[props.status]+" text-white"}>
-                    {props.course.getCode()}
-                    {props.enrollment.getStatus() > Enrollment.UserStatus.NONE && 
+                <div className={"card-header bg-"+CardColor[status]+" text-white"}>
+                    {course.getCode()}
+                    {enrollment.getStatus() > Enrollment.UserStatus.NONE && 
                     <>
                         <span className="float-right">
-                            <i  className={props.enrollment.getState() === Enrollment.DisplayState.VISIBLE ? 'fa fa-star-o' : "fa fa-star "} 
-                                onClick={() => actions.setEnrollmentState(props.enrollment)}></i>
+                            <i className={enrollment.getState() === Enrollment.DisplayState.VISIBLE ? 'fa fa-star-o' : "fa fa-star "} 
+                                onClick={() => actions.setEnrollmentState(enrollment)}></i>
                         </span>
-                        <p className="float-sm-right mr-2">{props.enrollment ? EnrollmentStatus[props.enrollment?.getStatus()]  : ''}</p>
+                        <p className="float-sm-right mr-2">{enrollment ? EnrollmentStatus[enrollment?.getStatus()]  : ''}</p>
                     </>
                     }
                 </div>
                 
                 <div className="card-body position-relative">
-                    <h5 className="card-title">{props.course.getName()} - {props.course.getYear()}/{props.course.getTag()}</h5>
-                    { props.status === Enrollment.UserStatus.NONE ? 
-                        <div className="btn btn-primary course-button" onClick={() => actions.enroll(props.course.getId())}>Enroll</div>
-                    : props.status === Enrollment.UserStatus.PENDING ?
+                    <h5 className="card-title">{course.getName()} - {course.getYear()}/{course.getTag()}</h5>
+                    { status === Enrollment.UserStatus.NONE ? 
+                        <div className="btn btn-primary course-button" onClick={() => actions.enroll(course.getId())}>Enroll</div>
+                    : status === Enrollment.UserStatus.PENDING ?
                         <div className="btn btn-secondary course-button disabled">Pending</div>
                     :
-                        <div className="btn btn-primary course-button" onClick={() => history.push("/course/"+props.enrollment.getCourseid())}>Go to Course</div>
+                        <div className="btn btn-primary course-button" onClick={() => history.push("/course/"+enrollment.getCourseid())}>Go to Course</div>
                     }
                 </div>
-
             </div>
         </div>
     )
