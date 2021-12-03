83c605b4c39cf934514bb4294ce5f7545d8f62b2
JosteinLindhom <jossi2010@gmail.com>
2021-12-03 09:18:46 +0100

    Enable hiding non-manual submissions.

--- dev/src/components/Review.tsx
+++ dev/src/components/Review.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react"
 import { useActions, useAppState } from "../overmind"
-import { Submission, SubmissionLink } from "../../proto/ag/ag_pb"
+import { SubmissionLink } from "../../proto/ag/ag_pb"
 import Lab from "./Lab"
-import { getCourseID } from "../Helpers"
+import { getCourseID, isManuallyGraded } from "../Helpers"
 import Search from "./Search"
 import { json } from "overmind"
-import ReviewForm from "./forms/ReviewForm"
 import ManageSubmissionStatus from "./ManageSubmissionStatus"
 
-
+// TODO: Refactor & come up with better visuals
 const Review = (): JSX.Element => {
     const state = useAppState()
     const actions = useActions()
@@ -16,7 +15,7 @@ const Review = (): JSX.Element => {
     const courseID = getCourseID()
 
     const [selected, setSelected] = useState<number>(0)
-    const [hideApproved, setHideApproved] = useState<boolean>(false)
+    const [hideNonManual, setHideNonManual] = useState<boolean>(false)
     const [selectedSubLink, setSelectedSubLink] = useState<SubmissionLink>()
 
     useEffect(() => {
@@ -31,14 +30,15 @@ const Review = (): JSX.Element => {
         const assignment = json(submissionLink.getAssignment())
 
         let reviews: JSX.Element | null = null
-
+        let className = "list-group-item"
         if (assignment) { 
-            reviews = assignment.getReviewers() > 0 ? <span className="float-right">{submission ? submission.getReviewsList().length : 0}/{assignment.getReviewers()}</span> : null
+            reviews = isManuallyGraded(assignment) ? <span className="float-right">{submission ? submission.getReviewsList().length : 0}/{assignment.getReviewers()}</span> : null
+            className = !isManuallyGraded(assignment) ? className + " list-group-item-secondary" : className
         }
         return (
-                <li className="list-group-item" 
+                <li className={className}
                     onClick={() => { actions.setActiveSubmission(json(submission)); setSelectedSubLink(submissionLink)}} 
-                    hidden={selected !== assignment?.getId() && selected !== 0 || hideApproved && submission?.getStatus() == Submission.Status.APPROVED}
+                    hidden={selected !== assignment?.getId() && selected !== 0 || hideNonManual && (assignment && !isManuallyGraded(assignment))}
                 >
                     <span>
                         {assignment?.getName()} - {submission?.getScore()} / 100
@@ -78,7 +78,7 @@ const Review = (): JSX.Element => {
                     <option value={0}>All Submissions</option>
                     {Options}
                 </select>
-                <input type={"checkbox"} checked={hideApproved} onChange={(e) => setHideApproved(e.target.checked)}></input>
+                <input type={"checkbox"} checked={hideNonManual} onChange={(e) => setHideNonManual(e.target.checked)}></input>
 
                 <button onClick={() => actions.getAllCourseSubmissions(courseID)}>Refresh ... </button>
                 <div className="row">
