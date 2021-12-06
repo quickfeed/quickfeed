1486a41364665887dc61b0e4a19a25b713fc4515
JosteinLindhom <jossi2010@gmail.com>
2021-12-06 11:38:49 +0100

    Fix formatting

--- dev/src/components/Search.tsx
+++ dev/src/components/Search.tsx
@@ -1,23 +1,29 @@
 import React, { useEffect } from "react"
 import { useActions } from "../overmind"
 
+//** Search */
+/**
+ *  This component updates either the supplied query state, or the query in Overmind state
+ *  Used to determine if elements in the component it is in should be hidden or not.
+ */
 export const Search = ({placeholder, setQuery}: {placeholder?: string, setQuery?: (e: unknown) => void}): JSX.Element => {
     const actions = useActions()
 
     useEffect(() => {
+        // Reset query in state when component loads
         return actions.setQuery("")
     }, [])
 
     return (
         <div className="input-group">
-        <input  
-                type={"text"} 
-                className="form-control"
-                onFocus={() => actions.enableRedirect(false)} 
-                onBlur={() => actions.enableRedirect(true)} 
-                placeholder={placeholder ? placeholder : "Search"} 
-                onKeyUp={(e) => setQuery ? setQuery(e.currentTarget.value.toLowerCase()) : actions.setQuery(e.currentTarget.value.toLowerCase()) }>
-        </input>
+            <input  
+                    type={"text"} 
+                    className="form-control"
+                    onFocus={() => actions.enableRedirect(false)} 
+                    onBlur={() => actions.enableRedirect(true)} 
+                    placeholder={placeholder ? placeholder : "Search"} 
+                    onKeyUp={(e) => setQuery ? setQuery(e.currentTarget.value.toLowerCase()) : actions.setQuery(e.currentTarget.value.toLowerCase()) }>
+            </input>
         </div>
     )
 }
