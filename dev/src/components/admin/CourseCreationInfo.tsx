dcbca7adca9c46f8d031f052dd5e8e4ce6253946
JosteinLindhom <jossi2010@gmail.com>
2021-12-06 17:25:13 +0100

    Only show course creation info while creating a course, not while editing one

---
+++ dev/src/components/admin/CourseCreationInfo.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+const CourseCreationInfo = (): JSX.Element => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">Create Course</h1>
+            <p className="lead">
+                For each new semester of a course, QuickFeed requires a new GitHub organization. This is to keep the student roster for the different runs of the course separate.
+            </p>
+            <p><a className="badge-pill badge-success" href="https://github.com/account/organizations/new">Create an organization</a>  for your course. The course organization must allow private repositories.</p>
+            <p>
+                QuickFeed will create the following repositories for you:
+            </p>
+            <ul>
+                <li>info</li>
+                <li>assignments</li>
+                <li>tests</li>
+            </ul>
+            <p>Please refer to the <a className="badge-pill badge-primary" href="https://github.com/autograde/quickfeed/blob/master/doc/teacher.md">documentation</a> for further instructions on how to work with the various repositories.</p>
+        </div>
+    )
+}
+
+export default CourseCreationInfo
--- dev/src/components/forms/CourseForm.tsx
+++ dev/src/components/forms/CourseForm.tsx
@@ -3,6 +3,7 @@ import { useActions } from "../../overmind"
 import { Course } from "../../../proto/ag/ag_pb"
 import { json } from "overmind"
 import FormInput from "./FormInput"
+import CourseCreationInfo from "../admin/CourseCreationInfo"
 
 
 
@@ -45,22 +46,7 @@ export const CourseForm = ({editCourse}: {editCourse?: Course}): JSX.Element => 
 
     return (
         <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-4">Create Course</h1>
-                <p className="lead">
-                For each new semester of a course, QuickFeed requires a new GitHub organization. This is to keep the student roster for the different runs of the course separate.
-                </p>
-                <p><a className="badge-pill badge-success" href="https://github.com/account/organizations/new">Create an organization</a>  for your course. The course organization must allow private repositories.</p>
-                <p>
-                QuickFeed will create the following repositories for you:
-                </p>
-                <ul>
-                    <li>info</li>
-                    <li>assignments</li>
-                    <li>tests</li>
-                </ul>
-                <p>Please refer to the <a className="badge-pill badge-primary" href="https://github.com/autograde/quickfeed/blob/master/doc/teacher.md">documentation</a> for further instructions on how to work with the various repositories.</p>
-            </div>
+            {editCourse ? <></> : <CourseCreationInfo />}
             <div className="row" hidden={editCourse ? true : false}>
                 <div className="col input-group mb-3">
                     <div className="input-group-prepend">
