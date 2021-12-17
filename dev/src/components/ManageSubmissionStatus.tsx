bb6209a20ec5afd9fff81b13bb7dca912774583a
JosteinLindhom <jossi2010@gmail.com>
2021-12-17 10:42:01 +0100

    Remove redundant function

--- dev/src/components/ManageSubmissionStatus.tsx
+++ dev/src/components/ManageSubmissionStatus.tsx
@@ -7,13 +7,6 @@ const ManageSubmissionStatus = (): JSX.Element => {
     const actions = useActions()
     const state = useAppState()
 
-    // TODO: Add an "Are you sure you want to <Status> submission" prompt?
-    const updateStatus = (status: Submission.Status) => {
-        if (state.activeSubmission) {
-            actions.updateSubmission(status)
-        }
-    }
-
     const buttons: {text: string, status: Submission.Status, style: string, onClick?: () => void}[] = [
         {text: "Approve", status: Submission.Status.APPROVED, style: "primary"}, 
         {text: "Revision", status: Submission.Status.REVISION, style: "warning"},
@@ -25,16 +18,14 @@ const ManageSubmissionStatus = (): JSX.Element => {
         const style = state.activeSubmission?.getStatus() === button.status ? `col btn btn-${button.style} mr-2` : `col btn btn-outline-${button.style} mr-2`
         // TODO: Perhaps refactor button into a separate general component to enable reuse
         return (
-            <div key={index} className={style} onClick={() => button.onClick ? button.onClick() : updateStatus(button.status)}>
+            <div key={index} className={style} onClick={() => button.onClick ? button.onClick() : actions.updateSubmission(button.status)}>
                 {button.text}
             </div>
         )
     })
     return (
-        <div className="container">
-            <div className="row m-auto">
-                {StautusButtons}
-            </div>
+        <div className="row m-auto">
+            {StautusButtons}
         </div>
     )
 }
