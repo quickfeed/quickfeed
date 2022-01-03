fe5b9b8f6be2bce760969f213bd0d1cff6b1682f
JosteinLindhom <jossi2010@gmail.com>
2022-01-03 23:42:22 +0100

    Remove unused code. Left empty for now

--- dev/src/components/Statistics.tsx
+++ dev/src/components/Statistics.tsx
@@ -1,50 +1,6 @@
 import React from "react"
-import {Bar} from 'react-chartjs-2'
-import { useParams } from "react-router-dom";
-import { Submission } from "../../proto/ag/ag_pb";
-import { useAppState } from "../overmind";
 
-const StatisticsView = () => {
-    const course = useParams<{id?: string}>()
-    const courseID = Number(course.id)
-    let d: number[] = [0, 0]
-    const state = useAppState()
-    const extractData = () => {
-        state.courseSubmissions[courseID]?.forEach(link => {
-            link.submissions?.forEach(s => {
-                if (!s.getSubmission()) {
-                    return
-                }
-                let c = s.getSubmission()?.getStatus()
-                if (c === Submission.Status.APPROVED) {
-                    d[0] += 1
-                }
-                else {
-                    d[1] += 1
-                }
-            })
-        })
-    }
-    extractData()
-    let data = {
-        labels: ['Approved', 'Not Approved'],
-        datasets: [
-          {
-            label: '# Approved',
-            data: d,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-    };
-
+const StatisticsView = (): JSX.Element => {
     return (
         <div>
             
