745314da1f2eb4945b29a208d2c770b6ca3db3ad
Quickfeed shared account <quickfeed@itest.run>
2022-01-04 15:00:05 +0100

    Disabled hack for approvelist

--- web/autograder_service.go
+++ web/autograder_service.go
@@ -546,9 +546,9 @@ func (s *AutograderService) GetSubmissionsByCourse(ctx context.Context, in *pb.S
 	}
 	// TODO(meling) This is a hack to give access to cmd/approvelist via the root usr admin
 	// Normally, the root admin should not have access to submissions for all courses.
-	if usr.IsAdmin {
-		goto BYPASS
-	}
+	//	if usr.IsAdmin {
+	//		goto BYPASS
+	//	}
 
 	// ensure that current user is teacher or enrolled admin to process the submission request
 	if !s.hasCourseAccess(usr.GetID(), in.GetCourseID(), func(e *pb.Enrollment) bool {
@@ -563,7 +563,7 @@ func (s *AutograderService) GetSubmissionsByCourse(ctx context.Context, in *pb.S
 		s.logger.Errorf("GetSubmissionsByCourse failed: user %s is not teacher or submission author", usr.GetLogin())
 		return nil, status.Error(codes.PermissionDenied, "only teachers can get all lab submissions")
 	}
-BYPASS:
+	//BYPASS:
 	s.logger.Debugf("GetSubmissionsByCourse: %v", in)
 
 	courseLinks, err := s.getAllCourseSubmissions(in)
