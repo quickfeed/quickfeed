ec0939c3f8a256dc23b88d22fbba91c1783029e8
JosteinLindhom <jossi2010@gmail.com>
2022-01-07 06:29:23 +0100

    Use state rather than function to determine if user info is valid

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useAppState, useActions } from './overmind'
 import NavBar from "./components/NavBar";
 import { Switch, Route } from 'react-router-dom';
 import Profile from "./components/Profile";
 import CoursePage from "./pages/CoursePage"
 import Courses from "./components/Courses";
-import Alert from './components/Alert';
 import AdminPage from './pages/AdminPage';
 import Loading from './components/Loading';
-import { isValid } from './Helpers';
 import Dashboard from './components/Dashboard';
 import AboutPage from './pages/AboutPage';
 
@@ -24,6 +22,7 @@ const App = (): JSX.Element => {
         async function setup() {
             await actions.fetchUserData()
         }
+        // If the user is not logged in, fetch user data to initialize the app state.
         if (!state.isLoggedIn) {
             setup()
         }
@@ -45,7 +44,7 @@ const App = (): JSX.Element => {
                     {state.isLoading ? ( // If state.isLoading
                         <Loading />
                         ) : ( // Else if, user logged in, but has not added their information redirect to Profile
-                            !isValid(state.self) && state.isLoggedIn ? (
+                            !state.isValid && state.isLoggedIn ? (
                                 <Switch>
                                     <Route path="/" component={Profile} />
                                     <Route path="/profile" component={Profile} />
