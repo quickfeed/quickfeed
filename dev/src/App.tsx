742758895a58dbcda1c6c5f539107a8507277419
JosteinLindhom <jossi2010@gmail.com>
2022-01-10 13:30:56 +0100

    If-else to determine which routes are available to the user.

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -9,16 +9,13 @@ import AdminPage from './pages/AdminPage';
 import Loading from './components/Loading';
 import Dashboard from './components/Dashboard';
 import AboutPage from './pages/AboutPage';
-
-
-
-
+import { isValid } from './Helpers';
 
 const App = (): JSX.Element => {
     const state = useAppState()
     const actions = useActions()
-    
-    useEffect( () => {
+
+    useEffect(() => {
         async function setup() {
             await actions.fetchUserData()
         }
@@ -28,41 +25,55 @@ const App = (): JSX.Element => {
         }
     }, [])
 
-    // This is just to Update the Time object in state, every 20 minutes (after mount, it mounts with a new dateobject)
-    useEffect(()=> {
-        const updateDateNow = setInterval(()=>{
+    // Update the state's Time object every 20 seconds.
+    // After component mounts, it uses a new Time object
+    useEffect(() => {
+        const updateDateNow = setInterval(() => {
             actions.setTimeNow()
-        },1200000)
-        return() => clearInterval(updateDateNow)
-    },[])
+        }, 1200000)
+        return () => clearInterval(updateDateNow)
+    }, [])
 
-    return  (
-        <div> 
+    const Main = () => {
+        // Determine which routes are available to the user depending on the state
+        if (state.isLoading) {
+            return <Loading />
+        } else if (!isValid(state.self) && state.isLoggedIn) {
+            // user logged in without profile information: redirect to Profile page
+            return (
+                <Switch>
+                    <Route path="/" component={Profile} />
+                    <Route path="/profile" component={Profile} />
+                </Switch>
+            )
+        } else if (state.isLoggedIn) {
+            // user logged in: show Dashboard page
+            return (
+                <Switch>
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/about" component={AboutPage} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/course/:id" component={CoursePage} />
+                    <Route path="/courses" exact component={Courses} />
+                    <Route path="/admin" component={AdminPage} />
+                </Switch>
+            )
+        } else {
+            //  user not logged in: show About page
+            return (
+                <Switch>
+                    <Route path="/" component={AboutPage} />
+                </Switch>
+            )
+        }
+    }
+
+    return (
+        <div>
             <NavBar />
             <div className="app wrapper">
                 <div id="content">
-                    {state.isLoading ? ( // If state.isLoading
-                        <Loading />
-                        ) : ( // Else if, user logged in, but has not added their information redirect to Profile
-                            !state.isValid && state.isLoggedIn ? (
-                                <Switch>
-                                    <Route path="/" component={Profile} />
-                                    <Route path="/profile" component={Profile} />
-                                </Switch>
-                        ) : (state.isLoggedIn ? ( // Else render page as expected for a logged in user
-                            <Switch>
-                                <Route path="/" exact component={Dashboard} />
-                                <Route path="/about" component={AboutPage} />
-                                <Route path="/profile" component={Profile} />
-                                <Route path="/course/:id" component={CoursePage} />
-                                <Route path="/courses" exact component={Courses} />
-                                <Route path="/admin" component={AdminPage} />
-                            </Switch>
-                        ) : (
-                            <Switch>
-                                <Route path="/" component={AboutPage} />
-                            </Switch>
-                    )))}
+                    {Main()}
                 </div>
             </div>
         </div>
