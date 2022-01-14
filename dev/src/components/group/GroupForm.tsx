c7e2ff9660b2e9e128db0433da2ca2c84badbbfc
JosteinLindhom <jossi2010@gmail.com>
2022-01-14 05:27:41 +0100

    GroupForm finally makes some sense - revamped logic
    Formatting to make it more readable

--- dev/src/components/group/GroupForm.tsx
+++ dev/src/components/group/GroupForm.tsx
@@ -1,12 +1,9 @@
 import { json } from "overmind/lib/utils"
 import React, { useEffect, useState } from "react"
 import { Enrollment, Group, User } from "../../../proto/ag/ag_pb"
-import { getCourseID, isHidden, isStudent, sortByField } from "../../Helpers"
+import { getCourseID, isApprovedGroup, isHidden, isStudent, sortByField } from "../../Helpers"
 import { useActions, useAppState } from "../../overmind"
 import Search from "../Search"
-
-
-
 
 const GroupForm = ({ editGroup, setGroup }: { editGroup?: Group, setGroup?: React.Dispatch<React.SetStateAction<Group | undefined>> }): JSX.Element => {
     const state = useAppState()
@@ -20,12 +17,12 @@ const GroupForm = ({ editGroup, setGroup }: { editGroup?: Group, setGroup?: Reac
 
     useEffect(() => {
         // Add self to group if not teacher
-        if (!users.includes(state.self.getId()) && isStudent(state.enrollmentsByCourseId[courseID])) {
+        if (!users.includes(state.self.getId()) && isStudent(state.enrollmentsByCourseID[courseID])) {
             users.push(state.self.getId())
             setUsers([...users])
         }
         if (group) {
-            users.splice(0, 100)
+            users.splice(0, users.length)
             for (const user of group.getUsersList()) {
                 users.push(user.getId())
             }
@@ -44,34 +41,38 @@ const GroupForm = ({ editGroup, setGroup }: { editGroup?: Group, setGroup?: Reac
         return false
     }
 
+    const indexOf = (users: User[], id: number): number => {
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].getId() === id) {
+                return i
+            }
+        }
+        return -1
+    }
+
     const updateGroupUsers = (id: number) => {
         if (id === state.self.getId()) {
             return
         }
 
         if (group) {
-            let added = false
-            for (const user of group.getUsersList()) {
-                if (id == user.getId()) {
-                    group.getUsersList().splice(group.getUsersList().indexOf(user), 1)
-                    group.setUsersList(group.getUsersList())
-                    added = true
-                }
-            }
-            if (!added) {
+            const userList = group.getUsersList()
+            const index = indexOf(userList, id)
+            if (index >= 0) {
+                // Remove user with id from group
+                userList.splice(index, 1)
+            } else {
+                // User with id not found in group; add user
                 const enrollment = state.courseEnrollments[courseID].find(enrollment => enrollment.getUserid() == id)
-                if (enrollment?.hasUser()) {
-                    group.getUsersList().push(json(enrollment).getUser() as User)
-                    group.setUsersList(group.getUsersList())
-                }
+                const user = enrollment?.hasUser() ? enrollment.getUser() : null
+                userList.push(user as User)
             }
-
+            group.setUsersList(userList)
         }
         if (!users.includes(id)) {
             users.push(id)
             setUsers([...users])
-        }
-        else {
+        } else {
             setUsers(users.filter(e => e != id))
         }
 
@@ -88,7 +89,7 @@ const GroupForm = ({ editGroup, setGroup }: { editGroup?: Group, setGroup?: Reac
 
     const sortedEnrollments = sortByField(state.courseEnrollments[courseID], [Enrollment.prototype.getUser], User.prototype.getName) as Enrollment[]
 
-    const FreeUser = ({ enrollment }: { enrollment: Enrollment }) => {
+    const AvailableUser = ({ enrollment }: { enrollment: Enrollment }) => {
         const id = enrollment.getUserid()
         if (id !== state.self.getId() && !users.includes(id)) {
             return (
@@ -115,8 +116,12 @@ const GroupForm = ({ editGroup, setGroup }: { editGroup?: Group, setGroup?: Reac
         return null
     }
 
-    const GroupNameBanner = group ? <div className="card-header" style={{ textAlign: "center" }}>{group.getName()}</div> : <div className="card-header" style={{ textAlign: "center" }}>{name}</div>
-    const GroupNameInput = group && group.getStatus() === Group.GroupStatus.APPROVED ? null : <input placeholder={"Group Name:"} onKeyUp={e => updateGroupName(e.currentTarget.value)}></input>
+    const GroupNameBanner = group
+        ? <div className="card-header" style={{ textAlign: "center" }}>{group.getName()}</div>
+        : <div className="card-header" style={{ textAlign: "center" }}>{name}</div>
+    const GroupNameInput = group && isApprovedGroup(group)
+        ? null
+        : <input placeholder={"Group Name:"} onKeyUp={e => updateGroupName(e.currentTarget.value)}></input>
 
     return (
         <div className="container">
@@ -129,7 +134,7 @@ const GroupForm = ({ editGroup, setGroup }: { editGroup?: Group, setGroup?: Reac
 
                     <ul className="list-group list-group-flush">
                         {sortedEnrollments.map((enrollment, index) => {
-                            return <FreeUser key={index} enrollment={enrollment} />
+                            return <AvailableUser key={index} enrollment={enrollment} />
                         })}
                     </ul>
                 </div>
