4239d575af9b266d871726140c6fc23615f3fbf2
JosteinLindhom <jossi2010@gmail.com>
2022-01-14 05:35:15 +0100

    Simplified, modified handleCriteria Added deleteCriterion Fixed some logic li -> div

--- dev/src/components/teacher/EditBenchmark.tsx
+++ dev/src/components/teacher/EditBenchmark.tsx
@@ -1,40 +1,38 @@
-import React, { useState } from "react";
-import { Assignment, GradingBenchmark } from "../../../proto/ag/ag_pb";
-import { useActions, useGrpc } from "../../overmind";
+import { json } from "overmind"
+import React, { useState } from "react"
+import { Assignment, GradingBenchmark } from "../../../proto/ag/ag_pb"
+import { useActions, useGrpc } from "../../overmind"
 
 
 
 const EditBenchmark = ({ children, benchmark, assignment }: { children?: React.ReactNode, benchmark?: GradingBenchmark, assignment: Assignment }): JSX.Element => {
-
     const grpc = useGrpc().grpcMan
     const actions = useActions()
-    const bm = benchmark ? benchmark : new GradingBenchmark()
+    const bm = json(benchmark) ?? new GradingBenchmark()
 
     const [editing, setEditing] = useState<boolean>(false)
 
     const handleBenchmark = (event: React.KeyboardEvent<HTMLInputElement>) => {
         const { value } = event.currentTarget
         if (event.key === "Enter") {
-            if (bm.getId()) {
+            if (bm.getId() > 0) {
                 grpc.updateBenchmark(bm)
             } else {
-                actions.createBenchmark({ benchmark: bm, assignment: assignment as Assignment })
+                actions.createBenchmark({ benchmark: bm, assignment: assignment })
             }
         } else {
             bm.setHeading(value)
         }
-
-
     }
 
     return (
         <>
-            <li className="list-group-item list-group-item-primary">
-                {editing ?
-                    <input className="form-control" type="text" defaultValue={bm?.getHeading()} onClick={() => setEditing(true)} onKeyUp={e => { handleBenchmark(e) }}></input>
+            <div className="list-group-item list-group-item-primary">
+                {editing
+                    ? <input className="form-control" type="text" defaultValue={bm?.getHeading()} onClick={() => setEditing(true)} onKeyUp={e => { handleBenchmark(e) }}></input>
                     : <span onClick={() => setEditing(true)}>{bm?.getHeading()}</span>
                 }
-            </li>
+            </div>
             {children}
         </>
     )
--- dev/src/components/teacher/EditCriterion.tsx
+++ dev/src/components/teacher/EditCriterion.tsx
@@ -1,52 +1,45 @@
-import React, { useState } from "react";
-import { Assignment, GradingCriterion } from "../../../proto/ag/ag_pb";
-import { useActions, useGrpc } from "../../overmind";
-
+import React, { useState } from "react"
+import { Assignment, GradingCriterion } from "../../../proto/ag/ag_pb"
+import { useActions } from "../../overmind"
 
 
 const EditCriterion = ({ criterion, benchmarkID, assignment }: { criterion?: GradingCriterion, benchmarkID: number, assignment: Assignment }): JSX.Element => {
-    const grpc = useGrpc().grpcMan
     const actions = useActions()
 
     const [editing, setEditing] = useState<boolean>(false)
     const [add, setAdd] = useState<boolean>(criterion ? false : true)
 
-    const c = criterion ? criterion : new GradingCriterion()
+    const c = criterion ?? new GradingCriterion().setBenchmarkid(benchmarkID)
 
-    const handleCriteria = (event: React.FormEvent<HTMLInputElement>) => {
-        console.log(c.getDescription())
-        const { name, value } = event.currentTarget
-        switch (name) {
-            case "criterion":
-                c.setDescription(value)
-                break
-            case "submit":
-                c.getId() ? grpc.updateCriterion(c) : actions.createCriterion({ criterion: c, assignment: assignment })
-                break
-            default:
-                break
+    const handleCriteria = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const { value } = event.currentTarget
+        if (event.key === "Enter") {
+            actions.createOrUpdateCriterion({ criterion: c, assignment: assignment })
+            event.currentTarget.blur()
+        } else {
+            c.setDescription(value)
         }
-        //if (!c.getId() && )
     }
 
     if (add) {
         return (
-            <li className="list-group-item">
+            <div className="list-group-item">
                 <button className="btn btn-primary" name="submit" onClick={() => { setAdd(false); setEditing(true) }}>Add</button>
-            </li>
+            </div>
         )
     }
 
     return (
-        <li className="list-group-item" onClick={() => setEditing(!editing)}>
-            {editing ?
-                <input className="form-control" type="text" onBlur={() => setEditing(false)} autoFocus defaultValue={c?.getDescription()} name="criterion" onKeyUp={e => { handleCriteria(e) }}></input>
-                : <>
+        <div className="list-group-item" onClick={() => setEditing(!editing)}>
+            {editing
+                ? <input className="form-control" type="text" onBlur={() => { setEditing(false) }} autoFocus defaultValue={c?.getDescription()} name="criterion" onKeyUp={e => { handleCriteria(e) }}></input>
+                :
+                <>
                     <span>{c.getDescription()}</span>
-                    <span className="float-right">Delete</span>
+                    <span className="float-right" onClick={() => actions.deleteCriterion({ criterion: criterion, assignment: assignment })}>Delete</span>
                 </>
             }
-        </li>
+        </div>
     )
 }
 
