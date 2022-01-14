d44d94acede4ecba2cef1f7a2367a283e00c4c18
JosteinLindhom <jossi2010@gmail.com>
2022-01-14 02:41:36 +0100

    Added newlines & update link to point to '/quickfeed/quickfeed' rather than '/autograde/quickfeed'

--- dev/src/components/admin/CourseCreationInfo.tsx
+++ dev/src/components/admin/CourseCreationInfo.tsx
@@ -6,9 +6,15 @@ const CourseCreationInfo = (): JSX.Element => {
         <div className="jumbotron">
             <h1 className="display-4">Create Course</h1>
             <p className="lead">
-                For each new semester of a course, QuickFeed requires a new GitHub organization. This is to keep the student roster for the different runs of the course separate.
+                For each new semester of a course, QuickFeed requires a new GitHub organization.
+                This is to keep the student roster for the different runs of the course separate.
             </p>
-            <p><a className="badge-pill badge-success" href="https://github.com/account/organizations/new">Create an organization</a>  for your course. The course organization must allow private repositories.</p>
+            <p>
+                <a className="badge-pill badge-success" href="https://github.com/account/organizations/new">
+                    Create an organization
+                </a> for your course.
+                The course organization must allow private repositories.
+            </p>
             <p>
                 QuickFeed will create the following repositories for you:
             </p>
@@ -17,7 +23,13 @@ const CourseCreationInfo = (): JSX.Element => {
                 <li>assignments</li>
                 <li>tests</li>
             </ul>
-            <p>Please refer to the <a className="badge-pill badge-primary" href="https://github.com/autograde/quickfeed/blob/master/doc/teacher.md">documentation</a> for further instructions on how to work with the various repositories.</p>
+            <p>
+                <span>Please refer to the </span>
+                <a className="badge-pill badge-primary" href="https://github.com/quickfeed/quickfeed/blob/master/doc/teacher.md">
+                    documentation
+                </a>
+                <span> for further instructions on how to work with the various repositories.</span>
+            </p>
         </div>
     )
 }
