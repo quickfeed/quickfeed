6ba31c0fa92824f58abf2c0929e7e804b2277c18
JosteinLindhom <jossi2010@gmail.com>
2022-01-14 05:20:10 +0100

    Simplified getBuildLog & fixed comments

--- dev/src/components/Lab.tsx
+++ dev/src/components/Lab.tsx
@@ -1,4 +1,3 @@
-import { json } from 'overmind'
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router'
 import { Assignment, Submission } from '../../proto/ag/ag_pb'
@@ -14,11 +13,8 @@ interface MatchProps {
     lab: string
 }
 
-
-
-/** Displays a Lab submission based on the /course/:id/:lab route
- *
- *  If used to display a lab for grading purposes, pass in a TeacherLab object
+/** Lab displays a submission based on the /course/:id/:lab route if the user is a student.
+ *  If the user is a teacher, Lab displays a submission based on the submission in state.currentSubmission.
  */
 const Lab = (): JSX.Element => {
 
@@ -34,27 +30,25 @@ const Lab = (): JSX.Element => {
         }
     }, [lab])
 
-
     const Lab = () => {
         let submission: Submission | undefined
         let assignment: Assignment | undefined
 
-        // If used for grading purposes, retrieve submission from courseSubmissions
         if (state.isTeacher) {
+            // If used for grading purposes, retrieve submission from state.currentSubmission
             submission = state.currentSubmission
             assignment = state.assignments[courseID].find(a => a.getId() === submission?.getAssignmentid())
-        }
-        // Retreive personal submission
-        else {
+        } else {
+            // Retrieve the student's submission
             submission = state.submissions[courseID]?.find(s => s.getAssignmentid() === assignmentID)
             assignment = state.assignments[courseID]?.find(a => a.getId() === assignmentID)
         }
 
-        // Confirm both assignment and submission exists before attempting to render
         if (assignment && submission) {
+            // Confirm both assignment and submission exists before attempting to render
             const review = hasReviews(submission) ? submission.getReviewsList() : []
             let buildLog: JSX.Element[] = []
-            const buildLogRaw = submission.hasBuildinfo() ? (submission.getBuildinfo() as BuildInfo).getBuildlog() : null
+            const buildLogRaw = submission.getBuildinfo()?.getBuildlog()
             if (buildLogRaw) {
                 buildLog = buildLogRaw.split("\n").map((x: string, i: number) => <span key={i} >{x}<br /></span>)
             }
