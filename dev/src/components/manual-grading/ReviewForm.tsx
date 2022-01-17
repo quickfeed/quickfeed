5733bb3dff8b33c0fe5438f6d50f9807cf6bd177
JosteinLindhom <jossi2010@gmail.com>
2022-01-17 21:57:44 +0100

    Hide TA reviews from other TAs, teacher can view all

--- dev/src/components/manual-grading/ReviewForm.tsx
+++ dev/src/components/manual-grading/ReviewForm.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react"
-import { Assignment, Review, Submission } from "../../../proto/ag/ag_pb"
+import React from "react"
+import { Assignment, Review } from "../../../proto/ag/ag_pb"
 import { getCourseID, isManuallyGraded, Color } from "../../Helpers"
 import { useActions, useAppState } from "../../overmind"
 import Button, { ButtonType } from "../admin/Button"
@@ -7,27 +7,10 @@ import ReviewInfo from "./ReviewInfo"
 import ReviewResult from "../ReviewResult"
 
 
-// TODO: Figure out who to hide reviews from
-
 const ReviewForm = (): JSX.Element => {
     const state = useAppState()
     const actions = useActions()
     const courseID = getCourseID()
-
-    const selectReview = () => {
-        if (state.activeSubmission) {
-            const reviews = state.review.reviews[courseID][state.activeSubmission]
-            if (reviews.length > 0) {
-                actions.review.setSelectedReview(0)
-            }
-        }
-    }
-
-    useEffect(() => {
-        if (state.activeSubmissionLink) {
-            selectReview()
-        }
-    }, [])
 
     if (!state.activeSubmissionLink) {
         return <div>None</div>
@@ -37,53 +20,43 @@ const ReviewForm = (): JSX.Element => {
         return <div>No Submission</div>
     }
 
-    const countReadyReviews = (submission: Submission) => {
-        let total = 0
-        for (const review of submission.getReviewsList()) {
-            total = review.getReady() ? total++ : total
-        }
-        return total
+    const isAuthor = (review: Review) => {
+        console.log("Checking author")
+        return review?.getReviewerid() === state.self.getId()
     }
 
-    const canTakeOver = (submission: Submission, assignment: Assignment) => {
-        if (submission.getStatus() === Submission.Status.REVISION || countReadyReviews(submission) < assignment.getReviewers()) {
-            return true
-        }
-        return false
-    }
-
-    const isAuthor = (reviews: Review[]) => {
-        for (const review of reviews) {
-            if (state.self.getId() === review.getReviewerid()) {
-                return true
-            }
-        }
-        return false
-    }
-
-    const reviews = state.review.reviews[courseID][state.activeSubmission]
-
+    const reviewers = state.activeSubmissionLink?.getAssignment()?.getReviewers() ?? 0
+    const isCourseCreator = state.courses[courseID].getCoursecreatorid() === state.self.getId()
+    const reviews = state.review.reviews[courseID][state.activeSubmission] ?? []
     const selectReviewButton: JSX.Element[] = []
-    for (let i = 0; i < (state.activeSubmissionLink.getAssignment()?.getReviewers() ?? 0); i++) {
-        if (reviews && reviews[i]) {
+
+    reviews.forEach((review, index) => {
+        if (isCourseCreator || isAuthor(review)) {
+            // Teaching assistants can only select their own reviews, and course creators can select any review
             selectReviewButton.push(
-                <Button key={i} onclick={() => { actions.review.setSelectedReview(i) }}
-                    classname={`mr-1 ${state.review.selectedReview === i ? "active border border-dark" : ""}`}
-                    text={reviews[i].getReady() ? "Ready" : "In Progress"}
-                    color={reviews[i].getReady() ? Color.GREEN : Color.YELLOW}
-                    type={ButtonType.BUTTON}
-                />
+                <Button key={review.getId()} onclick={() => { actions.review.setSelectedReview(index) }}
+                    classname={`mr-1 ${state.review.selectedReview === index ? "active border border-dark" : ""}`}
+                    text={review.getReady() ? "Ready" : "In Progress"}
+                    color={review.getReady() ? Color.GREEN : Color.YELLOW}
+                    type={ButtonType.BUTTON} />
             )
-        } else {
-            selectReviewButton.push(<Button classname="mr-1" key={i} type={ButtonType.BUTTON} color={Color.BLUE} text="Create a new review" onclick={() => actions.review.setSelectedReview(i)} />)
         }
+    })
+
+    if ((reviews.length === 0 || reviews.some(review => !isAuthor(review))) && (reviewers - reviews.length) > 0) {
+        // Display a button to create a new reviews if:
+        // there are no reviews or the current user is not the author of the review, and there are still available review slots
+        selectReviewButton.push(
+            <Button key="add" onclick={async () => { await actions.review.createReview() }}
+                classname="mr-1" text="Add Review" color={Color.BLUE} type={ButtonType.BUTTON} />
+        )
     }
 
     if (!isManuallyGraded(state.activeSubmissionLink.getAssignment() as Assignment)) {
         return <div>This assignment is not for manual grading.</div>
     } else {
         return (
-            <div className="col reviewLab">
+            <div className="col reviewLab reviewLabResult">
                 <div className="mb-1">
                     {selectReviewButton}
                 </div>
