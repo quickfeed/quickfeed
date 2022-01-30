6d6a8c367b214b26417f90e8053d67c55c2a6f52
Hein Meling <hein.meling@uis.no>
2022-01-30 11:46:06 +0100

    Added missing newlines

--- assignments/testdata/tests/scripts/run.sh
+++ assignments/testdata/tests/scripts/run.sh
@@ -1,7 +1,7 @@
 #image/quickfeed:go
 
 printf "Creator Access Token:        {{ .CreatorAccessToken }}\n"
-printf "Assignment Name:             {{ .AssignmentName }}"
-printf "Student Repository URL:      {{ .GetURL }}"
-printf "Course Tests Repository URL: {{ .TestURL }}"
-printf "QuickFeed Session Secret:    {{ .RandomSecret }}"
+printf "Assignment Name:             {{ .AssignmentName }}\n"
+printf "Student Repository URL:      {{ .GetURL }}\n"
+printf "Course Tests Repository URL: {{ .TestURL }}\n"
+printf "QuickFeed Session Secret:    {{ .RandomSecret }}\n"
