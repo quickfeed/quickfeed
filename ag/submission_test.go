5033718338dd8afe96cf71b367a6b2c5dcaabcc2
Hein Meling <hein.meling@uis.no>
2022-03-10 17:32:39 -0500

    Added ByUser and ByGroup predicate methods to Submission
    These two methods can be used to determine if a submission belongs
    to a specific userID or groupID, respectively.

--- ag/submission_test.go
+++ ag/submission_test.go
@@ -54,3 +54,63 @@ func TestNewestSubmissionDate(t *testing.T) {
 		t.Errorf("NewestBuildDate(%v) = %v, expected '%v'\n", tim, new, buildDate)
 	}
 }
+
+func TestByUser(t *testing.T) {
+	submission := &pb.Submission{}
+	if submission.ByUser(0) {
+		t.Errorf("ByUser(0) = true, expected false\n")
+	}
+
+	submission = &pb.Submission{
+		UserID: 1,
+	}
+	if !submission.ByUser(1) {
+		t.Errorf("ByUser(1) = false, expected true\n")
+	}
+
+	submission = &pb.Submission{
+		GroupID: 1,
+	}
+	if submission.ByUser(1) {
+		t.Errorf("ByUser(1) = true, expected false\n")
+	}
+
+	// submissions with both user and group ID are invalid
+	submission = &pb.Submission{
+		UserID:  1,
+		GroupID: 2,
+	}
+	if submission.ByUser(1) {
+		t.Errorf("ByUser(1) = true, expected false\n")
+	}
+}
+
+func TestByGroup(t *testing.T) {
+	submission := &pb.Submission{}
+	if submission.ByGroup(0) {
+		t.Errorf("ByGroup(0) = true, expected false\n")
+	}
+
+	submission = &pb.Submission{
+		GroupID: 1,
+	}
+	if !submission.ByGroup(1) {
+		t.Errorf("ByGroup(1) = false, expected true\n")
+	}
+
+	submission = &pb.Submission{
+		UserID: 1,
+	}
+	if submission.ByGroup(1) {
+		t.Errorf("ByGroup(1) = true, expected false\n")
+	}
+
+	// submissions with both user and group ID are invalid
+	submission = &pb.Submission{
+		UserID:  1,
+		GroupID: 2,
+	}
+	if submission.ByGroup(1) {
+		t.Errorf("ByGroup(1) = true, expected false\n")
+	}
+}
