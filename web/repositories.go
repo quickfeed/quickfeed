bb42ebe8183cd6b220f260d59b1e733d12151290
Hein Meling <hein.meling@uis.no>
2022-03-09 23:38:34 -0500

    Replace getRepositoryURL with getRepo in GetRepositories
    This removes the getRepositoryURL method and adds tests for the
    GetRepositories gRPC method.

--- web/repositories.go
+++ web/repositories.go
@@ -30,40 +30,6 @@ func (s *AutograderService) getRepo(course *pb.Course, id uint64, repoType pb.Re
 	return repos[0], nil
 }
 
-// getRepositoryURL returns URL of a course repository of the given type.
-func (s *AutograderService) getRepositoryURL(currentUser *pb.User, courseID uint64, repoType pb.Repository_Type) (string, error) {
-	course, err := s.db.GetCourse(courseID, false)
-	if err != nil {
-		return "", err
-	}
-	userRepoQuery := &pb.Repository{
-		OrganizationID: course.GetOrganizationID(),
-		RepoType:       repoType,
-	}
-
-	switch repoType {
-	case pb.Repository_USER:
-		userRepoQuery.UserID = currentUser.GetID()
-	case pb.Repository_GROUP:
-		enrol, err := s.db.GetEnrollmentByCourseAndUser(courseID, currentUser.GetID())
-		if err != nil {
-			return "", err
-		}
-		if enrol.GetGroupID() > 0 {
-			userRepoQuery.GroupID = enrol.GroupID
-		}
-	}
-
-	repos, err := s.db.GetRepositories(userRepoQuery)
-	if err != nil {
-		return "", err
-	}
-	if len(repos) != 1 {
-		return "", fmt.Errorf("found %d repositories for query %+v", len(repos), userRepoQuery)
-	}
-	return repos[0].HTMLURL, nil
-}
-
 // isEmptyRepo returns nil if all repositories for the given course and student or group are empty,
 // returns an error otherwise.
 func (s *AutograderService) isEmptyRepo(ctx context.Context, sc scm.SCM, request *pb.RepositoryRequest) error {
