a973b85f6c90d7cda10ba1d2e81c90d014a2154b
Hein Meling <hein.meling@uis.no>
2022-04-04 17:54:11 -0700

    Fixed lint issue: shadowing of predeclared identifier: new
    Turns out it was a bad idea to recommend calling the variable new.
    Instead I renamed it to newTask.

--- ag/task.go
+++ ag/task.go
@@ -1,6 +1,6 @@
 package ag
 
-// HasChanged returns true if task t has a different title or body than the new task
-func (t *Task) HasChanged(new *Task) bool {
-	return t.Title != new.Title || t.Body != new.Body
+// HasChanged returns true if task t has a different title or body than the new task.
+func (t *Task) HasChanged(newTask *Task) bool {
+	return t.Title != newTask.Title || t.Body != newTask.Body
 }
