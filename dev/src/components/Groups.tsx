8bb0fee348b23b2573fd2ab5d0af65c2ab04e743
JosteinLindhom <jossi2010@gmail.com>
2022-04-12 15:28:07 +0200

    Converted Groups.tsx to use AsObject

--- dev/src/components/Groups.tsx
+++ dev/src/components/Groups.tsx
@@ -13,22 +13,22 @@ const Groups = (): JSX.Element => {
     const courseID = getCourseID()
 
 
-    const [editing, setEditing] = useState<Group>()
+    const [editing, setEditing] = useState<Group.AsObject>()
 
-    const groupSearch = (group: Group) => {
+    const groupSearch = (group: Group.AsObject) => {
         // Show all groups if query is empty
         if (state.query.length == 0) {
             return false
         }
 
         // Show group if group name includes query
-        if (group.getName().toLowerCase().includes(state.query)) {
+        if (group.name.toLowerCase().includes(state.query)) {
             return false
         }
 
         // Show group if any group user includes query
-        for (const user of group.getUsersList()) {
-            if (user.getName().toLowerCase().includes(state.query)) {
+        for (const user of group.usersList) {
+            if (user.name.toLowerCase().includes(state.query)) {
                 return false
             }
         }
@@ -36,11 +36,11 @@ const Groups = (): JSX.Element => {
         return true
     }
 
-    const updateGroupStatus = (group: Group, status: Group.GroupStatus) => {
+    const updateGroupStatus = (group: Group.AsObject, status: Group.GroupStatus) => {
         actions.updateGroupStatus({ group, status })
     }
 
-    const GroupButtons = ({ group }: { group: Group }) => {
+    const GroupButtons = ({ group }: { group: Group.AsObject }) => {
         if (isPendingGroup(group)) {
             return (
                 <td>
@@ -53,21 +53,21 @@ const Groups = (): JSX.Element => {
         return <td><span className="badge badge-info clickable" onClick={() => setEditing(group)}>Edit</span></td>
     }
 
-    const GroupRow = ({ group }: { group: Group }) => {
+    const GroupRow = ({ group }: { group: Group.AsObject }) => {
         return (
             <>
                 <tr hidden={groupSearch(group)}>
-                    <th key={group.getId()}>
-                        {group.getName()}
+                    <th key={group.id}>
+                        {group.name}
                         <span className="badge badge-warning ml-2">{isPendingGroup(group) ? "Pending" : null}</span>
                     </th>
                     <td>
                         <div>
                             {// Populates the unordered list with list elements for every user in the group
-                                hasEnrollments(group) && group.getEnrollmentsList().map((enrol, index) =>
-                                    <span key={enrol.getId()} className="inline-block">
-                                        <a href={`https://github.com/${enrol.getUser()?.getLogin()}`} target="_blank" rel="noopener noreferrer">{enrol.getUser()?.getName()}</a>
-                                        {index >= group.getEnrollmentsList().length - 1 ? "" : ", "}
+                                hasEnrollments(group) && group.enrollmentsList.map((enrol, index) =>
+                                    <span key={enrol.id} className="inline-block">
+                                        <a href={`https://github.com/${enrol.user?.login}`} target="_blank" rel="noopener noreferrer">{enrol.user?.name}</a>
+                                        {index >= group.enrollmentsList.length - 1 ? "" : ", "}
                                     </span>
                                 )}
                         </div>
@@ -80,11 +80,11 @@ const Groups = (): JSX.Element => {
 
     // Generates JSX.Element array containing all groups for the course
     const PendingGroups = state.groups[courseID]?.filter(group => isPendingGroup(group)).map(group => {
-        return <GroupRow key={group.getId()} group={group} />
+        return <GroupRow key={group.id} group={group} />
     })
 
     const ApprovedGroups = state.groups[courseID]?.filter(group => isApprovedGroup(group)).map(group => {
-        return <GroupRow key={group.getId()} group={group} />
+        return <GroupRow key={group.id} group={group} />
     })
 
     if (editing) {
