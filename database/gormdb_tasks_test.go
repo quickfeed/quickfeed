9517c3572998ee01830cb67b9820c9c5bfde49f3
Ole Jørgen <oj.espeland@stud.uis.no>
2022-04-19 06:08:09 +0200

    Redone SynchronizeAssignmentTasks test

--- database/gormdb_tasks_test.go
+++ database/gormdb_tasks_test.go
@@ -1,6 +1,8 @@
 package database_test
 
 import (
+	"errors"
+	"sort"
 	"testing"
 
 	pb "github.com/autograde/quickfeed/ag"
@@ -28,12 +30,9 @@ func getTasksFromAssignments(assignments []*pb.Assignment) map[uint32]map[string
 func createCourseWithAssignments(t *testing.T, db database.Database) *pb.Course {
 	t.Helper()
 	admin := qtest.CreateFakeUser(t, db, uint64(1))
-	qtest.CreateCourse(t, db, admin, &pb.Course{})
+	course := &pb.Course{}
+	qtest.CreateCourse(t, db, admin, course)
 
-	course, err := db.GetCourse(1, false)
-	if err != nil {
-		t.Fatal(err)
-	}
 	assignments := []*pb.Assignment{
 		{CourseID: course.GetID(), Name: "Lab1", Order: 1},
 		{CourseID: course.GetID(), Name: "Lab2", Order: 2},
@@ -82,166 +81,173 @@ func TestGormDBNonExistingTasksForAssignment(t *testing.T) {
 	}
 }
 
-// TestSynchronizeTasks tests whether tasks are correctly updated in the database
+// TestGormDBSynchronizeAssignmentTasks tests whether SynchronizeAssignmentTasks
+// correctly synchronizes tasks in the database, and whether it returns the correct values.
+// It runs through possible assignment sequences.
 func TestGormDBSynchronizeAssignmentTasks(t *testing.T) {
-	db, cleanup := qtest.TestDB(t)
-	defer cleanup()
-	course := createCourseWithAssignments(t, db)
-
-	foundAssignments1, foundTasks1 := initialAssignments()
-
-	// Should create a new database-record for each task in foundTasks
-	if _, _, err := db.SynchronizeAssignmentTasks(course, getTasksFromAssignments(foundAssignments1)); err != nil {
-		t.Error(err)
-	}
-
-	wantTasks1 := foundTasks1
-	gotTasks1, err := db.GetTasks(&pb.Task{})
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(wantTasks1, gotTasks1, protocmp.Transform()); diff != "" {
-		t.Errorf("Synchronization mismatch (-wantTasks1, +gotTasks1):\n%s", diff)
-	}
-
-	// -------------------------------------------------------------------------- //
-	// Testing adding one new task and updating another
-	// -------------------------------------------------------------------------- //
-	foundAssignments2, foundTasks2 := initialAssignments()
-
-	newTask := &pb.Task{
-		AssignmentOrder: 2,
-		Title:           "Lab2, task3",
-		Body:            "Description of task3 in lab2",
-		Name:            "Lab2/3",
-	}
-	foundTasks2 = append(foundTasks2, newTask)
-	foundAssignments2[1].Tasks = append(foundAssignments2[1].Tasks, newTask)
-	foundAssignments2[0].Tasks[0].Body = "New body for lab1 task1"
-
-	if _, _, err = db.SynchronizeAssignmentTasks(course, getTasksFromAssignments(foundAssignments2)); err != nil {
-		t.Error(err)
-	}
-
-	wantTasks2 := foundTasks1
-	wantTasks2 = append(wantTasks2, foundTasks2[len(foundTasks2)-1])
-	wantTasks2[0].Body = "New body for lab1 task1"
-
-	gotTasks2, err := db.GetTasks(&pb.Task{})
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(wantTasks2, gotTasks2, protocmp.Transform()); diff != "" {
-		t.Errorf("Synchronization mismatch (-wantTasks2, +gotTasks2):\n%s", diff)
-	}
-
-	// -------------------------------------------------------------------------- //
-	// Test adding a new task to database not represented by tasks supplied to
-	// SynchronizeAssignmentTasks, then finding the same tasks as in previous test.
-	// -------------------------------------------------------------------------- //
-	if _, _, err = db.SynchronizeAssignmentTasks(course, getTasksFromAssignments(foundAssignments2)); err != nil {
-		t.Error(err)
-	}
-
-	wantTasks3 := wantTasks2
-	gotTasks3, err := db.GetTasks(&pb.Task{})
-	if err != nil {
-		t.Error(err)
-	}
-	if diff := cmp.Diff(wantTasks3, gotTasks3, protocmp.Transform()); diff != "" {
-		t.Errorf("Synchronization mismatch (-wantTasks3, +gotTasks3):\n%s", diff)
-	}
-}
-
-// TestSynchronizeAssignmentTasksReturn tests if SynchronizeAssignmentTasks returns correct values
-func TestGormDBReturnSynchronizeAssignmentTasks(t *testing.T) {
-	db, cleanup := qtest.TestDB(t)
-	defer cleanup()
-	course := createCourseWithAssignments(t, db)
-
-	foundAssignments1, foundTasks1 := initialAssignments()
-
-	// Creating four new tasks
-	gotCreatedTasks, gotUpdatedTasks, err := db.SynchronizeAssignmentTasks(course, getTasksFromAssignments(foundAssignments1))
-	if err != nil {
-		t.Error(err)
-	}
-
-	wantUpdatedTasks := []*pb.Task{}
-	wantCreatedTasks := foundTasks1
-
-	if diff := cmp.Diff(wantCreatedTasks, gotCreatedTasks, protocmp.Transform()); diff != "" {
-		t.Errorf("SynchronizeAssignmentTasks return mismatch (-wantCreatedTasks, +gotCreatedTasks):\n%s", diff)
-	}
-	if diff := cmp.Diff(wantUpdatedTasks, gotUpdatedTasks, protocmp.Transform()); diff != "" {
-		t.Errorf("SynchronizeAssignmentTasks return mismatch (-wantUpdatedTasks, +gotUpdatedTasks):\n%s", diff)
-	}
-
-	// -------------------------------------------------------------------------- //
-	// Creating three new tasks, updating two existing and deleting two existing
-	// -------------------------------------------------------------------------- //
-	foundAssignments2, foundTasks2 := initialAssignments()
-
-	newTasks := []*pb.Task{
-		{
-			AssignmentOrder: 1,
-			Title:           "New Task 1",
-			Body:            "Description of New Task 1",
-			Name:            "Lab1/3",
+	tests := map[string]struct {
+		foundAssignmentSequence [][]*pb.Assignment
+	}{
+		"Create update delete": {
+			foundAssignmentSequence: [][]*pb.Assignment{
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/1"},
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/2"},
+					}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/1"},
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/2"},
+					}},
+				},
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/1"},
+						{AssignmentOrder: 1, Title: "x", Body: "y", Name: "Lab1/2"},
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/3"},
+					}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{
+						{AssignmentOrder: 2, Title: "y", Body: "x", Name: "Lab2/1"},
+					}},
+				},
+			},
 		},
-		{
-			AssignmentOrder: 1,
-			Title:           "New Task 2",
-			Body:            "Description of New Task 2",
-			Name:            "Lab1/4",
+		"No initial tasks": {
+			foundAssignmentSequence: [][]*pb.Assignment{
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{}},
+				},
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{}},
+				},
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/1"},
+					}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/1"},
+					}},
+				},
+			},
 		},
-		{
-			AssignmentOrder: 2,
-			Title:           "New Task 1 in another assignment",
-			Body:            "Description of New Task 1 in another assignment",
-			Name:            "Lab2/3",
+		"Delete and recreate": {
+			foundAssignmentSequence: [][]*pb.Assignment{
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/1"},
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/2"},
+					}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/1"},
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/2"},
+					}},
+				},
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{}},
+				},
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{}},
+				},
+				{
+					{Name: "Lab1", Order: 1, Tasks: []*pb.Task{
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/1"},
+						{AssignmentOrder: 1, Title: "x", Body: "x", Name: "Lab1/2"},
+					}},
+					{Name: "Lab2", Order: 2, Tasks: []*pb.Task{
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/1"},
+						{AssignmentOrder: 2, Title: "x", Body: "x", Name: "Lab2/2"},
+					}},
+					{Name: "Lab3", Order: 3, Tasks: []*pb.Task{
+						{AssignmentOrder: 3, Title: "x", Body: "x", Name: "Lab3/1"},
+						{AssignmentOrder: 3, Title: "x", Body: "x", Name: "Lab3/2"},
+					}},
+				},
+			},
 		},
 	}
 
-	foundTasks2 = append(foundTasks2, newTasks...)
-	for _, assignment := range foundAssignments2 {
-		tasks := []*pb.Task{}
-		for _, task := range foundTasks2 {
-			if assignment.Order == task.AssignmentOrder {
-				tasks = append(tasks, task)
+	sortTasksByName := func(tasks []*pb.Task) {
+		sort.Slice(tasks, func(i, j int) bool {
+			return tasks[i].Name < tasks[j].Name
+		})
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			db, cleanup := qtest.TestDB(t)
+			defer cleanup()
+			admin := qtest.CreateFakeUser(t, db, 1)
+			course := &pb.Course{}
+			qtest.CreateCourse(t, db, admin, course)
+
+			previousTasks := make(map[string]*pb.Task)
+
+			for _, foundAssignments := range tt.foundAssignmentSequence {
+				wantTasks := []*pb.Task{}
+				for _, assignment := range foundAssignments {
+					assignment.CourseID = course.GetID()
+					if err := db.CreateAssignment(assignment); err != nil {
+						t.Error(err)
+					}
+					wantTasks = append(wantTasks, assignment.Tasks...)
+				}
+				gotCreatedTasks, gotUpdatedTasks, err := db.SynchronizeAssignmentTasks(course, getTasksFromAssignments(foundAssignments))
+				if err != nil {
+					t.Error(err)
+				}
+				gotTasks, err := db.GetTasks(&pb.Task{})
+				if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+					t.Fatal(err)
+				}
+
+				wantCreatedTasks := []*pb.Task{}
+				wantUpdatedTasks := []*pb.Task{}
+				for _, wantTask := range wantTasks {
+					task, ok := previousTasks[wantTask.GetName()]
+					if ok {
+						wantTask.ID = task.GetID()
+						wantTask.AssignmentID = task.GetAssignmentID()
+						if task.HasChanged(wantTask) {
+							wantUpdatedTasks = append(wantUpdatedTasks, wantTask)
+						}
+					} else {
+						wantCreatedTasks = append(wantCreatedTasks, wantTask)
+					}
+					delete(previousTasks, wantTask.GetName())
+				}
+				for name, deletedTask := range previousTasks {
+					deletedTask.MarkDeleted()
+					wantUpdatedTasks = append(wantUpdatedTasks, deletedTask)
+					delete(previousTasks, name)
+				}
+
+				sortTasksByName(wantTasks)
+				sortTasksByName(gotTasks)
+				if diff := cmp.Diff(wantTasks, gotTasks, protocmp.Transform()); diff != "" {
+					t.Errorf("Synchronization mismatch (-wantTasks, +gotTasks):\n%s", diff)
+				}
+
+				sortTasksByName(wantCreatedTasks)
+				// gotCreatedTasks is already sorted by SynchronizeAssignmentTasks
+				if diff := cmp.Diff(wantCreatedTasks, gotCreatedTasks, protocmp.Transform()); diff != "" {
+					t.Errorf("Synchronization return mismatch (-wantCreatedTasks, +gotCreatedTasks):\n%s", diff)
+				}
+
+				sortTasksByName(wantUpdatedTasks)
+				sortTasksByName(gotUpdatedTasks)
+				if diff := cmp.Diff(wantUpdatedTasks, gotUpdatedTasks, protocmp.Transform()); diff != "" {
+					t.Errorf("Synchronization return mismatch (-wantUpdatedTasks, +gotUpdatedTasks):\n%s", diff)
+				}
+
+				for _, task := range wantTasks {
+					previousTasks[task.GetName()] = task
+				}
 			}
-		}
-		assignment.Tasks = tasks
-	}
-
-	foundAssignments2[0].Tasks = append(foundAssignments2[0].Tasks[:1], foundAssignments2[0].Tasks[2:]...)
-	foundAssignments2[1].Tasks = append(foundAssignments2[1].Tasks[:1], foundAssignments2[1].Tasks[2:]...)
-
-	foundAssignments2[0].Tasks[0].Title = "New title for task 1 assignment 1"
-	foundAssignments2[1].Tasks[0].Title = "New title for task 1 assignment 2"
-
-	gotCreatedTasks, gotUpdatedTasks, err = db.SynchronizeAssignmentTasks(course, getTasksFromAssignments(foundAssignments2))
-	if err != nil {
-		t.Error(err)
-	}
-	wantCreatedTasks = newTasks
-
-	foundAssignments1[0].Tasks[0].Title = "New title for task 1 assignment 1"
-	foundAssignments1[0].Tasks[1].MarkDeleted()
-	foundAssignments1[1].Tasks[0].Title = "New title for task 1 assignment 2"
-	foundAssignments1[1].Tasks[1].MarkDeleted()
-	wantUpdatedTasks = append(wantUpdatedTasks,
-		foundAssignments1[0].Tasks[0], // updated task 1 of assignment 1
-		foundAssignments1[0].Tasks[1], // deleted task 2 of assignment 1
-		foundAssignments1[1].Tasks[0], // updated task 1 of assignment 2
-		foundAssignments1[1].Tasks[1], // deleted task 2 of assignment 2
-	)
-
-	if diff := cmp.Diff(wantCreatedTasks, gotCreatedTasks, protocmp.Transform()); diff != "" {
-		t.Errorf("SynchronizeAssignmentTasks return mismatch (-wantCreatedTasks, +gotCreatedTasks):\n%s", diff)
-	}
-	if diff := cmp.Diff(wantUpdatedTasks, gotUpdatedTasks, protocmp.Transform()); diff != "" {
-		t.Errorf("SynchronizeAssignmentTasks return mismatch (-wantUpdatedTasks, +gotUpdatedTasks):\n%s", diff)
+		})
 	}
 }
 
