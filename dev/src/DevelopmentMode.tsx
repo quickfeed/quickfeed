6bd704ada1875af8f8f6b4cee8787871fbd5b449
JosteinLindhom <jossi2010@gmail.com>
2022-04-22 17:44:44 +0200

    Renamed dev mode component, added indicator for active dev mode

--- dev/src/DevelopmentMode.tsx
+++ dev/src/DevelopmentMode.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { MockGrpcManager } from "./MockGRPCManager"
 import { useActions, useGrpc } from "./overmind"
 
 
@@ -6,19 +7,30 @@ import { useActions, useGrpc } from "./overmind"
 // and to switch between different signed in users.
 // This is only used for development purposes.
 // NOTE: this only works with the mocked gRPC manager.
-export const DevelopmentButtons = () => {
+export const DevelopmentMode = () => {
     const actions = useActions()
     const effects = useGrpc()
 
+    if (!(effects.grpcMan instanceof MockGrpcManager)) {
+        return null
+    }
+
     const setUser = async (id: string) => {
-        effects.grpcMan.setCurrentUser(Number(id))
+        (effects.grpcMan as MockGrpcManager).setCurrentUser(Number(id))
         actions.resetState()
         await actions.fetchUserData()
     }
 
     return (
-        <div>
-            {/*<button className="btn btn-primary" onClick={() => actions.saveState()}>Save State</button>*/}
+        <div style={{
+            position: "fixed",
+            bottom: "0",
+            zIndex: "1000",
+            width: "15.6rem",
+        }}>
+            <span className="badge badge-danger" style={{ width: "100%" }}>
+                Development Mode
+            </span>
             <select className="form-control" onChange={(e) => setUser(e.target.value)}>
                 {effects.grpcMan.getMockedUsers().getUsersList().map((user) => (
                     <option key={user.getId()} value={user.getId()}>{user.getName()}</option>
@@ -28,4 +40,4 @@ export const DevelopmentButtons = () => {
     )
 }
 
-export default DevelopmentButtons
+export default DevelopmentMode
