e1c00617183c9343162dc7a1a98bba4a793bc1fa
JosteinLindhom <jossi2010@gmail.com>
2022-04-22 17:45:34 +0200

    Inject dev mode component if running in development mode

--- dev/src/index.tsx
+++ dev/src/index.tsx
@@ -6,18 +6,22 @@ import { Provider } from 'overmind-react'
 import { config } from './overmind'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
+import DevelopmentMode from './DevelopmentMode'
 
 const overmind = createOvermind(config, {
     // Enable devtools by setting the below to ex. 'devtools: "localhost:3301"'
     // then run 'npx overmind-devtools@latest' to start the devtools
-    devtools: false,
+    devtools: "localhost:3301",
 })
 
 
+const DEVELOPMENT_MODE = (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') && window.location.hostname === 'localhost'
 
-render((<Provider value={overmind}>
-            <HashRouter>
-                <App />
-            </HashRouter>
-        </Provider>
-        ), document.getElementById('root'))
+render((
+    <Provider value={overmind}>
+        <HashRouter>
+            {DEVELOPMENT_MODE && <DevelopmentMode />}
+            <App />
+        </HashRouter>
+    </Provider>
+), document.getElementById('root'))
