4c0745607eb7194d7572305222c8c46ea8bc27f4
JosteinLindhom <jossi2010@gmail.com>
2022-05-16 14:20:11 +0200

    Fixed updateAdmin: user must be in `allUsers`.

--- dev/src/__tests__/updateAdmin.test.tsx
+++ dev/src/__tests__/updateAdmin.test.tsx
@@ -8,7 +8,7 @@ describe("Correct permission status should be set", () => {
     it("If user is not admin, promote to admin", () => {
         const user = new User().setId(1).setName("Test User").setIsadmin(false)
         const mockedOvermind = createOvermindMock(config, (state) => {
-            state.self = user
+            state.allUsers = [user]
         })
         window.confirm = jest.fn(() => true)
         updateAdmin(mockedOvermind, user)
@@ -18,7 +18,7 @@ describe("Correct permission status should be set", () => {
     it("If user is admin, demote user", () => {
         const user2 = new User().setId(2).setName("Test User2").setIsadmin(true)
         const mockedOvermind2 = createOvermindMock(config, (state) => {
-            state.self = user2
+            state.allUsers = [user2]
         })
         window.confirm = jest.fn(() => true)
         updateAdmin(mockedOvermind2, user2)
@@ -28,7 +28,7 @@ describe("Correct permission status should be set", () => {
     it("If user does not confirm, dont make any changes", () => {
         const user3 = new User().setId(3).setName("Test User3").setIsadmin(true)
         const mockedOvermind2 = createOvermindMock(config, (state) => {
-            state.self = user3
+            state.allUsers = [user3]
         })
         window.confirm = jest.fn(() => false)
         updateAdmin(mockedOvermind2, user3)
