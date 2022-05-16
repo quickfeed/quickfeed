accddc11b6292e29ec6a0e7edd1100d3169823f4
JosteinLindhom <jossi2010@gmail.com>
2022-05-16 12:54:23 +0200

    let -> const for variables that are not reassigned value

--- dev/src/__tests__/e2e/navbarResponsive.test.tsx
+++ dev/src/__tests__/e2e/navbarResponsive.test.tsx
@@ -2,17 +2,17 @@ import { Builder, By, Capabilities } from 'selenium-webdriver'
 import { isOverlapping } from '../testHelpers/testHelpers'
 
 describe("Front page elements login and logo should not overlap", () => {
-    //Laptop, desktop, mobile
+    // Laptop, desktop, mobile
     const overlapTests: { width: number, height: number, want: boolean }[] = [
         // Insert which resolutions to test here
-        { width: 960, height: 1080, want: false },//desktop
-        { width: 1366, height: 768, want: false },//Laptop
-        { width: 960, height: 1080, want: false },//split screen desktop
-        { width: 683, height: 768, want: false } // split screen laptop
+        { width: 1920, height: 1080, want: false }, // Desktop
+        { width: 1366, height: 768, want: false }, // Laptop
+        { width: 960, height: 1080, want: false }, // Split screen desktop
+        { width: 683, height: 768, want: false } // Split screen laptop
     ]
     overlapTests.forEach(test => {
         it(`Should not overlap on res ${test.width}x${test.height}`, async () => {
-            //This test requires you to set
+            // This test requires you to set
             const firefox = require('selenium-webdriver/firefox')
             const service = new firefox.ServiceBuilder('drivers\\geckodriver.exe')
             const driver = new Builder().forBrowser('firefox').setFirefoxService(service).withCapabilities(Capabilities.firefox()
@@ -22,14 +22,13 @@ describe("Front page elements login and logo should not overlap", () => {
 
             const logo = await driver.findElement(By.className("navbar-brand"))
             const signIn = await driver.findElement(By.className("signIn"))
-            let rect = await logo.getRect()
-            let rect2 = await signIn.getRect()
+            const rect = await logo.getRect()
+            const rect2 = await signIn.getRect()
 
-            var overlap = isOverlapping(rect, rect2)
+            const overlap = isOverlapping(rect, rect2)
 
             jest.setTimeout(50000)
             expect(overlap).toBe(test.want)
         }, 50000)
     })
-
 })
