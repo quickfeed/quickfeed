a93e7e0d01932a6eec161fc28976d594702b981a
JosteinLindhom <jossi2010@gmail.com>
2022-05-16 12:53:14 +0200

    Renamed file, and await clicks, rather than sleeping.

--- dev/src/__tests__/e2e/E2ECourseVisibility.test.tsx
+++ dev/src/__tests__/e2e/E2ECourseVisibility.test.tsx
@@ -1,7 +1,7 @@
 import { Builder, By, Capabilities, until } from 'selenium-webdriver'
 
 
-describe("End to End course visability", () => {
+describe("End to End course visibility", () => {
     it("Should not overlap on res 1920x1080", async () => {
         const firefox = require('selenium-webdriver/firefox')
         const service = new firefox.ServiceBuilder('drivers\\geckodriver.exe')
@@ -9,44 +9,37 @@ describe("End to End course visability", () => {
             .set("acceptInsecureCerts", true)).build()
         await driver.get("https://127.0.0.1/dev/")
 
-        //Go to coursepage
+        // Go to courseage
         const hamburger = await driver.findElement(By.className("hamburger"))
         await driver.wait(until.elementIsVisible(hamburger), 100)
-        hamburger.click()
-        await driver.sleep(1000)
+        await hamburger.click()
         const goToCourse = await driver.findElement(By.css(".courseLink"))
         await driver.wait(until.elementIsVisible(goToCourse), 100)
-        goToCourse.click()
-        await driver.sleep(1000)
+        await goToCourse.click()
 
-        //Find course code from coursecard and hamburger menu
+        // Find course code from coursecard and hamburger menu
         const card = await driver.findElement(By.css(".card"))
         const cardCourseCode = await card.findElement(By.css(".card-header")).getText()
         const courseCode = cardCourseCode.split("\n")[0]
-        await driver.sleep(1000)
         const hamburgerCode = await driver.findElement(By.css("#title")).getText()
 
-        //Unfavorite the course
-        await driver.sleep(1000)
+        // Unfavorite the course
         const star = await driver.findElement(By.css(".fa-star"))
         await driver.wait(until.elementIsVisible(star), 100)
-        star.click()
+        await star.click()
 
-        //Get courseCode from myCourses section
-        await driver.sleep(1000)
+        // Get courseCode from myCourses section
         const myCourse = await driver.findElement(By.css(".myCourses"))
         const myCoursesCard = await myCourse.findElement(By.css(".card-header")).getText()
 
         const hasMoved = (courseCode === myCoursesCard.split("\n")[0])
 
-        //Find coursecodes in navigator
-        await driver.sleep(1000)
+        // Find coursecodes in navigator
         const navigatorCourses = await driver.findElement(By.css(".navigator"))
-        await driver.sleep(1000)
         const courses = await navigatorCourses.findElements(By.css("#title"))
 
-        //Check if the course has moved from navigator
-        var isInList = true
+        // Check if the course has moved from navigator
+        let isInList = true
         for (let i = 0; i < courses.length; i++) {
             if (await courses[i].getText() === hamburgerCode) {
                 isInList = false
@@ -55,7 +48,6 @@ describe("End to End course visability", () => {
         const movedFromFavorites = (isInList && hasMoved)
 
         await driver.close()
-
         expect(movedFromFavorites).toBe((true))
     }, 50000)
 })
