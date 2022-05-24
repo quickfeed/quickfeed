1b7e4c38ae24a1f531e6b11d0ec1316ddba17085
JosteinLindhom <jossi2010@ggmail.com>
2022-05-24 16:08:57 +0200

    Removed bold font from hamburger menu

--- dev/src/components/NavBar.tsx
+++ dev/src/components/NavBar.tsx
@@ -14,8 +14,8 @@ const NavBar = (): JSX.Element => {
     return (
         <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#222", color: "#d4d4d4" }} id="main" >
             {!state.showFavorites &&
-                <div className="navbar-brand clickable" style={{ marginLeft: "30px", fontWeight: "bold", fontSize: "30px" }}>
-                    <Link to="/" style={{ color: "#d4d4d4" }}>QuickFeed</Link>
+                <div className="navbar-brand clickable" style={{ marginLeft: "30px", fontSize: "30px" }}>
+                    <Link to="/" style={{ color: "#d4d4d4", fontWeight: "bold" }}>QuickFeed</Link>
                     {hamburger}
                 </div>
             }
