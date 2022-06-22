94ba407d3c9b86b3113cb2d9e93d69aea0b97075
JosteinLindhom <jossi2010@ggmail.com>
2022-06-22 11:26:09 +0200

    Reduced HTML nesting

--- dev/src/components/CourseCard.tsx
+++ dev/src/components/CourseCard.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router'
 import { EnrollmentStatus, hasEnrolled, hasNone, hasPending, isVisible } from '../Helpers'
 import { useActions } from '../overmind'
 import { Course, Enrollment } from '../../proto/ag/ag_pb'
+import CourseFavoriteButton from './CourseFavoriteButton'
 
 
 interface CardProps {
@@ -22,31 +23,38 @@ const CourseCard = ({ course, enrollment }: CardProps): JSX.Element => {
     const history = useHistory()
     const status = enrollment.status
 
+    const CourseEnrollmentButton = (): JSX.Element => {
+        if (hasNone(status)) {
+            return <div className="btn btn-primary course-button" onClick={() => actions.enroll(course.id)}>Enroll</div>
+        } else if (hasPending(status)) {
+            return <div className="btn btn-secondary course-button disabled">Pending</div>
+        }
+        return <div className="btn btn-primary course-button" onClick={() => history.push("/course/" + enrollment.courseid)}>Go to Course</div>
+    }
+
+    const CourseEnrollmentStatus = (): JSX.Element | null => {
+        if (hasEnrolled(status)) {
+            return (
+                <>
+                    <CourseFavoriteButton enrollment={enrollment} style={{ "float": "right" }} />
+                    <p className="float-sm-right mr-2">{EnrollmentStatus[status]}</p>
+                </>
+            )
+        }
+        return null
+    }
+
     return (
         <div className="col-sm-4">
             <div className="card" style={{ maxWidth: "35rem", marginBottom: "10px", minHeight: "205px" }}>
                 <div className={"card-header bg-" + CardColor[status] + " text-white"}>
                     {course.code}
-                    {hasEnrolled(status) &&
-                        <>
-                            <span className="float-right">
-                                <i className={isVisible(enrollment) ? 'fa fa-star' : "fa fa-star-o"}
-                                    onClick={() => actions.setEnrollmentState(enrollment)}></i>
-                            </span>
-                            <p className="float-sm-right mr-2">{EnrollmentStatus[status]}</p>
-                        </>
-                    }
+                    <CourseEnrollmentStatus />
                 </div>
 
                 <div className="card-body position-relative">
                     <h5 className="card-title">{course.name} - {course.tag}/{course.year}</h5>
-                    {hasNone(status) ?
-                        <div className="btn btn-primary course-button" onClick={() => actions.enroll(course.id)}>Enroll</div>
-                        : hasPending(status) ?
-                            <div className="btn btn-secondary course-button disabled">Pending</div>
-                            :
-                            <div className="btn btn-primary course-button" onClick={() => history.push("/course/" + enrollment.courseid)}>Go to Course</div>
-                    }
+                    <CourseEnrollmentButton />
                 </div>
             </div>
         </div>
