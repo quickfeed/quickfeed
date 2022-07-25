663101404790f9061045ea18af79969eecbbcd5b
Hein Meling <hein.meling@uis.no>
2022-07-25 23:12:15 +0200

    Renamed some test vars to avoid interference
    Avoid most interference with any potential QUICKFEED env vars.
    We still use QUICKFEED env, but should be safe.

--- internal/env/env_test.go
+++ internal/env/env_test.go
@@ -37,14 +37,14 @@ func TestLoad(t *testing.T) {
 
 	want := map[string]string{
 		"QUICKFEED":           os.Getenv("QUICKFEED"),
-		"QUICKFEED_PATH":      "/quickfeed/root",
+		"SOME_PATH":           "/quickfeed/root",
 		"QUICKFEED_TEST_ENV":  "test",
 		"QUICKFEED_TEST_ENV2": "test2",
 		"QUICKFEED_TEST_ENV3": "test3",
 		"QUICKFEED_TEST_ENV4": "test4 xyz",
 		"QUICKFEED_TEST_ENV5": "test5 = zyx",
-		"QUICKFEED_CERT_FILE": "/quickfeed/root/cert/fullchain.pem",
-		"QUICKFEED_KEY_FILE":  filepath.Join(os.Getenv("QUICKFEED"), "cert/fullchain.pem"),
+		"SOME_CERT_FILE":      "/quickfeed/root/cert/fullchain.pem",
+		"SOME_KEY_FILE":       filepath.Join(os.Getenv("QUICKFEED"), "cert/fullchain.pem"),
 	}
 
 	input := `QUICKFEED_TEST_ENV=test
@@ -56,10 +56,10 @@ QUICKFEED_TEST_ENV4=test4 xyz
 ## Another comment
 QUICKFEED_TEST_ENV5=test5 = zyx
 # Variable to be expanded into other vars
-QUICKFEED_PATH=/quickfeed/root
+SOME_PATH=/quickfeed/root
 # Cert file and key file expanded
-QUICKFEED_CERT_FILE=$QUICKFEED_PATH/cert/fullchain.pem
-QUICKFEED_KEY_FILE=$QUICKFEED/cert/fullchain.pem
+SOME_CERT_FILE=$SOME_PATH/cert/fullchain.pem
+SOME_KEY_FILE=$QUICKFEED/cert/fullchain.pem
 `
 	if _, err = fi.WriteString(input); err != nil {
 		t.Fatal(err)
