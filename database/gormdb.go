315d6c81790f967aac4eef7cde6cb7ef4fd06af7
JosteinLindhom <jossi2010@ggmail.com>
2022-08-17 17:05:27 +0200

    Added the Grade type to the database

--- database/gormdb.go
+++ database/gormdb.go
@@ -58,6 +58,7 @@ func NewGormDB(path string, logger *zap.Logger) (*GormDB, error) {
 		&qf.Enrollment{},
 		&qf.Assignment{},
 		&qf.Submission{},
+		&qf.Grade{},
 		&qf.Group{},
 		&qf.Repository{},
 		&qf.UsedSlipDays{},
