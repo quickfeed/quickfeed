497212b6b39dfcc1fd29f984172d9eafdb1d2b94
JosteinLindhom <jossi2010@ggmail.com>
2022-08-17 16:52:52 +0200

    Added and updated Submission methods

--- qf/submission.go
+++ qf/submission.go
@@ -7,8 +7,46 @@ import (
 
 var ErrMissingBuildInfo = errors.New("submission missing build information")
 
-func (s *Submission) IsApproved() bool {
-	return s.GetStatus() == Submission_APPROVED
+func (s *Submission) IsApproved(userID uint64) bool {
+	for _, grade := range s.GetGrades() {
+		if grade.GetUserID() == userID && grade.GetStatus() == Submission_APPROVED {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Submission) IsAllApproved() bool {
+	for _, grade := range s.GetGrades() {
+		if grade.GetStatus() != Submission_APPROVED {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Submission) GetStatusByUser(userID uint64) Submission_Status {
+	for idx, grade := range s.GetGrades() {
+		if grade.GetUserID() == userID {
+			return s.Grades[idx].GetStatus()
+		}
+	}
+	return Submission_NONE
+}
+
+func (s *Submission) SetGrade(userID uint64, status Submission_Status) {
+	for idx, grade := range s.GetGrades() {
+		if grade.GetUserID() == userID {
+			s.Grades[idx].Status = status
+			return
+		}
+	}
+}
+
+func (s *Submission) SetGradeAll(status Submission_Status) {
+	for idx := range s.Grades {
+		s.Grades[idx].Status = status
+	}
 }
 
 // NewestBuildDate returns the submission's build date if newer than the provided submission date.
@@ -37,15 +75,21 @@ func (s *Submission) ByGroup(groupID uint64) bool {
 
 // Clean removes any score or reviews from the submission if it is not released.
 // This is to prevent users from seeing the score or reviews of a submission that has not been released.
-func (s *Submissions) Clean() {
+func (s *Submissions) Clean(userID uint64) {
 	for _, submission := range s.Submissions {
+		// Only send the grade belonging to the user requesting the submission
+		submission.Grades = []*Grade{{
+			UserID:       userID,
+			SubmissionID: submission.GetID(),
+			Status:       submission.GetStatusByUser(userID),
+		}}
+
 		// Released submissions, or submissions with no reviews need no cleaning.
 		if submission.GetReleased() || len(submission.GetReviews()) == 0 {
 			continue
 		}
-		// Remove any score, status, or reviews if the submission is not released.
+		// Remove any score or reviews if the submission is not released.
 		submission.Score = 0
-		submission.Status = Submission_NONE
 		submission.Reviews = nil
 	}
 }
