4a4ba17f9981dcd41727cea56e2845f67a0f78dc
Hein Meling <hein.meling@uis.no>
2022-09-04 18:10:58 +0200

    Removed StreamUserVerifier and renamed to user_auth.go

--- web/interceptor/user_auth.go
+++ web/interceptor/user_auth.go
@@ -10,37 +10,8 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/quickfeed/quickfeed/web/auth"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
-
-// StreamWrapper wraps a stream with a context.
-// This is required because we cannot modify the context of a stream directly.
-type StreamWrapper struct {
-	grpc.ServerStream
-	context context.Context
-}
-
-func (s *StreamWrapper) Context() context.Context {
-	return s.context
-}
-
-// TODO(jostein): Re-implement this for streaming.
-// StreamUserVerifier returns a gRPC stream server interceptor that verifies
-// the user is authenticated.
-// func StreamUserVerifier(logger *zap.SugaredLogger, tm *auth.TokenManager) grpc.StreamServerInterceptor {
-// 	return func(req interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-// 		inCtx := stream.Context()
-// 		context, err := getAuthenticatedContext(inCtx, logger, tm)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		// Wrapping the stream in a StreamWrapper to allow us to use a modified context.
-// 		// This way we can use the context to get the user ID in the handler.
-// 		wrappedStream := &StreamWrapper{ServerStream: stream, context: context}
-// 		return handler(req, wrappedStream)
-// 	}
-// }
 
 // UnaryUserVerifier returns a unary server interceptor verifying that the user is authenticated.
 // The request's session cookie is verified that it contains a valid JWT claim.
