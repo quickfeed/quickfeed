f7d17911d7b494a4e6ab88164a9ced3310d0f603
Hein Meling <hein.meling@uis.no>
2022-09-19 17:36:21 +0200

    Added support for adding cert to keychain on macOS

---
+++ internal/cert/add_cert.go
@@ -0,0 +1,9 @@
+//go:build !darwin
+
+package cert
+
+// AddTrustedCert adds given the certificate the user's keychain.
+func AddTrustedCert(certFile string) error {
+	log.Printf("Adding self-signed certificate to keychain on %s currently not supported", os.Getenv("OS"))
+	return nil
+}
