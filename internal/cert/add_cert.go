00ba88fe95621f0a5368bb64fa652a8fe588ca47
Hein Meling <hein.meling@uis.no>
2022-09-20 08:22:59 +0200

    Fixed missing imports in add_cert.go
    VSCode and gopls is still having problems with build tags.

--- internal/cert/add_cert.go
+++ internal/cert/add_cert.go
@@ -2,6 +2,11 @@
 
 package cert
 
+import (
+	"log"
+	"os"
+)
+
 // AddTrustedCert adds given the certificate the user's keychain.
 func AddTrustedCert(certFile string) error {
 	log.Printf("Adding self-signed certificate to keychain on %s currently not supported", os.Getenv("OS"))
