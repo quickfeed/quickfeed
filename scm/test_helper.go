47581782cfc823f923e0f5b6f95b93233cec4dd0
0xf8f8ff <0xf8f8ff@protonmail.com>
2022-09-21 16:35:13 +0200

    Removed scm/test_helpers.go

---
+++ scm/test_helper.go
@@ -0,0 +1,20 @@
+package scm
+
+// func TestSCMManager() *Manager {
+// 	conf := &Config{
+// 		"test",
+// 		"test",
+// 		&app.Config{},
+// 	}
+// 	sc := NewFakeSCMClient()
+// 	_, _ = sc.CreateOrganization(context.Background(), &OrganizationOptions{
+// 		Name: "testorg",
+// 		Path: "testorg",
+// 	})
+// 	return &Manager{
+// 		scms: map[string]SCM{
+// 			"testorg": sc,
+// 		},
+// 		Config: conf,
+// 	}
+// }
