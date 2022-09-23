b929fcc11a95f1f1ef164ccbc7232eedba673c05
JosteinLindhom <jossi2010@gmail.com>
2022-09-23 14:47:15 +0200

    Added s.streams to ghHook

--- web/webserver.go
+++ web/webserver.go
@@ -44,7 +44,7 @@ func (s *QuickFeedService) RegisterRouter(tm *auth.TokenManager, authConfig *oau
 	router.HandleFunc(auth.Logout, auth.OAuth2Logout())
 
 	// Register hooks.
-	ghHook := hooks.NewGitHubWebHook(s.logger, s.db, s.scmMgr, s.runner, s.bh.Secret)
+	ghHook := hooks.NewGitHubWebHook(s.logger, s.db, s.scmMgr, s.runner, s.streams, s.bh.Secret)
 	router.HandleFunc(auth.Hook, ghHook.Handle())
 
 	return router
