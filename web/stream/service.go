c7e4e5209afd2d3308b38f421a3621b57ea5eceb
JosteinLindhom <jossi2010@gmail.com>
2022-09-29 15:42:56 +0200

    Moved previous `pool` functionality into stream and service to reduce complexity.

--- web/stream/service.go
+++ web/stream/service.go
@@ -1,11 +1,10 @@
 package stream
 
 import (
-	"errors"
+	"context"
 	"sync"
 
 	"github.com/bufbuild/connect-go"
-	"github.com/quickfeed/quickfeed/internal/multierr"
 	"github.com/quickfeed/quickfeed/qf"
 )
 
@@ -20,63 +19,73 @@ type StreamServices struct {
 // NewStreamServices creates a new StreamServices.
 func NewStreamServices() *StreamServices {
 	return &StreamServices{
-		Submission: newService[qf.Submission](),
+		Submission: NewService[qf.Submission](),
 	}
 }
 
-// Service[T] is a type specific stream service.
-// It contains a pool that is used to send data to connected clients.
+// Service[T] is a type specific stream service..
 // It also contains a map of streams that are currently connected.
 type Service[T any] struct {
 	mu sync.RWMutex
-	// The pool of channels that are used to send data to clients.
-	pool *Pool[T]
 	// The map of streams.
-	streams map[uint64]*Stream[T]
+	streams map[uint64]Stream[T]
 }
 
 // NewService creates a new service.
-func newService[T any]() *Service[T] {
+func NewService[T any]() *Service[T] {
 	return &Service[T]{
-		pool:    NewPool[T](),
-		streams: make(map[uint64]*Stream[T]),
+		streams: make(map[uint64]Stream[T]),
 	}
 }
 
 // SendTo sends data to client(s) with the given ID(s). If no ID is given, data is sent to all clients.
-func (s *Service[T]) SendTo(data *T, userIDs ...uint64) error {
+func (s *Service[T]) SendTo(data *T, userIDs ...uint64) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 	if len(userIDs) == 0 {
 		// Broadcast to all clients.
-		s.pool.Send(data)
+		for _, stream := range s.streams {
+			stream.Send(data)
+		}
+		return
 	}
-	var errs []error
+
 	for _, userID := range userIDs {
-		err := s.pool.SendTo(userID, data)
-		errs = append(errs, err)
+		stream, ok := s.streams[userID]
+		if !ok {
+			continue
+		}
+		stream.Send(data)
 	}
-	return multierr.Join(errs...)
 }
 
 // Add adds a new stream to the service.
 // It returns the stream which must be run by the caller.
-func (s *Service[T]) Add(userID uint64, st *connect.ServerStream[T]) *Stream[T] {
-	// Delete the stream if it already exists.
-	s.Remove(userID)
-	// Add the stream to the map.
-	stream := NewStream(st, s.GetPool(), userID)
+func (s *Service[T]) Add(ctx context.Context, userID uint64, st *connect.ServerStream[T]) *stream[T] {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+	// Delete the stream if it already exists.
+	s.internalRemove(userID)
+	// Add the stream to the map.
+	stream := NewStream(ctx, st, userID)
 	s.streams[stream.GetID()] = stream
 	return stream
 }
 
-// Remove removes a stream from the service.
-// This closes the stream and removes it from the pool and map.
-func (s *Service[T]) Remove(id uint64) {
+func (s *Service[T]) AddStream(userID uint64, st Stream[T]) Stream[T] {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if _, ok := s.streams[id]; ok {
-		s.streams[id].Close()
+	s.internalRemove(userID)
+	// Add the stream to the map.
+	s.streams[st.GetID()] = st
+	return st
+}
+
+// Remove removes a stream from the service.
+// This closes the stream and removes it from the map.
+func (s *Service[T]) internalRemove(id uint64) {
+	if stream, ok := s.streams[id]; ok {
+		stream.Close()
 		delete(s.streams, id)
 	}
 }
@@ -90,24 +99,10 @@ func (s *Service[T]) Close() {
 
 // CloseBy closes a single stream by ID.
 func (s *Service[T]) CloseBy(id uint64) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	if stream, ok := s.streams[id]; ok {
 		stream.Close()
 	}
 	return nil
-}
-
-// GetStream returns a stream by ID.
-func (s *Service[T]) GetStream(id uint64) (*Stream[T], error) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	stream, ok := s.streams[id]
-	if !ok {
-		return nil, errors.New("stream not found")
-	}
-	return stream, nil
-}
-
-// GetPool returns the pool of the service.
-func (s *Service[T]) GetPool() *Pool[T] {
-	return s.pool
 }
--- web/stream/stream.go
+++ web/stream/stream.go
@@ -1,19 +1,28 @@
 package stream
 
 import (
+	"context"
+	"fmt"
+	"sync/atomic"
+
 	"github.com/bufbuild/connect-go"
 )
 
+type Stream[T any] interface {
+	Close()
+	GetID() uint64
+	Run() error
+	Send(data *T)
+}
+
 // Stream wraps a connect.ServerStream.
-type Stream[T any] struct {
+type stream[T any] struct {
 	// stream is the underlying connect stream
 	// that does the actual transfer of data
 	// between the server and a client
 	stream *connect.ServerStream[T]
-	// pool is stored to allow the stream to
-	// removed itself from the pool when the
-	// stream is closed
-	pool *Pool[T]
+	// context is the context of the stream
+	ctx context.Context
 	// The channel that we listen to for any
 	// new data that we need to send to the client.
 	ch chan *T
@@ -24,32 +33,75 @@ type Stream[T any] struct {
 }
 
 // NewStream creates a new stream.
-func NewStream[T any](stream *connect.ServerStream[T], pool *Pool[T], id uint64) *Stream[T] {
-	return &Stream[T]{
-		stream: stream,
-		pool:   pool,
-		ch:     pool.Add(id),
+func NewStream[T any](ctx context.Context, st *connect.ServerStream[T], id uint64) *stream[T] {
+	return &stream[T]{
+		stream: st,
+		ctx:    ctx,
+		ch:     make(chan *T),
 		id:     id,
 	}
 }
 
 // Close closes the stream.
-func (s *Stream[T]) Close() {
-	s.pool.Remove(s.id)
+func (s *stream[T]) Close() {
+	close(s.ch)
 }
 
 // GetID returns the user ID of the stream.
-func (s *Stream[T]) GetID() uint64 {
+func (s *stream[T]) GetID() uint64 {
 	return s.id
 }
 
 // Run runs the stream.
 // Run will block until the stream is closed.
-func (s *Stream[T]) Run() error {
-	for data := range s.ch {
+func (s *stream[T]) Run() error {
+	select {
+	case <-s.ctx.Done():
+		return s.ctx.Err()
+	case data, ok := <-s.ch:
+		if !ok {
+			return fmt.Errorf("stream closed")
+		}
 		if err := s.stream.Send(data); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func (s *stream[T]) Send(data *T) {
+	s.ch <- data
+}
+
+type mockStream[T any] struct {
+	*stream[T]
+	counter *uint32
+}
+
+func NewTestStream[T any](ctx context.Context, id uint64, counter *uint32) *mockStream[T] {
+	return &mockStream[T]{
+		stream:  &stream[T]{ctx: ctx, ch: make(chan *T), id: id},
+		counter: counter,
+	}
+}
+
+func (m *mockStream[T]) Run() error {
+	for {
+		select {
+		case data, ok := <-m.ch:
+			if !ok {
+				fmt.Println("stream closed")
+				return nil
+			}
+			atomic.AddUint32(m.counter, 1)
+			fmt.Println(m.GetID(), data, &m.ch)
+		case <-m.ctx.Done():
+			fmt.Println("context closed")
+			return m.ctx.Err()
+		}
+	}
+}
+
+func (m *mockStream[T]) GetChannel() chan *T {
+	return m.ch
 }
