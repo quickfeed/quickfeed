b71b7071ecec31630a2e722cff2cff3044e7e375
0xf8f8ff <0xf8f8ff@protonmail.com>
2022-09-29 13:06:04 +0200

    Add repository tests for mock scm

--- scm/mock_test.go
+++ scm/mock_test.go
@@ -2,9 +2,13 @@ package scm_test
 
 import (
 	"context"
+	"sort"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/quickfeed/quickfeed/internal/qtest"
+	"github.com/quickfeed/quickfeed/qf"
 	"github.com/quickfeed/quickfeed/scm"
 )
 
@@ -75,6 +79,88 @@ func TestMockOrganizations(t *testing.T) {
 		if err := s.RemoveMember(ctx, opt); err == nil {
 			t.Errorf("expected error: %s", org.err)
 		}
+	}
+}
+
+func TestMockRepositories(t *testing.T) {
+	s := scm.NewMockSCMClient()
+	ctx := context.Background()
+	repos := []*scm.Repository{
+		{
+			OrgID: qtest.MockCourses[0].OrganizationID,
+			Owner: qtest.MockCourses[0].OrganizationName,
+			Path:  "info",
+		},
+		{
+			OrgID: qtest.MockCourses[0].OrganizationID,
+			Owner: qtest.MockCourses[0].OrganizationName,
+			Path:  "tests",
+		},
+		{
+			OrgID: qtest.MockCourses[2].OrganizationID,
+			Owner: qtest.MockCourses[2].OrganizationName,
+			Path:  "assignments",
+		},
+		{
+			OrgID: qtest.MockCourses[2].OrganizationID,
+			Owner: qtest.MockCourses[2].OrganizationName,
+			Path:  "tests"},
+	}
+
+	for _, repo := range repos {
+		r, err := s.CreateRepository(ctx, &scm.CreateRepositoryOptions{
+			Organization: &qf.Organization{ID: repo.OrgID, Name: repo.Owner},
+			Path:         repo.Path,
+			Owner:        repo.Owner,
+			Permission:   "read",
+		})
+		if err != nil {
+			t.Error(err)
+		}
+		repo.ID = r.ID
+		if err := s.UpdateRepoAccess(ctx, repo, "", ""); err != nil {
+			t.Error(err)
+		}
+	}
+
+	for _, repo := range repos {
+		r, err := s.GetRepository(ctx, &scm.RepositoryOptions{
+			ID:    repo.ID,
+			Path:  repo.Path,
+			Owner: repo.Owner,
+		})
+		if err != nil {
+			t.Error(err)
+		}
+		if diff := cmp.Diff(repo, r, cmpopts.IgnoreFields(scm.Repository{}, "HTMLURL")); diff != "" {
+			t.Errorf("Expected same repository, got (-sub +want):\n%s", diff)
+		}
+	}
+
+	wantRepos := []*scm.Repository{repos[0], repos[1]}
+	courseRepos, err := s.GetRepositories(ctx, &qf.Organization{ID: qtest.MockCourses[0].OrganizationID})
+	if err != nil {
+		t.Error(err)
+	}
+	sort.Slice(courseRepos, func(i, j int) bool {
+		return courseRepos[i].ID < courseRepos[j].ID
+	})
+	if diff := cmp.Diff(wantRepos, courseRepos, cmpopts.IgnoreFields(scm.Repository{}, "HTMLURL")); diff != "" {
+		t.Errorf("Expected same repositories, got (-sub +want):\n%s", diff)
+	}
+
+	if err := s.DeleteRepository(ctx, &scm.RepositoryOptions{ID: 3}); err != nil {
+		t.Error(err)
+	}
+	courseRepos, err = s.GetRepositories(ctx, &qf.Organization{ID: qtest.MockCourses[2].OrganizationID})
+	if err != nil {
+		t.Error(err)
+	}
+	if len(courseRepos) > 1 {
+		t.Errorf("expected 1 repository, got %d", len(courseRepos))
+	}
+	if diff := cmp.Diff(repos[3], courseRepos[0], cmpopts.IgnoreFields(scm.Repository{}, "HTMLURL")); diff != "" {
+		t.Errorf("Expected same repository, got (-sub +want):\n%s", diff)
 	}
 
 }
