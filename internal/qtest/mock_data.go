8caca49f316e12824701bee7f96436f01d82d70d
0xf8f8ff <0xf8f8ff@protonmail.com>
2022-10-03 10:57:21 +0200

    Add mock org name to qtest

--- internal/qtest/mock_data.go
+++ internal/qtest/mock_data.go
@@ -2,6 +2,10 @@ package qtest
 
 import "github.com/quickfeed/quickfeed/qf"
 
+var (
+	MockOrg = "qfTestOrg"
+)
+
 var MockCourses = []*qf.Course{
 	{
 		Name:             "Distributed Systems",
@@ -11,7 +15,7 @@ var MockCourses = []*qf.Course{
 		Tag:              "Spring",
 		Provider:         "fake",
 		OrganizationID:   1,
-		OrganizationName: "qfTestOrg",
+		OrganizationName: MockOrg,
 	},
 	{
 		Name:             "Operating Systems",
