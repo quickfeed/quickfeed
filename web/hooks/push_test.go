7825c38b35ded23946e76cd9e450e9b31f43f177
0xf8f8ff <0xf8f8ff@protonmail.com>
2022-10-11 16:14:24 +0200

    Add test for extracting assignments from webhook payload

--- web/hooks/push_test.go
+++ web/hooks/push_test.go
@@ -1,6 +1,7 @@
 package hooks
 
 import (
+	"sort"
 	"testing"
 	"time"
 
@@ -10,7 +11,96 @@ import (
 	"github.com/quickfeed/quickfeed/internal/qtest"
 	"github.com/quickfeed/quickfeed/qf"
 	"github.com/quickfeed/quickfeed/scm"
+	"google.golang.org/protobuf/testing/protocmp"
 )
+
+func TestExtractAssignments(t *testing.T) {
+	course := qtest.MockCourses[0]
+	db, cleanup := qtest.TestDB(t)
+	defer cleanup()
+	wh := NewGitHubWebHook(qtest.Logger(t), db, &scm.Manager{}, &ci.Local{}, "secret")
+	admin := qtest.CreateAdminUser(t, db, "fake")
+	qtest.CreateCourse(t, db, admin, course)
+
+	assignments := []*qf.Assignment{
+		{
+			CourseID: course.ID,
+			Order:    1,
+			Name:     "lab1",
+		},
+		{
+			CourseID: course.ID,
+			Order:    2,
+			Name:     "lab2",
+		},
+		{
+			CourseID: course.ID,
+			Order:    3,
+			Name:     "lab3",
+		},
+	}
+	for _, assignment := range assignments {
+		if err := db.CreateAssignment(assignment); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	tests := []struct {
+		name            string
+		modified        []string
+		added           []string
+		removed         []string
+		wantAssignments []*qf.Assignment
+	}{
+		{
+			"modified lab1, lab3",
+			[]string{"lab1/lab123.go", "lab3/temp.go"},
+			[]string{},
+			[]string{},
+			[]*qf.Assignment{assignments[0], assignments[2]},
+		},
+		{
+			"added lab2",
+			[]string{},
+			[]string{"lab2/assignment.go"},
+			[]string{},
+			[]*qf.Assignment{assignments[1]},
+		},
+		{
+			"removed lab1, modified lab1",
+			[]string{"lab1/name.go"},
+			[]string{},
+			[]string{"lab1/lab1.go"},
+			[]*qf.Assignment{assignments[0]},
+		},
+		{
+			"modified lab1, added lab2, removed lab3",
+			[]string{"lab1/test.go"},
+			[]string{"lab2/assignment.go"},
+			[]string{"lab3/lab1.go"},
+			assignments,
+		},
+	}
+
+	for _, tt := range tests {
+		got := wh.extractAssignments(&github.PushEvent{
+			Commits: []*github.HeadCommit{
+				{
+					Modified: tt.modified,
+					Added:    tt.added,
+					Removed:  tt.removed,
+				},
+			},
+		}, course)
+		sort.Slice(got, func(i, j int) bool {
+			return got[i].Order < got[j].Order
+		})
+		if diff := cmp.Diff(tt.wantAssignments, got, protocmp.Transform()); diff != "" {
+			t.Errorf("%s: mismatch (-want, +got):\n%s", tt.name, diff)
+		}
+	}
+
+}
 
 func TestLastActivityDate(t *testing.T) {
 	course := qtest.MockCourses[0]
@@ -57,7 +147,7 @@ func TestLastActivityDate(t *testing.T) {
 			CourseID:         course.ID,
 			LastActivityDate: "none",
 		}); err != nil {
-			t.Error(err)
+			t.Fatal(err)
 		}
 	}
 }
