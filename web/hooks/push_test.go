cf771e61f530c2351a8b66ec320890b60fcc7e16
0xf8f8ff <0xf8f8ff@protonmail.com>
2022-10-11 15:41:26 +0200

    Add test for last activity date updates

--- web/hooks/push_test.go
+++ web/hooks/push_test.go
@@ -2,9 +2,65 @@ package hooks
 
 import (
 	"testing"
+	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-github/v45/github"
+	"github.com/quickfeed/quickfeed/ci"
+	"github.com/quickfeed/quickfeed/internal/qtest"
+	"github.com/quickfeed/quickfeed/qf"
+	"github.com/quickfeed/quickfeed/scm"
 )
+
+func TestLastActivityDate(t *testing.T) {
+	course := qtest.MockCourses[0]
+	db, cleanup := qtest.TestDB(t)
+	defer cleanup()
+	wh := NewGitHubWebHook(qtest.Logger(t), db, &scm.Manager{}, &ci.Local{}, "secret")
+	admin := qtest.CreateAdminUser(t, db, "fake")
+	qtest.CreateCourse(t, db, admin, course)
+
+	date := time.Now().Format("02 Jan")
+
+	tests := []struct {
+		name string
+		repo *qf.Repository
+	}{
+		{
+			"user repo",
+			&qf.Repository{
+				UserID:   admin.ID,
+				RepoType: qf.Repository_USER,
+			},
+		},
+		{
+			"group repo",
+			&qf.Repository{
+				UserID:   admin.ID,
+				RepoType: qf.Repository_GROUP,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		wh.updateLastActivityDate(course, tt.repo, admin.Login)
+		enrol, err := db.GetEnrollmentByCourseAndUser(course.ID, admin.ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if diff := cmp.Diff(enrol.LastActivityDate, date); diff != "" {
+			t.Errorf("expected last activity date: %s, got %s", date, enrol.LastActivityDate)
+		}
+		// Remove updated date.
+		if err := db.UpdateEnrollment(&qf.Enrollment{
+			UserID:           admin.ID,
+			CourseID:         course.ID,
+			LastActivityDate: "none",
+		}); err != nil {
+			t.Error(err)
+		}
+	}
+}
 
 func TestBranchName(t *testing.T) {
 	tests := []struct {
@@ -74,11 +130,3 @@ func TestDefaultBranch(t *testing.T) {
 		}
 	}
 }
-
-// func TestGitHubWebHook_updateLastActivityDate(t *testing.T) {
-
-// 	tests := []struct {
-// 		name string
-// 		repo *qf.Repository
-// 	}{}
-// }
