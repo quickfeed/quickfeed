cd89c575fb495fcde4291f1f7dc07fd2ef1c6ef7
JosteinLindhom <jossi2010@gmail.com>
2022-10-12 15:51:21 +0200

    Added the ability change the key type for the streams map

--- web/stream/service.go
+++ web/stream/service.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	"github.com/quickfeed/quickfeed/qf"
+	"github.com/quickfeed/quickfeed/qf/device"
 )
 
 // StreamServices contain all available stream services.
@@ -12,38 +13,44 @@ import (
 // To add a new service, add a new field to this struct and
 // initialize the service in the NewStreamServices function.
 type StreamServices struct {
-	Submission *Service[qf.Submission]
+	Submission *Service[qf.Submission, uint64]
+	Device     *Service[device.DeviceResponse, string]
 }
 
 // NewStreamServices creates a new StreamServices.
 func NewStreamServices() *StreamServices {
 	return &StreamServices{
-		Submission: NewService[qf.Submission](),
+		Submission: NewService[qf.Submission, uint64](),
 	}
+}
+
+// ID is the allowed type for stream IDs.
+type ID interface {
+	uint64 | string
 }
 
 // Service[T] is a type specific stream service.
 // It also contains a map of streams that are currently connected.
-type Service[T any] struct {
+type Service[T any, V ID] struct {
 	mu sync.Mutex
 	// The map of streams.
-	streams map[uint64]StreamInterface[T]
+	streams map[V]StreamInterface[T]
 }
 
 // NewService creates a new service.
-func NewService[T any]() *Service[T] {
-	return &Service[T]{
-		streams: make(map[uint64]StreamInterface[T]),
+func NewService[T any, V ID]() *Service[T, V] {
+	return &Service[T, V]{
+		streams: make(map[V]StreamInterface[T]),
 	}
 }
 
 // SendTo sends data to connected clients with the given IDs.
 // If no ID is given, data is sent to all connected clients.
 // Unconnected clients are ignored and will not receive the data.
-func (s *Service[T]) SendTo(data *T, userIDs ...uint64) {
+func (s *Service[T, V]) SendTo(data *T, ids ...V) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if len(userIDs) == 0 {
+	if len(ids) == 0 {
 		// Broadcast to all clients.
 		for _, stream := range s.streams {
 			stream.Send(data)
@@ -51,8 +58,8 @@ func (s *Service[T]) SendTo(data *T, userIDs ...uint64) {
 		return
 	}
 
-	for _, userID := range userIDs {
-		stream, ok := s.streams[userID]
+	for _, id := range ids {
+		stream, ok := s.streams[id]
 		if !ok {
 			continue
 		}
@@ -61,27 +68,27 @@ func (s *Service[T]) SendTo(data *T, userIDs ...uint64) {
 }
 
 // Add adds a new stream for the given user to the service.
-func (s *Service[T]) Add(stream StreamInterface[T], userID uint64) {
+func (s *Service[T, V]) Add(stream StreamInterface[T], id V) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// Delete the stream if it already exists.
-	s.internalRemove(userID)
+	s.internalRemove(id)
 	// Add the stream to the map.
-	s.streams[userID] = stream
+	s.streams[id] = stream
 }
 
 // internalRemove removes a stream from the service.
 // This closes the stream and removes it from the map.
 // This function must only be called when holding the mutex.
-func (s *Service[T]) internalRemove(userID uint64) {
-	if stream, ok := s.streams[userID]; ok {
+func (s *Service[T, V]) internalRemove(id V) {
+	if stream, ok := s.streams[id]; ok {
 		stream.Close()
-		delete(s.streams, userID)
+		delete(s.streams, id)
 	}
 }
 
 // Close closes all streams in the service.
-func (s *Service[T]) Close() {
+func (s *Service[T, V]) Close() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	for _, stream := range s.streams {
@@ -90,10 +97,10 @@ func (s *Service[T]) Close() {
 }
 
 // CloseBy closes the given user's stream.
-func (s *Service[T]) CloseBy(userID uint64) {
+func (s *Service[T, V]) CloseBy(id V) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if stream, ok := s.streams[userID]; ok {
+	if stream, ok := s.streams[id]; ok {
 		stream.Close()
 	}
 }
