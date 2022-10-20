959abdda84b4d34ec97a596539553161bd360191
JosteinLindhom <jossi2010@gmail.com>
2022-10-20 12:46:22 +0200

    Update profile components to match new proto types

--- public/src/components/profile/ProfileForm.tsx
+++ public/src/components/profile/ProfileForm.tsx
@@ -3,8 +3,6 @@ import { hasEnrollment } from "../../Helpers"
 import { useActions, useAppState } from "../../overmind"
 import FormInput from "../forms/FormInput"
 import { useHistory } from "react-router"
-import { Converter } from "../../convert"
-
 
 const ProfileForm = ({ children, setEditing }: { children: React.ReactNode, setEditing: Dispatch<SetStateAction<boolean>> }): JSX.Element => {
     const state = useAppState()
@@ -12,7 +10,7 @@ const ProfileForm = ({ children, setEditing }: { children: React.ReactNode, setE
     const history = useHistory()
 
     // Create a copy of the user object, so that we can modify it without affecting the original object.
-    const [user, setUser] = useState(Converter.clone(state.self))
+    const [user, setUser] = useState(state.self.clone())
     const [isValid, setIsValid] = useState(state.isValid)
 
     // Update the user object when user input changes, and update the state.
@@ -26,11 +24,11 @@ const ProfileForm = ({ children, setEditing }: { children: React.ReactNode, setE
                 user.email = value
                 break
             case "studentid":
-                user.studentid = value
+                user.studentID = value
                 break
         }
         setUser(user)
-        if (user.name !== "" && user.email !== "" && user.studentid !== "") {
+        if (user.name !== "" && user.email !== "" && user.studentID !== "") {
             setIsValid(true)
         } else {
             setIsValid(false)
@@ -54,7 +52,7 @@ const ProfileForm = ({ children, setEditing }: { children: React.ReactNode, setE
             <form className="form-group" onSubmit={e => { e.preventDefault(); submitHandler() }}>
                 <FormInput prepend="Name" name="name" defaultValue={user.name} onChange={handleChange} />
                 <FormInput prepend="Email" name="email" defaultValue={user.email} onChange={handleChange} type="email" />
-                <FormInput prepend="Student ID" name="studentid" defaultValue={user.studentid} onChange={handleChange} type="number" />
+                <FormInput prepend="Student ID" name="studentid" defaultValue={user.studentID} onChange={handleChange} type="number" />
                 <div className="col input-group mb-3">
                     <input className="btn btn-primary" disabled={!isValid} type="submit" value="Save" style={{ marginTop: "20px" }} />
                 </div>
