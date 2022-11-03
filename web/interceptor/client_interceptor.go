7f6a644b4d3f4b3e4da97a35c9da678a5fef2658
Hein Meling <hein.meling@uis.no>
2022-11-03 19:45:37 +0100

    Added doc comment and reuse tokenHeader

--- web/interceptor/client_interceptor.go
+++ web/interceptor/client_interceptor.go
@@ -6,12 +6,13 @@ import (
 	"github.com/bufbuild/connect-go"
 )
 
+// NewClientInterceptor returns a client interceptor that will add the given token in the Authorization header.
 func NewClientInterceptor(token string) connect.UnaryInterceptorFunc {
 	interceptor := func(next connect.UnaryFunc) connect.UnaryFunc {
 		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
 			if req.Spec().IsClient {
 				// Send a token with client requests.
-				req.Header().Set("Authorization", token)
+				req.Header().Set(tokenHeader, token)
 			}
 			return next(ctx, req)
 		})
