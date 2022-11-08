0a64c2c333a10d419252c525a5cf5f981fe61de2
JosteinLindhom <jossi2010@gmail.com>
2022-11-08 09:38:49 +0100

    Added doc & discard unused parameter

--- web/quickfeed_service.go
+++ web/quickfeed_service.go
@@ -642,7 +642,10 @@ func (s *QuickFeedService) IsEmptyRepo(ctx context.Context, in *connect.Request[
 	return &connect.Response[qf.Void]{}, nil
 }
 
-func (s *QuickFeedService) SubmissionStream(ctx context.Context, req *connect.Request[qf.Void], st *connect.ServerStream[qf.Submission]) error {
+// SubmissionStream adds the the created stream to the stream service.
+// The stream may be used to send the submission results to the frontend.
+// The stream is closed when the client disconnects.
+func (s *QuickFeedService) SubmissionStream(ctx context.Context, _ *connect.Request[qf.Void], st *connect.ServerStream[qf.Submission]) error {
 	stream := stream.NewStream(ctx, st)
 	s.streams.Submission.Add(stream, userID(ctx))
 	return stream.Run()
