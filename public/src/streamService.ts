8c59238e1f0ee8e04111170879e5bea3ef32939d
JosteinLindhom <jossi2010@gmail.com>
2022-11-09 12:36:54 +0100

    Skip reconnecting if stream was canceled

--- public/src/streamService.ts
+++ public/src/streamService.ts
@@ -1,6 +1,6 @@
 import { QuickFeedService } from '../proto/qf/quickfeed_connectweb'
 import { Submission } from '../proto/qf/types_pb'
-import { createConnectTransport, createPromiseClient, PromiseClient } from "@bufbuild/connect-web"
+import { Code, createConnectTransport, createPromiseClient, PromiseClient } from "@bufbuild/connect-web"
 import { ConnStatus } from './Helpers'
 
 
@@ -29,6 +29,14 @@ export class StreamService {
                 options.onMessage(msg)
             }
         } catch (error) {
+            if (error.code === Code.Canceled) {
+                // The stream was canceled, so we don't need to reconnect.
+                // This happens when the stream is closed by the server
+                // which happens only if the user opens a new stream, i.e., opens the frontend in a new tab.
+                options.onError(error)
+                return
+            }
+
             /* TODO(jostein): 
              * Our streams currently time out after 2 minutes.
              * Once (if) https://github.com/golang/go/issues/54136 is accepted
