164e3d7a386dcc0dd347ef6fbaeb70ffa76719ff
Hein Meling <hein.meling@uis.no>
2022-11-15 21:58:51 +0100

    Fixed unused func arguments

--- ci/local_unix.go
+++ ci/local_unix.go
@@ -13,7 +13,7 @@ type Local struct{}
 
 // Run implements the CI interface. This method blocks until the job has been
 // completed or an error occurs, e.g., the context times out.
-func (l *Local) Run(ctx context.Context, job *Job) (string, error) {
+func (*Local) Run(_ context.Context, job *Job) (string, error) {
 	// TODO: Execute tests in something like: os.CreateTemp(os.TempDir(), "local-ci")
 	cmd := exec.Command("/bin/bash", "-c", strings.Join(job.Commands, "\n"))
 	cmd.Env = job.Env
