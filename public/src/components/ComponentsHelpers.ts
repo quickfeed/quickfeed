125a23c0b33974535c4a8efd42ca2b1f32385f51
JosteinLindhom <jossi2010@gmail.com>
2022-12-08 14:01:21 +0100

    Rewrote `generateSubmissionRows`. Will now generate enrollment rows using shared group submissions.

--- public/src/components/ComponentsHelpers.ts
+++ public/src/components/ComponentsHelpers.ts
@@ -1,30 +1,49 @@
-import { Assignment, Enrollment, Group, Submission } from "../../proto/qf/types_pb"
-import { groupRepoLink, userRepoLink } from "../Helpers"
+import { Assignment, Course, Enrollment, Group, Submission } from "../../proto/qf/types_pb"
+import { groupRepoLink, SubmissionsForCourse, userRepoLink } from "../Helpers"
 import { useActions, useAppState } from "../overmind"
+import { AssignmentsMap } from "../overmind/state"
 import { RowElement, Row } from "./DynamicTable"
 
-export const generateSubmissionRows = (elements: Enrollment[] | Group[], generator: (s: Submission, e?: Enrollment | Group) => RowElement, assignmentIDs: bigint[]): Row[] => {
+
+export const generateSubmissionRows = (elements: Enrollment[] | Group[], generator: (s: Submission, e?: Enrollment | Group) => RowElement): Row[] => {
     const state = useAppState()
-    const submissions = state.groupView ? state.submissionsByGroup : state.submissionsByEnrollment
     const course = state.courses.find(c => c.ID === state.activeCourse)
+    const assignments = state.getAssignmentsMap(state.activeCourse)
     return elements.map(element => {
-        const row: Row = []
-        if (element instanceof Enrollment && element.user) {
-            row.push({ value: element.user.Name, link: userRepoLink(element.user, course) })
-        } else if (element instanceof Group) {
-            row.push({ value: element.name, link: groupRepoLink(element, course) })
-        }
-        const submissionsList = submissions[element.ID.toString()].submissions
-        assignmentIDs?.forEach(assignment => {
-            const submission = submissionsList?.find(s => s.AssignmentID === assignment)
-            if (submission) {
-                row.push(generator(submission, element))
-                return
-            }
-            row.push("N/A")
-        })
-        return row
+        return generateRow(element, assignments, state.submissionsForCourse, generator, course)
     })
+}
+
+const generateRow = (enrollment: Enrollment | Group, assignments: AssignmentsMap, submissions: SubmissionsForCourse, generator: (s: Submission, e?: Enrollment | Group) => RowElement, course?: Course): Row => {
+    const row: Row = []
+    const isEnrollment = enrollment instanceof Enrollment
+    const isGroup = enrollment instanceof Group
+
+    if (isEnrollment && enrollment.user) {
+        row.push({ value: enrollment.user.Name, link: userRepoLink(enrollment.user, course) })
+    } else if (isGroup) {
+        row.push({ value: enrollment.name, link: groupRepoLink(enrollment, course) })
+    }
+
+    Object.entries(assignments)?.forEach(([assignmentID, isGroupLab]) => {
+        let submission: Submission | undefined
+        if (isGroup && !isGroupLab) {
+            // If the assignment is not a group assignment, and we're dealing with a group
+            // we should exit early without adding to the row.
+            return
+        }
+        if (isGroupLab) {
+            submission = submissions.getSubmissionsForGroup(enrollment)?.find(s => s.AssignmentID.toString() === assignmentID)
+        } else if (isEnrollment) {
+            submission = submissions.getSubmissionsForEnrollment(enrollment)?.find(s => s.AssignmentID.toString() === assignmentID)
+        }
+        if (submission) {
+            row.push(generator(submission, enrollment))
+            return
+        }
+        row.push("N/A")
+    })
+    return row
 }
 
 export const generateAssignmentsHeader = (base: RowElement[], assignments: Assignment[], group: boolean): Row => {
