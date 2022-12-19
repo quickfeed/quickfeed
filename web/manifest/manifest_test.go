08b6381489fd13a94c3fcc28647ec4ec29999171
Hein Meling <hein.meling@uis.no>
2022-12-19 00:06:34 -0800

    Added GITHUB_APP env to prevent github CI from running TestManifest

--- web/manifest/manifest_test.go
+++ web/manifest/manifest_test.go
@@ -1,11 +1,15 @@
 package manifest_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/quickfeed/quickfeed/web/manifest"
 )
 
 func TestManifest(t *testing.T) {
+	if os.Getenv("GITHUB_APP") == "" {
+		t.Skip("Skipping test; GITHUB_APP is not set")
+	}
 	manifest.SetupGitHubApp(t)
 }
