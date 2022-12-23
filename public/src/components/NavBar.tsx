a8905cee2d12591770468cb52cda9c0386f0dc4e
JosteinLindhom <jossi2010@gmail.com>
2022-12-23 14:50:38 +0100

    Modified navbar components. Logic related to user moved to NavBarUser. Added ActiveCourse Minor tweaks to classes

--- public/src/components/NavBar.tsx
+++ public/src/components/NavBar.tsx
@@ -3,36 +3,27 @@ import { useActions, useAppState } from "../overmind"
 import { Link } from "react-router-dom"
 import NavFavorites from "./NavFavorites"
 import NavBarUser from "./navbar/NavBarUser"
-import StreamStatus from "./navbar/StreamStatus"
+import NavBarActiveCourse from "./navbar/NavBarActiveCourse"
 
 
 const NavBar = (): JSX.Element => {
     const state = useAppState()
     const actions = useActions()
 
-    const hamburger = state.isLoggedIn ? <span onClick={() => actions.toggleFavorites()} className="ml-3 hamburger">☰</span> : null
+    let hamburger = null
+    if (state.isLoggedIn) {
+        const classname = `clickable hamburger ${state.showFavorites ? "open" : "closed"}`
+        hamburger = <span onClick={() => actions.toggleFavorites()} className={classname}>☰</span>
+    }
 
     return (
-        <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#222", color: "#d4d4d4" }} id="main" >
-            {!state.showFavorites &&
-                <div className="navbar-brand clickable" style={{ marginLeft: "30px", fontSize: "30px" }}>
-                    <Link to="/" style={{ color: "#d4d4d4", fontWeight: "bold" }}>QuickFeed</Link>
-                    {hamburger}
-                    <StreamStatus />
-                </div>
-            }
-            {!state.isLoggedIn &&
-                <div className="navbar-collapse ml-auto">
-                    <a href="/auth/github" className="nav-item ml-auto ms-auto signIn" style={{ textAlign: "right", color: "#d4d4d4", marginRight: "55px" }}>
-                        <i className="fa fa-2x fa-github align-middle ms-auto " id="github" />
-                    </a>
-                </div>
-            }
-            {state.isLoggedIn &&
-                <ul className="ms-auto ml-auto list-unstyled" style={{ marginRight: "55px", paddingTop: "15px" }}>
-                    <NavBarUser />
-                </ul>
-            }
+        <nav className="navbar navbar-top navbar-expand-sm flexbox" id="main" >
+            <div className="nav-child brand">
+                {hamburger}
+                <Link to="/" style={{ color: "#d4d4d4", fontWeight: "bold" }}>QuickFeed</Link>
+            </div>
+            <NavBarActiveCourse />
+            <NavBarUser />
             {state.showFavorites &&
                 <NavFavorites />
             }
--- public/src/components/navbar/NavBarUser.tsx
+++ public/src/components/navbar/NavBarUser.tsx
@@ -4,24 +4,35 @@ import AdminButton from "../navbar-buttons/AdminButton"
 import { useAppState } from "../../overmind"
 import ProfileButton from "../navbar-buttons/ProfileButton"
 import LogoutButton from "../navbar-buttons/LogoutButton"
+import StreamStatus from "./StreamStatus"
 
 const NavBarUser = (): JSX.Element => {
-    const { self } = useAppState()
+    const { self, isLoggedIn } = useAppState()
+
+    if (!isLoggedIn) {
+        return (
+            <ul>
+                <a href="/auth/github" className="signIn" style={{ textAlign: "right", color: "#d4d4d4", marginRight: "55px" }}>
+                    <i className="fa fa-2x fa-github align-middle ms-auto " id="github" />
+                </a>
+            </ul>
+        )
+    }
 
     return (
-        <div className="navbar-collapse ml-auto" id="main_nav">
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item dropdown ml-auto">
-                    <img className="rounded-circle" src={self.AvatarURL} id="avatar"
-                        style={{ height: "40px", borderRadius: "50%" }} />
-                    <ul className="dropdown-menu dropdown-menu-center bg-dark">
-                        <ProfileButton />
-                        <AboutButton />
-                        <AdminButton />
-                        <LogoutButton />
-                    </ul>
-                </li>
+        <div className="flex-user">
+
+            <StreamStatus />
+            <ul className="nav-item dropdown">
+                <img className="rounded-circle" src={self.AvatarURL} id="avatar" />
+                <ul className="dropdown-menu dropdown-menu-center bg-dark">
+                    <ProfileButton />
+                    <AboutButton />
+                    <AdminButton />
+                    <LogoutButton />
+                </ul>
             </ul>
+
         </div>
     )
 }
