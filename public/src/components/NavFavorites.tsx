827649e4079a70a69896be1d1fa2e434bc81e5bb
JosteinLindhom <jossi2010@gmail.com>
2022-12-23 14:41:46 +0100

    Removed "logo" and sidebar toggle from sidebar

--- public/src/components/NavFavorites.tsx
+++ public/src/components/NavFavorites.tsx
@@ -1,12 +1,11 @@
 import React from "react"
-import { useAppState, useActions } from "../overmind"
+import { useAppState } from "../overmind"
 import { Link } from "react-router-dom"
 import NavBarCourse from "./navbar/NavBarCourse"
 import { isEnrolled, isVisible } from "../Helpers"
 
 const NavFavorites = (): JSX.Element => {
     const state = useAppState()
-    const actions = useActions()
 
     const visible = state.enrollments.filter(enrollment => isEnrolled(enrollment) && isVisible(enrollment))
 
@@ -17,12 +16,6 @@ const NavFavorites = (): JSX.Element => {
     return (
         <nav className="navigator">
             <ul key="list" className="sidebarList">
-                <li key="logo" className="logo" style={{ paddingLeft: "40px", fontWeight: "bold" }}>
-                    <Link to="/" >
-                        QuickFeed
-                    </Link>
-                    <a  onClick={() => actions.toggleFavorites()} role="button" style={{ textDecoration: "none"}} className="closeButton">✖</a>
-                </li>
                 {courses}
                 {state.isLoggedIn &&
                     <li key="all" className="courseLink">
