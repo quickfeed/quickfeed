278d217695734acd262b693d89be1844af2b1d67
JosteinLindhom <jossi2010@gmail.com>
2023-02-16 16:09:43 +0100

    Added component to render string as markdown

---
+++ public/src/components/manual-grading/Comment.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { CodeProps } from "react-markdown/lib/ast-to-react";
+
+
+const CriterionComment = ({comment}: {comment: string}): JSX.Element | null => {
+    console.log(comment, comment.length)
+    if (comment == "" || comment.length == 0) {
+        console.log("returning null")
+        return null
+    }
+    return (
+        <div className="comment-md">
+        <ReactMarkdown
+            // eslint-disable-next-line react/no-children-prop
+            children={comment}
+            components={{
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                code({node, inline, className, children, ...props}: CodeProps) {
+                  const match = /language-(\w+)/.exec(className || '')
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                        // eslint-disable-next-line react/no-children-prop
+                        children={String(children).replace(/\n$/, '')}
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                        style={oneDark}
+                    />
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  )
+                }
+              }}
+            />
+        </div>
+    )
+}
+
+export default CriterionComment
