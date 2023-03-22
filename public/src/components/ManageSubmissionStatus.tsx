63c537d0672ca1dbaa2dfb55fa464e23f65a3d6e
JosteinLindhom <jossi2010@gmail.com>
2023-03-22 12:49:27 +0100

    refactored ManageSubmissionStatus: use button components

--- public/src/components/ManageSubmissionStatus.tsx
+++ public/src/components/ManageSubmissionStatus.tsx
@@ -1,7 +1,9 @@
-import React, { useMemo } from "react"
+import React from "react"
 import { Submission_Status } from "../../proto/qf/types_pb"
-import { isManuallyGraded } from "../Helpers"
+import { Color, isManuallyGraded } from "../Helpers"
 import { useActions, useAppState } from "../overmind"
+import { ButtonType } from "./admin/Button"
+import DynamicButton from "./DynamicButton"
 
 const ManageSubmissionStatus = (): JSX.Element => {
     const actions = useActions()
@@ -10,12 +12,6 @@ const ManageSubmissionStatus = (): JSX.Element => {
 
     const [rebuilding, setRebuilding] = React.useState(false)
     const [updating, setUpdating] = React.useState<Submission_Status>(Submission_Status.NONE)
-
-    const buttons: { text: string, status: Submission_Status, style: string, onClick?: () => void }[] = [
-        { text: "Approve", status: Submission_Status.APPROVED, style: "success" },
-        { text: "Revision", status: Submission_Status.REVISION, style: "warning" },
-        { text: "Reject", status: Submission_Status.REJECTED, style: "danger" },
-    ]
 
 
     const handleRebuild = async () => {
@@ -32,38 +28,45 @@ const ManageSubmissionStatus = (): JSX.Element => {
         setUpdating(Submission_Status.NONE)
     }
 
-    if (assignment && !isManuallyGraded(assignment)) {
-        // Add rebuild button if the assignment is not manually graded
-        buttons.push({ text: rebuilding ? "Rebuilding..." : "Rebuild", status: -1, style: rebuilding ? "secondary" : "primary", onClick: handleRebuild })
+    const getButtonType = (status: Submission_Status): ButtonType => {
+        if (state.selectedSubmission?.status === status) {
+            return ButtonType.BUTTON
+        }
+        return ButtonType.OUTLINE
     }
-
-    const StatusButtons = useMemo(() =>
-        buttons.map((button, index) => {
-            if (updating === button.status) {
-                // Show spinner while submission is being updated
-                return (
-                    <button key={index} className={`col btn btn-secondary mr-2`}>
-                        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
-                        <span className="sr-only">Loading...</span>
-                    </button>
-                )
-            }
-            const style = state.selectedSubmission?.status === button.status
-                ? `col btn btn-${button.style} mr-2`         // Show solid button if the submission status matches the button status
-                : `col btn btn-outline-${button.style} mr-2` // Show outlined button otherwise
-
-            return (
-                <button key={index} className={style} onClick={() => button.onClick ? button.onClick() : handleSetStatus(button.status)}>
-                    {button.text}
-                </button>
-            )
-        })
-        , [buttons, rebuilding, state.selectedSubmission?.status, updating]
-    )
 
     return (
         <div className="row m-auto">
-            {StatusButtons}
+            <DynamicButton
+                text="Approve"
+                onClick={() => handleSetStatus(Submission_Status.APPROVED)}
+                color={updating === Submission_Status.APPROVED ? Color.GRAY : Color.GREEN}
+                type={getButtonType(Submission_Status.APPROVED)}
+                className="col mr-2"
+            />
+            <DynamicButton
+                text="Revision"
+                onClick={() => handleSetStatus(Submission_Status.REVISION)}
+                color={updating === Submission_Status.REVISION ? Color.GRAY : Color.YELLOW}
+                type={getButtonType(Submission_Status.REVISION)}
+                className="col mr-2"
+            />
+            <DynamicButton
+                text="Reject"
+                onClick={() => handleSetStatus(Submission_Status.REJECTED)}
+                color={updating === Submission_Status.REJECTED ? Color.GRAY : Color.RED}
+                type={getButtonType(Submission_Status.REJECTED)}
+                className="col mr-2"
+            />
+            {assignment && !isManuallyGraded(assignment) && (
+                <DynamicButton
+                    text={rebuilding ? "Rebuilding..." : "Rebuild"}
+                    onClick={handleRebuild}
+                    color={rebuilding ? Color.GRAY : Color.BLUE}
+                    type={ButtonType.OUTLINE}
+                    className="col mr-2"
+                />
+            )}
         </div>
     )
 }
