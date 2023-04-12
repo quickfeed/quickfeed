3af6822c61cb2afa685fb3a1470ca82f7946591b
JosteinLindhom <jossi2010@gmail.com>
2023-04-12 13:54:29 +0200

    Removed grpcMan from effects

--- public/src/overmind/effects.tsx
+++ public/src/overmind/effects.tsx
@@ -9,17 +9,5 @@ export const client: ResponseClient<typeof QuickFeedService> = (() => {
     }))
 })()
 
-// Effects should contain all impure functions used to manage state.
-export const grpcMan: GrpcManager | MockGrpcManager = (() => {
-    let grpcMan: GrpcManager | MockGrpcManager
-    if ((process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") && window.location.hostname === "localhost") {
-        // If in development or test mode, and the hostname is localhost, use the MockGrpcManager.
-        grpcMan = new MockGrpcManager()
-    } else {
-        // Otherwise, use the real gRPC manager.
-        grpcMan = new GrpcManager()
-    }
-    return grpcMan
-})()
 
 export const streamService = new StreamService()
