ce8b031c21757f1ff4cee85a9d8533bd2a64bd76
JosteinLindhom <jossi2010@gmail.com>
2023-04-20 13:56:15 +0200

    Simplified `client` initialization

--- public/src/overmind/effects.tsx
+++ public/src/overmind/effects.tsx
@@ -1,13 +1,10 @@
 import { createConnectTransport } from "@bufbuild/connect-web"
 import { QuickFeedService } from "../../proto/qf/quickfeed_connectweb"
 import { StreamService } from "../streamService"
-import { ResponseClient, createResponseClient } from "../client"
+import { createResponseClient } from "../client"
 
-export const client: ResponseClient<typeof QuickFeedService> = (() => {
-    return createResponseClient(QuickFeedService, createConnectTransport({
-        baseUrl: `https://${window.location.host}`,
-    }))
-})()
-
+export const client = createResponseClient(QuickFeedService, createConnectTransport({
+    baseUrl: `https://${window.location.host}`
+}))
 
 export const streamService = new StreamService()
