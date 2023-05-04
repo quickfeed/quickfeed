5d72bdb8c9a0f584dbd5ee526863c6f643844bcf
JosteinLindhom <jossi2010@gmail.com>
2023-05-04 09:47:41 +0200

    Added helper function to simplify mocking rpc methods

--- public/src/__tests__/TestHelpers.ts
+++ public/src/__tests__/TestHelpers.ts
@@ -4,6 +4,7 @@ import { config } from "../overmind"
 import { State } from "../overmind/state"
 import { SubType } from "overmind/lib/internalTypes"
 import { ReviewState } from "../overmind/namespaces/review/state"
+import { ApiClient } from "../overmind/effects"
 
 export const isOverlapping = (rect: IRectangle, rect2: IRectangle) => {
     return (rect.x < rect2.x + rect2.width &&
@@ -72,12 +73,35 @@ export const setupDrivers = (path?: string): ThenableWebDriver[] => {
  * @param mockedEffects the mocked effects to initialize the mock with
  * NOTE: Directly setting derived values in the state is not supported.
 */
-export const initializeOvermind = (state: Partial<State & SubType<{ review: Partial<ReviewState>; }, object>>, mockedEffects?: Partial<typeof config.effects["client"]>
-) => {
+export const initializeOvermind = (state: Partial<State & SubType<{ review: Partial<ReviewState>; }, object>>, mockedApi?: ApiClient) => {
     const overmind = createOvermindMock(config, {
-        client: mockedEffects,
+        api: mockedApi
     }, initialState => {
         Object.assign(initialState, state)
-    })
+    });
+    Object.assign(overmind.effects.api, mockedApi)
     return overmind
+}
+
+/** UnaryApiClient is a type that represents the ApiClient without streaming methods. */
+interface UnaryApiClient {
+    client: Omit<ApiClient["client"], "submissionStream">;
+}
+
+/** Methods is a type that represents the methods of the UnaryApiClient */
+type Methods = UnaryApiClient["client"];
+
+/** mock is a helper function that takes a method and a mocked function to run in place of the method.
+ *  It returns a function that can be used to replace the method in the ApiClient.
+ * @param _method the method to mock
+ * @param mockFn the function to run in place of the method
+ * @example: mock("getSubmission", async (req) => { return { error: null, message: new Submission() } })
+*/
+export function mock<T extends keyof Methods>(
+    _method: T,
+    mockFn: (req: Parameters<Methods[T]>[0]) => ReturnType<Methods[T]>
+): Methods[T] {
+    return async function (...args: any[]): Promise<any> {
+        return mockFn(args);
+    };
 }
