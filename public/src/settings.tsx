b9893aaed4c177084eb5fd98f41e8afd16d111ea
JosteinLindhom <jossi2010@gmail.com>
2024-03-01 09:37:35 +0100

    fix(ui): simplified how settings are applied

--- public/src/settings.tsx
+++ public/src/settings.tsx
@@ -11,14 +11,13 @@ export const ThemeProvider: React.FC<ThemeContextProps> = ({ children }) => {
     const { settings } = useAppState()
 
     useEffect(() => {
-        // Update CSS variables when theme settings change
-        document.documentElement.style.setProperty('--passed-color', settings.settings.passedColor)
-        document.documentElement.style.setProperty('--failed-color', settings.settings.failedColor)
-        document.documentElement.style.setProperty('--bar-width', settings.settings.barWidth + 'px')
-        document.documentElement.style.setProperty('--approved-color', settings.settings.approvedColor)
-        document.documentElement.style.setProperty('--revision-color', settings.settings.revisionColor)
-        document.documentElement.style.setProperty('--rejected-color', settings.settings.rejectedColor)
-    }, [settings.settings.passedColor, settings.settings.failedColor, settings.settings.barWidth, settings.settings.approvedColor, settings.settings.revisionColor, settings.settings.rejectedColor])
+        const theme = settings.settings
+        Object.entries(theme).forEach(([key, value]) => {
+            if (value === undefined) return
+            document.documentElement.style.setProperty('--' + key, value)
+
+        })
+    }, [settings.settings])
 
     return <ThemeContext.Provider value={{}}>{children}</ThemeContext.Provider>
 };
