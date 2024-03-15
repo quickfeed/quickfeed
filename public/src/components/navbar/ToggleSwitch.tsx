cde74803ddc5d1adf455beaecf95762ec0fa4f79
JosteinLindhom <jossi2010@gmail.com>
2024-03-15 15:04:07 +0100

    style: format toggle switch file

--- public/src/components/navbar/ToggleSwitch.tsx
+++ public/src/components/navbar/ToggleSwitch.tsx
@@ -1,46 +1,50 @@
-import React, { useEffect } from 'react'
-import { Enrollment_UserStatus } from '../../../proto/qf/types_pb'
-import { useActions, useAppState } from '../../overmind'
-import { hasTeacher } from '../../Helpers'
-import { useHistory } from 'react-router'
+import React, { useEffect } from "react"
+import { Enrollment_UserStatus } from "../../../proto/qf/types_pb"
+import { useActions, useAppState } from "../../overmind"
+import { hasTeacher } from "../../Helpers"
+import { useHistory } from "react-router"
 
 const ToggleSwitch = () => {
-    const {activeCourse, enrollmentsByCourseID, status} = useAppState()
+    const { activeCourse, enrollmentsByCourseID, status } = useAppState()
     const actions = useActions()
     const navigate = useHistory()
-    const [enrollmentStatus, setEnrollmentStatus] = React.useState<boolean>(false)
+    const [enrollmentStatus, setEnrollmentStatus] =
+        React.useState<boolean>(false)
     const [text, setText] = React.useState<string>("")
 
     useEffect(() => {
         if (activeCourse && enrollmentsByCourseID[activeCourse.toString()]) {
-            updateStatus(isTeacher());
+            updateStatus(isTeacher())
         }
-    });
+    })
 
     const isTeacher = () => {
-        return enrollmentsByCourseID[activeCourse.toString()].status === Enrollment_UserStatus.TEACHER;
+        return (
+            enrollmentsByCourseID[activeCourse.toString()].status ===
+            Enrollment_UserStatus.TEACHER
+        )
     }
 
     const updateStatus = (isTeacher: boolean) => {
-        setEnrollmentStatus(isTeacher);
-        setText(isTeacher ? "T" : "S");
+        setEnrollmentStatus(isTeacher)
+        setText(isTeacher ? "T" : "S")
     }
 
     const switchView = () => {
         actions.changeView(activeCourse).then(() => {
-            updateStatus(isTeacher());
-            navigate.push("/course/" + activeCourse);
+            updateStatus(isTeacher())
+            navigate.push("/course/" + activeCourse)
         })
     }
-    
-    if  (!hasTeacher(status[activeCourse.toString()])) {
+
+    if (!hasTeacher(status[activeCourse.toString()])) {
         return null
     }
 
     return (
         <label className="switch">
             <input type="checkbox" readOnly checked={enrollmentStatus} />
-            <span className="slider round" onClick={switchView} >
+            <span className="slider round" onClick={switchView}>
                 <span className="toggle">{text}</span>
             </span>
         </label>
