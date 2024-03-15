602811bc6640445b2506773fe74e261ec4a93e19
JosteinLindhom <jossi2010@gmail.com>
2024-03-15 12:38:24 +0100

    fix: export overmind reaction hook

--- public/src/overmind/index.tsx
+++ public/src/overmind/index.tsx
@@ -1,4 +1,4 @@
-import { createStateHook, createActionsHook, createEffectsHook } from 'overmind-react'
+import { createStateHook, createActionsHook, createEffectsHook, createReactionHook } from 'overmind-react'
 import { state } from './state'
 import * as actions from './actions'
 import * as effects from './effects'
@@ -33,3 +33,4 @@ export type Context = IContext<{
 export const useAppState = createStateHook<Context>()
 export const useActions = createActionsHook<Context>()
 export const useGrpc = createEffectsHook<Context>()
+export const useReaction = createReactionHook<Context>()
