1695e93372a3608d7374bf4b50dd49df1afbe818
Hein Meling <hein.meling@uis.no>
2024-04-18 10:43:39 +0200

    chore: added newline

--- public/src/overmind/utils/errors.ts
+++ public/src/overmind/utils/errors.ts
@@ -9,14 +9,14 @@ export namespace Prompt {
     export const EnrollmentRepoNotEmpty = "Warning: The enrollment repository is not empty. Do you still want to delete the enrollment and enrollment repository?"
 }
 
-/** promptOnErrorResponse prompts the user with a warning if the response contains an error with the given code. 
+/** promptOnErrorResponse prompts the user with a warning if the response contains an error with the given code.
  *  If the user confirms the warning, the function returns null. Otherwise, it returns the error.
  *  The function is used to prompt the user before performing an action that may result in data loss.
  * @param response The response to check for errors.
  * @param errorCode The error code to check for.
  * @param message The message to display to the user.
  * @returns The error if the user did not confirm the warning, or null if the user did.
- * 
+ *
 */
 export function promptOnErrorResponse<T extends AnyMessage>(response: Response<T>, errorCode: Code, message: string) {
     if (response.error) {
