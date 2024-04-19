84f825c0cc4de80e8b51309ab16545d22c2afd5a
Hein Meling <hein.meling@uis.no>
2024-04-19 17:52:20 +0200

    fix(env): move setting root path to root() instead of init()

--- internal/env/path.go
+++ internal/env/path.go
@@ -18,32 +18,36 @@ var quickfeedRoot string
 
 func init() {
 	quickfeedRoot = os.Getenv("QUICKFEED")
-	if quickfeedRoot == "" {
-		out, err := sh.Output("go list -m -f {{.Dir}} " + quickfeedModulePath)
-		if err != nil {
-			log.Fatalf("Failed to set QUICKFEED variable: %v", err)
-		}
-		quickfeedRoot = strings.TrimSpace(out)
-		os.Setenv("QUICKFEED", quickfeedRoot)
-	}
 }
 
-// root returns the root directory as defined by $QUICKFEED.
+// root returns the root directory as defined by $QUICKFEED or
+// sets it relative to the quickfeed module's root.
 func root() string {
+	if quickfeedRoot != "" {
+		return quickfeedRoot
+	}
+
+	out, err := sh.Output("go list -m -f {{.Dir}} " + quickfeedModulePath)
+	if err != nil {
+		log.Fatalf("Failed to set QUICKFEED variable: %v", err)
+	}
+	quickfeedRoot = strings.TrimSpace(out)
+	os.Setenv("QUICKFEED", quickfeedRoot)
+
 	return quickfeedRoot
 }
 
 // RootEnv returns the path $QUICKFEED/{envFile}.
 func RootEnv(envFile string) string {
-	return filepath.Join(quickfeedRoot, envFile)
+	return filepath.Join(root(), envFile)
 }
 
 // PublicEnv returns the path $QUICKFEED/public/{envFile}.
 func PublicEnv(envFile string) string {
-	return filepath.Join(quickfeedRoot, "public", envFile)
+	return filepath.Join(root(), "public", envFile)
 }
 
 // TestdataPath returns the path to the testdata/courses directory.
 func TestdataPath() string {
-	return filepath.Join(quickfeedRoot, "testdata", "courses")
+	return filepath.Join(root(), "testdata", "courses")
 }
