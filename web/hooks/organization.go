d8f129f3653f98bd1dbfa87a4dbaca673c5d3ee8
JosteinLindhom <jossi2010@gmail.com>
2024-05-02 09:15:37 +0200

    refactor: restructured handleOrgInvite and added some comments

--- web/hooks/organization.go
+++ web/hooks/organization.go
@@ -9,39 +9,41 @@ import (
 )
 
 func (wh GitHubWebHook) handleOrgInvite(e *github.OrganizationEvent) {
+	login := e.GetInvitation().GetLogin()
+	org := e.GetOrganization()
+	course := &qf.Course{
+		ScmOrganizationID:   uint64(org.GetID()),
+		ScmOrganizationName: org.GetLogin(),
+	}
+
+	courseOrg := org.GetLogin()
 	// check that invitee is enrolled in course
-	invitee := e.GetInvitation().GetLogin()
-	user, err := wh.db.GetUserByCourse(&qf.Course{ScmOrganizationID: uint64(e.GetOrganization().GetID())}, invitee)
+	user, err := wh.db.GetUserByCourse(course, login)
 	if err != nil {
-		wh.logger.Error("Invite: Failed to find user:", err)
+		wh.logger.Error("Invite: Failed to find user %s in course %s: %v", login, courseOrg, err)
 		return // course does not exist, or user not enrolled
 	}
 
-	invite := e.GetInvitation()
-	if invite == nil {
-		wh.logger.Error("No invite")
-		return
-	}
-
-	orgName := e.GetOrganization().GetLogin()
-	sc, ok := wh.scmMgr.GetSCM(orgName)
+	sc, ok := wh.scmMgr.GetSCM(courseOrg)
 	if !ok {
-		wh.logger.Debug("Organization", orgName, "does not have a SCM")
+		wh.logger.Debug("Organization", courseOrg, "does not have a SCM")
 		return
 	}
 
 	// AcceptInvitations consumes the current refresh token and returns a new one
+	// If the token exchange fails, the original refresh token is returned
 	newRefreshToken, err := sc.AcceptInvitations(context.Background(), &scm.InvitationOptions{
 		Login:        user.GetLogin(),
-		Owner:        orgName,
+		Owner:        courseOrg,
 		RefreshToken: user.GetRefreshToken(),
 	})
 	if err != nil {
+		// Regardless of the error, we need to update the user's refresh token in the database
 		wh.logger.Error(err)
 	}
-	// The new refresh token needs to be stored in the DB
+
 	user.RefreshToken = newRefreshToken
 	if err := wh.db.UpdateUser(user); err != nil {
-		wh.logger.Errorf("failed to update refresh token for user (id=%d, name=%s): %w", user.ID, user.Name, err)
+		wh.logger.Errorf("Failed to update refresh token for user (id=%d, name=%s): %w", user.ID, user.Name, err)
 	}
 }
