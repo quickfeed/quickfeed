dfe2ec881ab6800c528ca2231e818c746e3aef15
JosteinLindhom <jossi2010@gmail.com>
2024-05-02 09:13:52 +0200

    fix: return the new or original refresh token on errors
    If the token exchange is successful, we need to return the new refresh token to the caller even if we at some point have an error.
    
    If the token exchange is *not* successful, we need to return the original refresh token.

--- scm/github_invite.go
+++ scm/github_invite.go
@@ -26,15 +26,17 @@ func (s *GithubSCM) AcceptInvitations(ctx context.Context, opt *InvitationOption
 	}
 	exchangeToken, err := s.config.ExchangeToken(opt.RefreshToken)
 	if err != nil {
-		return "", fmt.Errorf("failed to exchange refresh token for %s: %w", opt.Login, err)
+		// Return the original refresh token in case of an error.
+		return opt.RefreshToken, fmt.Errorf("failed to exchange refresh token for %s: %w", opt.Login, err)
 	}
 
+	refreshToken := exchangeToken.RefreshToken
 	userSCM := newGithubInviteClient(exchangeToken.AccessToken)
 	for _, repo := range []string{qf.InfoRepo, qf.AssignmentsRepo, qf.StudentRepoName(opt.Login)} {
 		// Important: Get repository invitations using the GitHub App client.
 		repoInvites, _, err := s.client.Repositories.ListInvitations(ctx, opt.Owner, repo, &github.ListOptions{})
 		if err != nil {
-			return "", fmt.Errorf("failed to fetch invitations for repository %s: %w", repo, err)
+			return refreshToken, fmt.Errorf("failed to fetch invitations for repository %s: %w", repo, err)
 		}
 
 		for _, invite := range repoInvites {
@@ -44,15 +46,14 @@ func (s *GithubSCM) AcceptInvitations(ctx context.Context, opt *InvitationOption
 			}
 			// Important: Accept repository invitations using the user-specific GitHub client.
 			if _, err := userSCM.Users.AcceptInvitation(ctx, invite.GetID()); err != nil {
-				return "", fmt.Errorf("failed to accept invitation for repository %s: %w", invite.Repo.GetName(), err)
+				return refreshToken, fmt.Errorf("failed to accept invitation for repository %s: %w", invite.Repo.GetName(), err)
 			}
 		}
 	}
 
 	state := "active"
 	if _, _, err := userSCM.Organizations.EditOrgMembership(ctx, "", opt.Owner, &github.Membership{State: &state}); err != nil {
-		return "", fmt.Errorf("failed to accept invitation to organization %s: %w", opt.Owner, err)
+		return refreshToken, fmt.Errorf("failed to accept invitation to organization %s: %w", opt.Owner, err)
 	}
-	// Return the new refresh token.
-	return exchangeToken.RefreshToken, nil
+	return refreshToken, nil
 }
