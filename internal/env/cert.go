29a74351a6eb861e6e7e442292120e6c6fcf5989
JoachimTislov <joachim.tislov@gmail.com>
2025-01-31 18:26:58 +0100

    test: pragma no cover

--- internal/env/cert.go
+++ internal/env/cert.go
@@ -57,7 +57,7 @@ func Whitelist() ([]string, error) {
 // AuthSecret returns the secret used to sign JWT tokens.
 // If QUICKFEED_AUTH_SECRET is not set, a random secret is generated.
 // Allows for a custom secret to be set.
-func AuthSecret() string {
+func AuthSecret() string { // pragma: no cover
 	authSecret := os.Getenv("QUICKFEED_AUTH_SECRET")
 	if authSecret == "" {
 		return rand.String()
