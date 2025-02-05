72c49711a4d168e28a98812d3ae905a50255e1e2
JoachimTislov <joachim.tislov@gmail.com>
2025-02-05 18:49:44 +0100

    chore: removed kill cmd, and prevent docker from exec setcap

--- air-build.sh
+++ air-build.sh
@@ -1,14 +1,11 @@
 # Script runs between each reload of the server
 # View .air.toml to alter configuration
 
-if pgrep quickfeed > /dev/null; then
-    pkill quickfeed
-fi
+# Build binary with version control system disabled
+go build -buildvcs=false -o ./tmp-air/quickfeed .
 
-# Following can be replaced with 'make install', but would have to update the tmp directory for air
-
-go build -o ./tmp-air/quickfeed .
-
-if [ "$(uname)" == "Linux" ]; then
+# Set capabilities to bind to privileged ports
+# This is only necessary on Linux and when not running in a Docker container
+if [ "$(uname)" == "Linux" ] && [ ! -f /.dockerenv ]; then
     setcap cap_net_bind_service=+ep ./tmp-air/quickfeed
 fi
