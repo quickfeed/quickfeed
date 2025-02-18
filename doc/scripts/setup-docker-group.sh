9ecdf3ae8ed70dcffd0c906089d3859ce8c13a46
Hein Meling <hein.meling@uis.no>
2025-02-19 00:03:11 +0100

    fix: script formatting and remove unnecessary semicolons

--- doc/scripts/setup-docker-group.sh
+++ doc/scripts/setup-docker-group.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 if [ "$(uname)" != "Linux" ]; then
-	echo "This is only for linux"
-	exit 1
+    echo "This is only for linux"
+    exit 1
 fi
 
 if [ -z "$(getent group docker)" ]; then
@@ -9,19 +11,19 @@ if [ -z "$(getent group docker)" ]; then
 fi
 
 if ! systemctl is-active --quiet docker; then
-    sudo systemctl start docker;
+    sudo systemctl start docker
 fi
 
 if [ -z "$(getent group docker | grep $USER)" ]; then
     # Add user
     sudo usermod -aG docker $USER
     # Restart docker
-    sudo systemctl restart docker;
+    sudo systemctl restart docker
 fi
 
 # Check if user does not have access, and if the group is configured correctly
 # The three last statements are opposites of previous statements
-if ! docker ps > /dev/null 2>&1; then
+if ! docker ps >/dev/null 2>&1; then
     echo "The group is configured, but you may need to restart your system for the changes to take effect"
 else
     echo "Docker is running and properly configured"
