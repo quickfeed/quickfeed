50c5bdbe7fe50ce21aff3b019bb450d8debf668b
JosteinLindhom <jossi2010@gmail.com>
2025-03-24 09:47:35 +0100

    style(tests): added newline, removed semicolons

--- public/src/__tests__/setup.jest.js
+++ public/src/__tests__/setup.jest.js
@@ -1,6 +1,6 @@
 // For some reason the new proto types cause issues with Jest
 // complaining about TextEncoder and TextDecoder not available
 // on globalThis. This setup file fixes that issue for now.
-const { TextDecoder, TextEncoder } = require('util');
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+const { TextDecoder, TextEncoder } = require('util')
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
