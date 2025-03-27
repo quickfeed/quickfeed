52d6b4c16d241c004c8e10529a1f865e11df9b59
Glattshaved <mikal94@live.no>
2025-03-27 16:34:26 +0100

    Trying to remove DeepSource nesting issue

--- public/src/pages/AboutPage.tsx
+++ public/src/pages/AboutPage.tsx
@@ -23,59 +23,25 @@ const AboutPage = () => {
                 </section>
                 <hr className="loginDivider" />
                 <div key="rowheader" className="row marketing">
-                    <MiniFeatureBlock
-                        title='GitHub Integration'
-                        content='Manage all students and courses on GitHub. Each student gets their own repository. Teachers get separate repositories for publishing assignments and information to students. All taken care of automatically.'
-                        icon={<i className="fa fa-github align-middle ms-auto" id="github logo" style={{ width: "140px", height: "140px", fontSize: "140px", lineHeight: "140px" }} />}
-                    />
-                    <MiniFeatureBlock
-                        title='Continuous Integration'
-                        content='Instantaneous feedback to students on how well their code performs. Students can quickly identify what they need to focus on to improve. All customizable for the teaching staff.'
-                        imageSrc='/assets/img/overlapping-arrows-no-background.png'
-                    />
-                    <MiniFeatureBlock
-                        title='Fair Grading'
-                        content='On due date of an assignment, the most recent version of each student&apos;s code is available through GitHub. Easily accessible for the teachers. Together with latest build log, this makes grading easier and more fair.'
-                        imageSrc='/assets/img/Aplus2-no-background.png'
-                    />
+                    <MiniFeatureBlock title='GitHub Integration' content='Manage all students and courses on GitHub. Each student gets their own repository. Teachers get separate repositories for publishing assignments and information to students. All taken care of automatically.' icon={<i className="fa fa-github align-middle ms-auto" id="github logo" style={{ width: "140px", height: "140px", fontSize: "140px", lineHeight: "140px" }} />} />
+                    <MiniFeatureBlock title='Continuous Integration' content='Instantaneous feedback to students on how well their code performs. Students can quickly identify what they need to focus on to improve. All customizable for the teaching staff.' imageSrc='/assets/img/overlapping-arrows-no-background.png' />
+                    <MiniFeatureBlock title='Fair Grading' content='On due date of an assignment, the most recent version of each student&apos;s code is available through GitHub. Easily accessible for the teachers. Together with latest build log, this makes grading easier and more fair.' imageSrc='/assets/img/Aplus2-no-background.png' />
                 </div>
                 <hr className="loginDivider" />
                 <section id="automatedfeedback">
-                    <FeatureBlock
-                        heading="QuickFeed"
-                        subheading="Automated student feedback"
-                        content="QuickFeed aims to provide students with fast feedback on their lab assignments, and is designed to help students learn about state-of-the-art tools used in the industry. QuickFeed builds upon version control systems and continuous integration. When students upload code to their repositories, QuickFeed automatically builds their code and provides feedback based on tests supplied by the teaching staff. When grading assignments, teaching staff can access the results of test execution and have a valuable tool in the grading process."
-                        imageSrc="/assets/img/intro1.png"
-                    />
+                    <FeatureBlock heading="QuickFeed" subheading="Automated student feedback" content="QuickFeed aims to provide students with fast feedback on their lab assignments, and is designed to help students learn about state-of-the-art tools used in the industry. QuickFeed builds upon version control systems and continuous integration. When students upload code to their repositories, QuickFeed automatically builds their code and provides feedback based on tests supplied by the teaching staff. When grading assignments, teaching staff can access the results of test execution and have a valuable tool in the grading process." imageSrc="/assets/img/intro1.png" />
                 </section>
                 <hr className="loginDivider" />
                 <section id="versioncontrol">
-                    <FeatureBlock
-                        heading="GitHub Integration"
-                        subheading="Managing courses and students"
-                        content="A course is an organization on GitHub. Students get access to their own private GitHub repository. Uploading their code for review or grading, students can learn to use git for version control."
-                        imageSrc="/assets/img/intro3.png"
-                        reverse
-                    />
+                    <FeatureBlock heading="GitHub Integration" subheading="Managing courses and students" content="A course is an organization on GitHub. Students get access to their own private GitHub repository. Uploading their code for review or grading, students can learn to use git for version control." imageSrc="/assets/img/intro3.png" reverse />
                 </section>
                 <hr className="loginDivider" />
                 <section id="ci">
-                    <FeatureBlock
-                        heading="Continuous Integration"
-                        subheading="Builds and tests student code"
-                        content="As code gets pushed up to GitHub, an automatic build process defined by the teacher, generates feedback to students. When the build process is completed, student gets immediate access to this feedback on their personal course page. Tests defined by either teachers or students will be processed and tell students about their progress on the assignments."
-                        imageSrc="/assets/img/intro2.png"
-                    />
+                    <FeatureBlock heading="Continuous Integration" subheading="Builds and tests student code" content="As code gets pushed up to GitHub, an automatic build process defined by the teacher, generates feedback to students. When the build process is completed, student gets immediate access to this feedback on their personal course page. Tests defined by either teachers or students will be processed and tell students about their progress on the assignments." imageSrc="/assets/img/intro2.png" />
                 </section>
                 <hr className="loginDivider" />
                 <section id="grading">
-                    <FeatureBlock
-                        heading="Grading"
-                        subheading="Easy and Fair"
-                        content="On the due date, teachers can access the test results and use this as a tool in the grading process. The teaching staff will immediately know which of their tests passed, and how much of the code is covered by the tests."
-                        imageSrc="/assets/img/intro4.png"
-                        reverse
-                    />
+                    <FeatureBlock heading="Grading" subheading="Easy and Fair" content="On the due date, teachers can access the test results and use this as a tool in the grading process. The teaching staff will immediately know which of their tests passed, and how much of the code is covered by the tests." imageSrc="/assets/img/intro4.png" reverse />
                 </section>
                 <footer className="text-center mt-5">
                     <button onClick={scrollToTop} className="btn align-items-center backToTop">
