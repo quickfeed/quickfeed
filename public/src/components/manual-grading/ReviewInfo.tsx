866b57f15fb7fb30ccd2ab1f8cd5d9395e1425a5
JoachimTislov <joachim.tislov@gmail.com>
2025-03-27 20:17:02 +0100

    chore: Remove yet another mark ready button adjust text for release

--- public/src/components/manual-grading/ReviewInfo.tsx
+++ public/src/components/manual-grading/ReviewInfo.tsx
@@ -40,16 +40,13 @@ const ReviewInfo = ({ review, selectReviewButton }: { review?: Review, selectRev
             <li className="list-group-item active">
                 <span className="align-middle">
                     <span style={{ display: "inline-block" }} className="w-25 mr-5 p-3">{assignment?.name} {selectReviewButton}</span>
-                    <div className="btn-group float-right">
-                        {<MarkReadyButton review={review} />}
-                        <DynamicButton
-                            text={submission?.released ? "Released" : "Release"}
-                            color={submission?.released ? Color.WHITE : Color.YELLOW}
-                            type={ButtonType.BUTTON}
-                            className={`${!state.isCourseCreator && "disabled"}`}
-                            onClick={() => actions.review.release({ submission, owner: state.submissionOwner })}
-                        />
-                    </div>
+                    <DynamicButton
+                        text={submission?.released ? "Release Lab" : "Revert Release"}
+                        color={submission?.released ? Color.WHITE : Color.YELLOW}
+                        type={ButtonType.BUTTON}
+                        className={`mt-3 float-right ${!state.isCourseCreator && "disabled"}`}
+                        onClick={() => actions.review.release({ submission, owner: state.submissionOwner })}
+                    />
                 </span>
             </li>
             {userLi}
@@ -64,7 +61,7 @@ const ReviewInfo = ({ review, selectReviewButton }: { review?: Review, selectRev
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Review Status: </span>
                 <span>{ready ? "Ready" : "In progress"}</span>
-                {ready && <MarkReadyButton review={review} />}
+                {<MarkReadyButton review={review} />}
             </li>
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Score: </span>
