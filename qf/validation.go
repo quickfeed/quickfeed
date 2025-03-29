8fa4ce97d0e978658a51d7a9124721213813ed99
JoachimTislov <joachim.tislov@gmail.com>
2025-03-29 01:42:59 +0100

    chore: Modify UpdateSubmissionRequest.IsValid - The validation now require a course ID and either a submission or assignment ID

--- qf/validation.go
+++ qf/validation.go
@@ -65,9 +65,11 @@ func (req *SubmissionRequest) IsValid() bool {
 	}
 }
 
-// IsValid ensures that both submission and course IDs are set
+// IsValid ensures that course ID is set and either Submission ID or Assignment ID is set, but not both.
+// Submission ID is for updating an existing submission
+// Assignment ID is for updating all submissions for a given assignment.
 func (req *UpdateSubmissionRequest) IsValid() bool {
-	return req.GetCourseID() > 0 && req.GetSubmissionID() > 0
+	return req.GetCourseID() > 0 && req.GetSubmissionID() > 0 != (req.GetAssignmentID() > 0)
 }
 
 // IsValid ensures course ID is always set and either user ID or group ID is set, but not both.
