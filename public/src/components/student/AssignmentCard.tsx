633e0894d23eec7d2928407ffa970c30fa8e5edf
JoachimTislov <joachim.tislov@gmail.com>
2025-03-30 05:30:21 +0200

    feat(AssignmentCard): Add redirectTo, configure hover and onclick

--- public/src/components/student/AssignmentCard.tsx
+++ public/src/components/student/AssignmentCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Assignment, Submission } from "../../../proto/qf/types_pb"
 import { getFormattedTime } from "../../Helpers"
 import SubmissionRow from './SubmissionRow'
+import { useHistory } from 'react-router'
 
 interface AssignmentCardProps {
   assignment: Assignment
@@ -11,8 +12,26 @@ interface AssignmentCardProps {
 }
 
 const AssignmentCard: React.FC<AssignmentCardProps> = ({ assignment, submissions, courseID, selfID }) => {
+  const history = useHistory()
+  const redirectTo = useCallback((submission: Submission) => {
+    if (submission.groupID !== 0n) {
+      history.push(`/course/${courseID}/group-lab/${submission.AssignmentID.toString()}`)
+    } else {
+      history.push(`/course/${courseID}/lab/${submission.AssignmentID.toString()}`)
+    }
+  }, [history, courseID])
+  const hasSubmissions = submissions.length > 0
+  const redirectToMainAssignment = () => {
+    if (hasSubmissions) {
+      redirectTo(submissions[0])
+    }
+  }
+  // Add onclick and hover only if there are submissions
+  const buttonRole = hasSubmissions ? "button" : ""
+  const ariaHidden = hasSubmissions ? "true" : "false"
+  const hover = hasSubmissions ? "hover-effect" : ""
   return (
-    <div key={assignment.ID.toString()} className="card mb-4 shadow-sm">
+    <div key={assignment.ID.toString()} className={`card mb-4 shadow-sm ${hover}`} onClick={redirectToMainAssignment} role={buttonRole} aria-hidden={ariaHidden}>
       <div className="card-header">
         <div className="d-flex justify-content-between align-items-center">
           <div className="d-flex align-items-center">
@@ -34,6 +53,7 @@ const AssignmentCard: React.FC<AssignmentCardProps> = ({ assignment, submissions
             assignment={assignment}
             courseID={courseID}
             selfID={selfID}
+            redirectTo={redirectTo}
           />
         ))}
       </div>
