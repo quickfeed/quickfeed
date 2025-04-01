2a026ccca7b172c915b1e241d5e95a9d05039ad1
JoachimTislov <joachim.tislov@gmail.com>
2025-04-01 03:46:03 +0200

    feature(esbuild): Add dependency and an external tool - The external tool is meant for development to build the frontend

--- Makefile
+++ Makefile
@@ -35,11 +35,13 @@ endif
 
 ui: version-check
 	@echo "Running npm ci and webpack"
-	@cd public; npm ci; webpack
+	@cd public; npm ci
+	@go run cmd/esbuild/main.go
 
 ui-update: version-check
 	@echo "Running npm install and webpack"
-	@cd public; npm i; webpack
+	@cd public; npm i
+	@go run cmd/esbuild/main.go
 
 proto:
 	buf dep update
---
+++ cmd/esbuild/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/quickfeed/quickfeed/internal/ui"
+)
+
+// Tool to execute esbuild externally.
+// Meant for development purposes.
+// Used to rebuild the UI after modifying the source code.
+// The frontend src code is located in the public directory.
+// The compiled code is placed in the dist directory.
+func main() {
+	if err := ui.Build(nil); err != nil {
+		log.Fatalf("failed to build UI: %v", err)
+	}
+}
