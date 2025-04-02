5375ed3c771aeedc0135545d7c3516f5551b95b4
JoachimTislov <joachim.tislov@gmail.com>
2025-04-02 20:08:27 +0200

    chore(frontend): Move code to ReviewBanner

--- public/src/components/manual-grading/ReviewForm.tsx
+++ public/src/components/manual-grading/ReviewForm.tsx
@@ -1,72 +1,39 @@
 import React from "react"
-import { Review } from "../../../proto/qf/types_pb"
-import { isManuallyGraded, Color } from "../../Helpers"
-import { useActions, useAppState } from "../../overmind"
-import Button, { ButtonType } from "../admin/Button"
+import { isManuallyGraded } from "../../Helpers"
+import { useAppState } from "../../overmind"
 import ReviewInfo from "./ReviewInfo"
 import ReviewResult from "../ReviewResult"
+import ReviewBanner from "./ReviewBanner"
 
 
 const ReviewForm = () => {
     const state = useAppState()
-    const actions = useActions()
 
-    if (!state.selectedSubmission) {
+    const submission = state.selectedSubmission
+    if (!submission) {
         return <div>No submission selected</div>
     }
-
     const assignment = state.selectedAssignment
     if (!assignment) {
         return <div>No Submission</div>
     }
-
-    const isAuthor = (review: Review) => {
-        return review?.ReviewerID === state.self.ID
-    }
-
-    const reviewers = assignment.reviewers ?? 0
-    const reviews = state.review.reviews.get(state.selectedSubmission.ID) ?? []
-    const selectReviewButton: React.JSX.Element[] = []
-
-    reviews.forEach((review, index) => {
-        selectReviewButton.push(
-            <Button key={review.ID.toString()}
-                text={review.ready ? "Ready" : "In Progress"}
-                color={review.ready ? Color.GREEN : Color.YELLOW}
-                type={ButtonType.BUTTON}
-                className={`mr-1 ${state.review.selectedReview === index ? "active border border-dark" : ""}`}
-                onClick={() => { actions.review.setSelectedReview(index) }}
-            />
-        )
-    })
-
-    if ((reviews.length === 0 || reviews.some(review => !isAuthor(review))) && (reviewers - reviews.length) > 0) {
-        // Display a button to create a new review if:
-        // there are no reviews or the current user is not the author of the review, and there are still available review slots
-        selectReviewButton.push(
-            <Button key="add"
-                text="Add Review"
-                color={Color.BLUE}
-                type={ButtonType.BUTTON}
-                className="mr-1"
-                onClick={async () => { await actions.review.createReview() }}
-            />
-        )
-    }
+    const review = state.review.currentReview
 
     if (!isManuallyGraded(assignment)) {
         return <div>This assignment is not for manual grading.</div>
     } else {
         return (
-            <div className="col lab-sticky reviewLabResult">
-                <div className="mb-1">{selectReviewButton}</div>
-                {state.review.currentReview ? (
-                    <>
-                        <ReviewInfo review={state.review.currentReview} />
-                        <ReviewResult review={state.review.currentReview} />
-                    </>
-                ) : null}
-            </div>
+            <div className="col">
+                <ReviewBanner assignment={assignment} submission={submission} review={review} />
+                <div className="reviewLabResult">
+                    {review ? (
+                        <>
+                            <ReviewInfo review={review} />
+                            <ReviewResult review={review} />
+                        </>
+                    ) : null}
+                </div>
+            </div >
         )
     }
 }
--- public/src/components/manual-grading/ReviewInfo.tsx
+++ public/src/components/manual-grading/ReviewInfo.tsx
@@ -1,27 +1,19 @@
 import React from "react"
 import { Review, Submission_Status } from "../../../proto/qf/types_pb"
 import { NoSubmission } from "../../consts"
-import { Color, getFormattedTime, getStatusByUser, SubmissionStatus } from "../../Helpers"
-import { useActions, useAppState } from "../../overmind"
-import { ButtonType } from "../admin/Button"
-import DynamicButton from "../DynamicButton"
-import ManageSubmissionStatus from "../ManageSubmissionStatus"
-import MarkReadyButton from "./MarkReadyButton"
+import { getFormattedTime, getStatusByUser, SubmissionStatus } from "../../Helpers"
+import { useAppState } from "../../overmind"
 
 
 const ReviewInfo = ({ review }: { review?: Review }) => {
     const state = useAppState()
-    const actions = useActions()
 
     if (!review) {
         return null
     }
 
-    const assignment = state.selectedAssignment
     const submission = state.selectedSubmission
     const ready = review.ready
-
-    const markReadyButton = <MarkReadyButton review={review} />
 
     const user = state.selectedEnrollment?.user
     let status = Submission_Status.NONE
@@ -37,24 +29,8 @@ const ReviewInfo = ({ review }: { review?: Review }) => {
         )
     }
 
-    const setReadyOrGradeButton = ready ? <ManageSubmissionStatus /> : markReadyButton
-    const releaseButton = (
-        <DynamicButton
-            text={submission?.released ? "Released" : "Release"}
-            color={submission?.released ? Color.WHITE : Color.YELLOW}
-            type={ButtonType.BUTTON}
-            className={`float-right ${!state.isCourseCreator && "disabled"} `}
-            onClick={() => actions.review.release({ submission, owner: state.submissionOwner })}
-        />
-    )
     return (
         <ul className="list-group">
-            <li className="list-group-item active">
-                <span className="align-middle">
-                    <span style={{ display: "inline-block" }} className="w-25 mr-5 p-3">{assignment?.name}</span>
-                    {releaseButton}
-                </span>
-            </li>
             {userLi}
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Reviewer: </span>
@@ -62,12 +38,11 @@ const ReviewInfo = ({ review }: { review?: Review }) => {
             </li>
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Submission Status: </span>
-                {submission ? SubmissionStatus[status] :  NoSubmission }
+                {submission ? SubmissionStatus[status] : NoSubmission}
             </li>
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Review Status: </span>
                 <span>{ready ? "Ready" : "In progress"}</span>
-                {ready && markReadyButton}
             </li>
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Score: </span>
@@ -80,9 +55,6 @@ const ReviewInfo = ({ review }: { review?: Review }) => {
             <li className="list-group-item">
                 <span className="w-25 mr-5 float-left">Graded: </span>
                 {state.review.graded}/{state.review.criteriaTotal}
-            </li>
-            <li className="list-group-item">
-                {setReadyOrGradeButton}
             </li>
         </ul>
     )
