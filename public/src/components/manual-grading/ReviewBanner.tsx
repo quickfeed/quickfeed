045ff87b5d139b133af3cd5d657dfd0ca2e4600a
JoachimTislov <joachim.tislov@gmail.com>
2025-04-03 10:43:08 +0200

    chore(ReviewBanner): Convert to radio and move mark ready button - Replaced review status with reviewer's initials

--- public/src/components/manual-grading/ReviewBanner.tsx
+++ public/src/components/manual-grading/ReviewBanner.tsx
@@ -27,11 +27,12 @@ const ReviewBanner = ({ assignment, submission, review }: ReviewBannerProps) => 
 
     reviews.forEach((review, index) => {
         const border = state.review.selectedReview === index ? "active border border-dark" : ""
-        const reviewStatus = review.ready ? "Ready" : "In Progress"
-        const selected = state.review.selectedReview === index ? "disabled" : ""
+        const selected = state.review.selectedReview !== index ? "disabled" : ""
+        const name = state.courseTeachers[review.ReviewerID.toString()]?.Name
+        const initials = name ? name.split(" ").map((n) => n[0]).join("") : ""
         selectReviewButtons.push(
             <Button key={review.ID.toString()}
-                text={`#${index + 1} ${reviewStatus}`}
+                text={`#${index + 1} ${initials}`}
                 color={review.ready ? Color.GREEN : Color.YELLOW}
                 type={ButtonType.BUTTON}
                 className={`mr-1 ${border} ${selected}`}
@@ -53,18 +54,25 @@ const ReviewBanner = ({ assignment, submission, review }: ReviewBannerProps) => 
 
     return (
         <div className="lab-sticky bg-dark text-white d-flex flex-column">
-            <div className="d-flex p-2">
-                {selectReviewButtons}
-                {addReviewButton}
-            </div>
+            <ul className="nav nav-tabs p-2">
+                {selectReviewButtons.map((button) => (
+                    <li className="nav-item" key={button.key}>
+                        {button}
+                    </li>
+                ))}
+                <li className="nav-item">
+                    {addReviewButton}
+                </li>
+            </ul>
             <div className="d-flex">
                 <div className="d-flex p-2 w-40 mr-5">
                     <h4 className="p-2">{assignment.name}</h4>
                     <div className="mt-1">
-                        {review ? <MarkReadyButton review={review} /> : null}
+                        {review?.ready ? <MarkReadyButton review={review} /> : null}
                     </div>
                 </div>
                 <div className="ml-auto mt-2 mr-2">
+                    {review && !review.ready ? <MarkReadyButton review={review} /> : null}
                     {review?.ready ?
                         <DynamicButton
                             text={submission?.released ? "Revert Release" : "Release Lab"}
