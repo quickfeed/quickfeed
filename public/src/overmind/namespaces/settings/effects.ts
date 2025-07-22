fd9a689a9a45f72c9996031b6560e77596280d40
JosteinLindhom <jossi2010@gmail.com>
2025-07-22 13:32:51 +0200

    style: remove unnecessary semicolons for consistency

--- public/src/overmind/namespaces/settings/effects.ts
+++ public/src/overmind/namespaces/settings/effects.ts
@@ -1,12 +1,12 @@
 // src/overmind/effects.ts
-import { UserSettings } from './state';
+import { UserSettings } from './state'
 
 export const settings = {
     saveSettings(settings: UserSettings) {
-        localStorage.setItem('userSettings', JSON.stringify(settings));
+        localStorage.setItem('userSettings', JSON.stringify(settings))
     },
     loadSettings(): UserSettings {
-        const settings = localStorage.getItem('userSettings');
-        return settings ? JSON.parse(settings) : null;
+        const settings = localStorage.getItem('userSettings')
+        return settings ? JSON.parse(settings) : null
     },
-};
+}
