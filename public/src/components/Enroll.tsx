ababb7ef06b0b17758a0fe9082ba9d98a440a01b
JosteinLindhom <jossi2010@gmail.com>
2025-08-18 13:57:26 +0200

    feat: add pending enrollment check to Enroll component

--- public/src/components/Enroll.tsx
+++ public/src/components/Enroll.tsx
@@ -4,6 +4,7 @@ import { useAppState } from "../overmind"
 import CourseCard from "./CourseCard"
 import { create } from "@bufbuild/protobuf"
 import { EnrollmentSchema } from "../../proto/qf/types_pb"
+import { isPending } from "../Helpers"
 
 
 export const Enroll = ({ courseID }: { courseID: bigint }) => {
@@ -14,6 +15,18 @@ export const Enroll = ({ courseID }: { courseID: bigint }) => {
         // If no course is found, we can return a placeholder or an error message.
         return <Navigate to="/" replace />
     }
+
+    if (isPending(enrollment)) {
+        // If the user is already pending enrollment, inform them that they are pending
+        return (
+            <div className="box centered">
+                <h3>You have already requested to enroll in {course.name}.</h3>
+                <p>Please wait for the course administrator to approve your enrollment.</p>
+                <CourseCard course={course} enrollment={enrollment} />
+            </div>
+        )
+    }
+
     return (
         <div className="box centered">
             <h3>Enroll in {course.name}</h3>
