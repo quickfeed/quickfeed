a17ee761e787844b59147bb3670fa1a2a2f21c32
JosteinLindhom <jossi2010@gmail.com>
2025-09-23 14:31:16 +0200

    fix: specify type for courseFeedbackData FeedbackSummaryView

--- public/src/components/feedback/FeedbackSummaryView.tsx
+++ public/src/components/feedback/FeedbackSummaryView.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 import { useAppState } from '../../overmind'
 import { Assignment } from '../../../proto/qf/types_pb'
+import { CourseFeedbackData } from "../../overmind/namespaces/feedback/state"
 
 interface FeedbackSummaryViewProps {
     assignments: Assignment[]
-    courseFeedbackData: any
+    courseFeedbackData: CourseFeedbackData | undefined
     avgTimeSpent: (assignmentID: bigint) => [number, number]
 }
 
