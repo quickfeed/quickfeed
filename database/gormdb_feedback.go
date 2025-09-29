8c48463589d65f9a9f5b691c907fed43790f03e6
JosteinLindhom <jossi2010@gmail.com>
2025-09-29 16:27:28 +0200

    refactor: update GetAssignmentFeedback to use CourseRequest and simplify query logic

--- database/gormdb_feedback.go
+++ database/gormdb_feedback.go
@@ -20,19 +20,9 @@ func (db *GormDB) CreateAssignmentFeedback(feedback *qf.AssignmentFeedback) erro
 // GetAssignmentFeedback returns a list of assignment feedback matching the given query.
 // If userID is specified, returns feedback from that user only.
 // Otherwise, returns all feedback for the assignment.
-func (db *GormDB) GetAssignmentFeedback(query *qf.AssignmentFeedbackRequest) (*qf.AssignmentFeedbacks, error) {
+func (db *GormDB) GetAssignmentFeedback(query *qf.CourseRequest) (*qf.AssignmentFeedbacks, error) {
 	var feedbacks []*qf.AssignmentFeedback
 	dbQuery := db.conn.Model(&qf.AssignmentFeedback{})
-
-	if query.GetAssignmentID() > 0 || query.GetUserID() > 0 {
-		switch query.GetMode().(type) {
-		case *qf.AssignmentFeedbackRequest_AssignmentID:
-			dbQuery = dbQuery.Where("assignment_id = ?", query.GetAssignmentID())
-		case *qf.AssignmentFeedbackRequest_UserID:
-			dbQuery = dbQuery.Where("user_id = ?", query.GetUserID())
-		}
-	}
-
 	dbQuery = dbQuery.Where("course_id = ?", query.GetCourseID())
 
 	if err := dbQuery.Find(&feedbacks).Error; err != nil {
