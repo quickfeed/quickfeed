ad48a976aebfb2ec3278ca7de8f36dfb5984e6bf
JosteinLindhom <jossi2010@gmail.com>
2026-02-27 14:06:46 +0100

    style(Alerts): add margin spacing with Tailwind

--- public/src/components/alerts/Alerts.tsx
+++ public/src/components/alerts/Alerts.tsx
@@ -6,12 +6,11 @@ import Alert from "./Alert"
 /* This component displays all alerts found in state.alerts */
 const Alerts = () => {
     const state = useAppState()
-    /* Index is used to remove the alert from the state.alerts array */
     const alerts = state.alerts.map((alert) => {
         return <Alert alert={alert} key={alert.id} />
     })
 
-    return <div>{alerts}</div>
+    return <div className="mb-3">{alerts}</div>
 }
 
 export default Alerts
