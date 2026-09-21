0e9a3c4fd94e3ed19b060e9b823e81b2100b8754
JosteinLindhom <jossi2010@gmail.com>
2026-09-21 12:50:30 +0200

    fix: collision between FA and DaisyUI caused FA icons to be positioned incorrectly

--- public/src/pages/LoginPage.tsx
+++ public/src/pages/LoginPage.tsx
@@ -12,7 +12,7 @@ const LoginPage = () => {
             </p>
             <section className="mb-12">
                 <div className="card bg-base-200 shadow-xl p-8 text-center min-w-[300px]">
-                    <i className="fab fa-github fa-5x mb-4 text-base-content self-center" />
+                    <i className="fa-brands fa-github fa-5x mb-4 text-base-content self-center" />
                     <h4 className="text-xl font-semibold mb-2">Sign in with GitHub</h4>
                     <p className="text-base-content/60 mb-6">to continue to QuickFeed</p>
                     <a
